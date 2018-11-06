832d1f9fc333668355e4b89866d08342286fbde6
Maël Nison <nison.mael@gmail.com>
2018-11-06 23:22:17 +0000

    Adds an help command

---
+++ packages/berry-cli/sources/commands/help.ts
@@ -0,0 +1,14 @@
+import {Writable} from 'stream';
+
+export default (concierge: any) => concierge
+
+  .command(`help [command]`)
+  .describe(`print the program usage`)
+
+  .action(async ({command, ... rest}: {command: string | null, rest: {[key: string]: any}}) => {
+    if (command) {
+      return await concierge.run(null, [command, `-h`], rest);
+    } else {
+      return await concierge.run(null, [`-h`], rest);
+    }
+  });
