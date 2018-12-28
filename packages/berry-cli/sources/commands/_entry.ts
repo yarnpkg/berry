c91f061770ae92107f3b0cb419f6b26f72b1886e
Maël Nison <mael@fb.com>
2018-12-28 12:23:10 +0000

    Adds support for --version

--- packages/berry-cli/sources/commands/_entry.ts
+++ packages/berry-cli/sources/commands/_entry.ts
@@ -4,8 +4,10 @@ export default (concierge: any) => concierge
   .describe(`select whether to use install or run`)
   .flags({proxyArguments: true, defaultCommand: true, hiddenCommand: true})
 
-  .action(async ({args, ... env}: any) => {
-    if (args.length === 0 || args[0].charAt(0) === `-`) {
+  .action(async ({version, args, stdout, ... env}: any) => {
+    if (args.length === 1 && args[0] === `--version`) {
+      stdout.write(`v2.0.0\n`);
+    } else if (args.length === 0 || args[0].charAt(0) === `-`) {
       return await concierge.run(null, [`install`, ... args], env);
     } else {
       return await concierge.run(null, [`run`, ... args], env);
