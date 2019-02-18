707a3658f1f6814e39cdf60c2623af8cb246f682
Maël Nison <mael@fb.com>
2019-02-18 13:59:25 +0000

    Adds a microkernel entry point

--- packages/berry-pnp/sources/hydratePnpApi.ts
+++ packages/berry-pnp/sources/hydratePnpApi.ts
@@ -8,6 +8,25 @@ import {SerializedState}     from './types';
 
 const readFileP = promisify(readFile);
 
+// Note that using those functions is typically NOT needed! The PnP API is
+// designed to be consumed directly from within Node - meaning that depending
+// on your situation you probably should use one of those two alternatives
+// instead:
+//
+//   - If your script is executing within a PnP environment, you'll be able to
+//     simply `require("pnpapi")` in order to get a reference to the running
+//     API. You can also simply check whether you're actually running within a
+//     PnP environment by checking `process.versions.pnp`.
+//
+//   - Or if you're not running within a PnP environment, or wish to interact
+//     with a different one than the current one, then you can directly require
+//     its `.pnp.js` file.
+//
+// The function exported in this file only work when the PnP data are kept
+// outside of the loader (pnpEnableInlining = false in Yarn), and their only
+// real use case is to access the PnP API without running the risk of executing
+// third-party Javascript code.
+
 export async function hydratePnpFile(location: string, {pnpapiResolution}: {pnpapiResolution: string}) {
   const source = await readFileP(location, `utf8`);
 
