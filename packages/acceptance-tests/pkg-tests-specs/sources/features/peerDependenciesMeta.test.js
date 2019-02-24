fb82b164d93da74f37d4085ffa52249fe425959e
Maël Nison <nison.mael@gmail.com>
2019-02-24 02:06:12 +0000

    Adds a test for peerDependenciesMeta.optional

---
+++ packages/acceptance-tests/pkg-tests-specs/sources/features/peerDependenciesMeta.test.js
@@ -0,0 +1,31 @@
+describe(`Features`, () => {
+  describe(`peerDependenciesMeta`, () => {
+    test(
+      `it should report a warning when omitting a peer dependencies`,
+      makeTemporaryEnv(
+        {
+          dependencies: {[`peer-deps`]: `1.0.0`},
+        },
+        async ({path, run, source}) => {
+          const {stdout} = await run(`install`);
+
+          expect(stdout).toContain(`BR0002`);
+        },
+      ),
+    );
+
+    test(
+      `it should not report a warning when omitting an optional peer dependency`,
+      makeTemporaryEnv(
+        {
+          dependencies: {[`optional-peer-deps`]: `1.0.0`},
+        },
+        async ({path, run, source}) => {
+          const {stdout} = await run(`install`);
+
+          expect(stdout).not.toContain(`BR0002`);
+        },
+      ),
+    );
+  });
+});
