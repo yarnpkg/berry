e7f88b3d694fb99c49176877aa29c4f43b7f4343
Maël Nison <nison.mael@gmail.com>
2019-03-20 14:40:27 -0700

    Update config.ts

--- packages/plugin-essentials/sources/commands/config.ts
+++ packages/plugin-essentials/sources/commands/config.ts
@@ -17,6 +17,8 @@ export default (concierge: any, pluginConfiguration: PluginConfiguration) => con
 
   .detail(`
     This command prints the current active configuration settings. When used together with the \`-v,--verbose\` option, the output will contain the settings description on top of the regular key/value information.
+
+    Note that the paths settings will be normalized - especially on Windows. It means that paths such as \`C:\\project\` will be transparently shown as \`/mnt/c/project\`.
   `)
 
   .example(
