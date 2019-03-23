97e5c816f102dd6838609b3a2cce92c8b6076ab9
Maël Nison <mael@fb.com>
2019-03-23 15:21:02 -0700

    Fixes the favicon

--- packages/gatsby/gatsby-config.js
+++ packages/gatsby/gatsby-config.js
@@ -57,19 +57,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Yarn`,
+        short_name: `yarn`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2188b6`,
+        theme_color: `#2188b6`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-favicon`,
-      options: {
-        logo: `${__dirname}/src/images/yarn-kitten.svg`,
+        icon: `src/images/yarn-kitten.svg`,
       },
     },
     {
