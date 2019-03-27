fafdb9180d4c2841621669c748c63d39e066303a
Maël Nison <nison.mael@gmail.com>
2019-03-27 22:08:21 +0000

    Fixes the stage tests

--- packages/acceptance-tests/pkg-tests-specs/sources/commands/stage.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/commands/stage.test.js
@@ -1,3 +1,5 @@
+import {normalize} from 'path';
+
 const {
   exec: {execFile},
   fs: {createTemporaryFolder, mkdirp, readJson, writeFile},
@@ -9,10 +11,10 @@ describe(`Commands`, () => {
       `it should stage the initial files`,
       makeTemporaryEnv({}, async ({path, run, source}) => {
         await execFile(`git`, [`init`], {cwd: path});
-        await writeFile(`${path}/.yarnrc`, `plugins:\n  - "${require.resolve(`@berry/monorepo/scripts/plugin-stage.js`)}"\n`);
+        await writeFile(`${path}/.yarnrc`, `plugins:\n  - ${JSON.stringify(require.resolve(`@berry/monorepo/scripts/plugin-stage.js`))}\n`);
 
         await expect(run(`stage`, `-n`, {cwd: path})).resolves.toMatchObject({
-          stdout: `${path}/.yarnrc\n${path}/package.json\n`,
+          stdout: `${normalize(`${path}/.yarnrc`)}\n${normalize(`${path}/package.json`)}\n`,
         });
       }),
     );
@@ -21,12 +23,12 @@ describe(`Commands`, () => {
       `it should not stage non-yarn files`,
       makeTemporaryEnv({}, async ({path, run, source}) => {
         await execFile(`git`, [`init`], {cwd: path});
-        await writeFile(`${path}/.yarnrc`, `plugins:\n  - "${require.resolve(`@berry/monorepo/scripts/plugin-stage.js`)}"\n`);
+        await writeFile(`${path}/.yarnrc`, `plugins:\n  - ${JSON.stringify(require.resolve(`@berry/monorepo/scripts/plugin-stage.js`))}\n`);
 
         await writeFile(`${path}/index.js`, `module.exports = 42;\n`);
 
         await expect(run(`stage`, `-n`, {cwd: path})).resolves.toMatchObject({
-          stdout: `${path}/.yarnrc\n${path}/package.json\n`,
+          stdout: `${normalize(`${path}/.yarnrc`)}\n${normalize(`${path}/package.json`)}\n`,
         });
       }),
     );
@@ -41,17 +43,17 @@ describe(`Commands`, () => {
         await run(`install`);
 
         await execFile(`git`, [`init`], {cwd: path});
-        await writeFile(`${path}/.yarnrc`, `plugins:\n  - "${require.resolve(`@berry/monorepo/scripts/plugin-stage.js`)}"\n`);
+        await writeFile(`${path}/.yarnrc`, `plugins:\n  - ${JSON.stringify(require.resolve(`@berry/monorepo/scripts/plugin-stage.js`))}\n`);
 
         await expect(run(`stage`, `-n`, {cwd: path})).resolves.toMatchObject({
           stdout: [
-            `${path}/.pnp.js\n`,
-            `${path}/.yarn/build-state.yml\n`,
-            `${path}/.yarn/cache/.gitignore\n`,
-            `${path}/.yarn/cache/no-deps-npm-1.0.0-7b98016e4791f26dcb7dcf593c5483002916726a04cbeec6eb2ab72d35ed3c1e.zip\n`,
-            `${path}/.yarnrc\n`,
-            `${path}/package.json\n`,
-            `${path}/yarn.lock\n`,
+            normalize(`${path}/.pnp.js\n`),
+            normalize(`${path}/.yarn/build-state.yml\n`),
+            normalize(`${path}/.yarn/cache/.gitignore\n`),
+            normalize(`${path}/.yarn/cache/no-deps-npm-1.0.0-7b98016e4791f26dcb7dcf593c5483002916726a04cbeec6eb2ab72d35ed3c1e.zip\n`),
+            normalize(`${path}/.yarnrc\n`),
+            normalize(`${path}/package.json\n`),
+            normalize(`${path}/yarn.lock\n`),
           ].join(``),
         });
       }),
