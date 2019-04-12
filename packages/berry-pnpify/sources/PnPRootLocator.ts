c7abc5a91ac2826c1618c8a9e35111afd41e9530
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-04-12 21:50:27 +0300

    Implement pnp root locator cache invalidation

--- packages/berry-pnpify/sources/PnPRootLocator.ts
+++ packages/berry-pnpify/sources/PnPRootLocator.ts
@@ -56,9 +56,35 @@ type PnPRootCheckTree = Map<string, any | true>;
  *
  * PnP root locator tries to answer this question with minimal possible number of fs calls.
  *
- * Note, it does not handle the case when PnP projects are added or removed during
- * the lifespan of currently running node proces.
+ * Assumptions:
+ *  - PnP root cannot be inside `node_modules`
+ *  - PnP root cannot be inside other PnP root
  */
+export interface PnPRootLocator {
+  /**
+   * Constructs new instance of PnP root locator
+   *
+   * @param options optional locator options
+   */
+  new(options?: PnPLocatorOptions): PnPRootLocator;
+
+  /**
+   * Finds PnP root directory for given `sourcePath`.
+   *
+   * @param sourcePath some directory that might be inside or outside PnP project
+   *
+   * @returns null if `sourcePath` is not inside PnP project, or root directory of PnP project otherwise
+   */
+  findApiRoot(sourcePath: string): string | null;
+
+  /**
+   * Tell locator that the given path and all child paths should be rechecked
+   *
+   * @param sourcePath path to invalidate, empty string invalidates all the paths
+   */
+  invalidatePath(sourcePath: string): void;
+}
+
 export class PnPRootLocator {
   private readonly options: DefinedPnPLocatorOptions;
   private checkTree: PnPRootCheckTree;
@@ -72,39 +98,46 @@ export class PnPRootLocator {
     this.checkTree = new Map();
   }
 
-  /**
-   * Finds PnP root directory for given `sourcePath`.
-   *
-   * Note, this method assumes PnP root cannot be inside `node_modules`
-   *
-   * @param sourcePath some directory that might be inside or outside PnP project
-   *
-   * @returns null if `sourcePath` is not inside PnP project, or root directory of PnP project otherwise
-   */
+  private getPathSubdirs(sourcePath: string): string[] {
+    const normalizedPath = sourcePath.replace(/\\/g, '/').replace(/\/+$/, '');
+    const idx = normalizedPath.indexOf('\/node_modules');
+    return (idx >= 0 ? normalizedPath.substring(0, idx) : normalizedPath).split('/');
+  }
+
   public findApiRoot(sourcePath: string): string | null {
     let apiPath = null;
-    const normalizedPath = sourcePath.replace(/\\/g, '/');
-    const matches = normalizedPath.match(/^(.*)(?:\/node_modules(?:\/|$).*)?/);
-    if (matches) {
-      const basePath = matches[1];
-      const subdirs = basePath.split('/');
-      let currentPath;
-      let node = this.checkTree;
-      for (const subdir of subdirs) {
-        currentPath = typeof currentPath === 'undefined' ? subdir : currentPath + '/' + subdir;
-        let val = node.get(subdir);
-        if (typeof val === 'undefined') {
-          val = this.options.existsSync(currentPath + '/' + this.options.pnpFileName) ? true : new Map();
-          node.set(subdir, val);
-        }
-        if (val === true) {
-          apiPath = currentPath;
-          break;
-        }
-        node = val;
+    const subdirs = this.getPathSubdirs(sourcePath);
+
+    let currentPath;
+    let node = this.checkTree;
+    for (const subdir of subdirs) {
+      currentPath = typeof currentPath === 'undefined' ? subdir : currentPath + '/' + subdir;
+      let val = node.get(subdir);
+      if (typeof val === 'undefined') {
+        val = this.options.existsSync(currentPath + '/' + this.options.pnpFileName) ? true : new Map();
+        node.set(subdir, val);
       }
+      if (val === true) {
+        apiPath = currentPath;
+        break;
+      }
+      node = val;
     }
 
     return apiPath;
+  }
+
+  invalidatePath(sourcePath: string) {
+    const subdirs = this.getPathSubdirs(sourcePath);
+    let node = this.checkTree;
+    for (const subdir of subdirs.slice(0, -1)) {
+      node = node.get(subdir);
+      if (typeof node === 'undefined') {
+        break;
+      }
+    }
+    if (typeof node !== 'undefined') {
+      node.delete(subdirs[subdirs.length - 1]);
+    }
   }
 }
--- packages/berry-pnpify/tests/PnPRootLocator.test.ts
+++ packages/berry-pnpify/tests/PnPRootLocator.test.ts
@@ -36,8 +36,31 @@ describe('PnPRootLocator should', () => {
     expect(checkCount).toEqual(lastCheckCount);
   })
 
+  it('not do extra checks for folders inside node_modules', () => {
+    locator.findApiRoot('/home/user');
+    const lastCheckCount = checkCount;
+    locator.findApiRoot('/home/user/node_modules/a/b/c/d/e');
+    expect(checkCount).toEqual(lastCheckCount);
+  });
+
   it('finds pnp roots for two different pnp projects', () => {
     expect(locator.findApiRoot('/home/user/pnp_project/some_dir/some_file.js')).toEqual('/home/user/pnp_project');
     expect(locator.findApiRoot('/home/user/some_dir/pnp_project/other_dir/some_file.js')).toEqual('/home/user/some_dir/pnp_project');
+  });
+
+  it('support invalidating all the cache', () => {
+    locator.findApiRoot('/home/user/pnp_project/some_dir/some_file.js');
+    const lastCheckCount = checkCount;
+    locator.invalidatePath('/');
+    locator.findApiRoot('/home/user/pnp_project/some_dir/some_file.js');
+    expect(checkCount).toEqual(lastCheckCount * 2);
+  });
+
+  it('support partial cache invalidation', () => {
+    locator.findApiRoot('/home/user/pnp_project');
+    const lastCheckCount = checkCount;
+    locator.invalidatePath('/home/user/pnp_project');
+    locator.findApiRoot('/home/user/pnp_project/some_dir/some_file.js');
+    expect(checkCount).toEqual(lastCheckCount + 1);
   });
 });
