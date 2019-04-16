880113fccb752fed010df105048cbad6b3bf6734
Maël Nison <nison.mael@gmail.com>
2019-04-16 12:26:45 +0100

    Update tauModule.ts

--- packages/plugin-constraints/sources/tauModule.ts
+++ packages/plugin-constraints/sources/tauModule.ts
@@ -3,12 +3,14 @@ import pl        from 'tau-prolog';
 
 const {is_atom} = pl.type;
 
-function prependGoals(
-    thread: pl.type.Thread, point: pl.type.State,
-    goals: pl.type.Term<number, string>[]): void {
+function prependGoals(thread: pl.type.Thread, point: pl.type.State, goals: pl.type.Term<number, string>[]): void {
   thread.prepend(goals.map(
-      goal => new pl.type.State(
-          point.goal.replace(goal), point.substitution, point)));
+    goal => new pl.type.State(
+      point.goal.replace(goal),
+      point.substitution,
+      point,
+    ),
+  ));
 }
 
 const projects = new WeakMap<pl.type.Session, Project>();
@@ -22,40 +24,38 @@ function getProject(thread: pl.type.Thread): Project {
   return project;
 }
 
-const tauModule = new pl.type.Module(
-    `constraints`, {
-      'workspace_field/3': (thread, point, atom) => {
-        const [workspaceCwd, fieldName, fieldValue] = atom.args;
+const tauModule = new pl.type.Module(`constraints`, {
+  [`workspace_field/3`]: (thread, point, atom) => {
+    const [workspaceCwd, fieldName, fieldValue] = atom.args;
 
-        if (!is_atom(workspaceCwd) || !is_atom(fieldName)) {
-          thread.throwError(pl.error.instantiation(atom.indicator));
-          return;
-        }
+    if (!is_atom(workspaceCwd) || !is_atom(fieldName)) {
+      thread.throwError(pl.error.instantiation(atom.indicator));
+      return;
+    }
 
-        const workspace =
-            getProject(thread).tryWorkspaceByCwd(workspaceCwd.id);
+    const project = getProject(thread);
+    const workspace = project.tryWorkspaceByCwd(workspaceCwd.id);
 
-        if (workspace == null)
-          // Workspace not found => this predicate can never match
-          // We might want to throw here? We can be pretty sure the user did
-          // something wrong at this point
-          return;
+    // Workspace not found => this predicate can never match
+    // We might want to throw here? We can be pretty sure the user did
+    // something wrong at this point
+    if (workspace == null)
+      return;
 
-        const manifest: {[key: string]: any} = workspace.manifest.raw!;
+    const manifest: {[key: string]: any} = workspace.manifest.raw!;
 
-        if (!(fieldName.id in manifest))
-          // Field is not present => this predicate can never match
-          return;
+    // Field is not present => this predicate can never match
+    if (!(fieldName.id in manifest))
+      return;
 
-        prependGoals(thread, point, [new pl.type.Term('=', [
-                       fieldValue,
-                       new pl.type.Term(String((manifest)[fieldName.id])),
-                     ])]);
-      },
-    },
-    [
-      `workspace_field/3`,
-    ]);
+    prependGoals(thread, point, [new pl.type.Term('=', [
+      fieldValue,
+      new pl.type.Term(String((manifest)[fieldName.id])),
+    ])]);
+  },
+}, [
+  `workspace_field/3`,
+]);
 
 export function linkProjectToSession(session: pl.type.Session, project: Project) {
   projects.set(session, project);
