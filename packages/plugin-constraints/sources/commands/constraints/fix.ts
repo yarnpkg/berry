ee40f0dd7d95fec73170b36a19a884b5b15e1965
Maël Nison <mael@fb.com>
2019-04-18 15:13:54 +0100

    Fixes the fix command

--- packages/plugin-constraints/sources/commands/constraints/fix.ts
+++ packages/plugin-constraints/sources/commands/constraints/fix.ts
@@ -84,6 +84,8 @@ export default (clipanion: any, pluginConfiguration: PluginConfiguration) => cli
           }
         }
       }
+
+      await workspace.persistManifest();
     }
 
     if (result.invalidDependencies) {
