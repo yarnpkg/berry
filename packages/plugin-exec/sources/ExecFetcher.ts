d67b76882104c9ca228769e4bd61da0af2758c28
Maël Nison <mael@fb.com>
2019-04-19 01:12:45 +0100

    Implements the exec plugin

---
+++ packages/plugin-exec/sources/ExecFetcher.ts
@@ -0,0 +1,107 @@
+import {Fetcher, FetchOptions, MinimalFetchOptions}               from '@berry/core';
+import {Locator, MessageName}                                     from '@berry/core';
+import {execUtils, miscUtils, scriptUtils, structUtils, tgzUtils} from '@berry/core';
+import {NodeFS, xfs}                                              from '@berry/fslib';
+import {posix}                                                    from 'path';
+import querystring                                                from 'querystring';
+import {dirSync}                                                  from 'tmp';
+
+import {PROTOCOL}                                                 from './constants';
+
+export class ExecFetcher implements Fetcher {
+  supports(locator: Locator, opts: MinimalFetchOptions) {
+    if (!locator.reference.startsWith(PROTOCOL))
+      return false;
+
+    return true;
+  }
+
+  getLocalPath(locator: Locator, opts: FetchOptions) {
+    const {parentLocator, filePath} = this.parseLocator(locator);
+
+    if (posix.isAbsolute(filePath))
+      return filePath;
+
+    const parentLocalPath = opts.fetcher.getLocalPath(parentLocator, opts);
+
+    if (parentLocalPath !== null) {
+      return posix.resolve(parentLocalPath, filePath);
+    } else {
+      return null;
+    }
+  }
+
+  async fetch(locator: Locator, opts: FetchOptions) {
+    const expectedChecksum = opts.checksums.get(locator.locatorHash) || null;
+
+    const [packageFs, releaseFs, checksum] = await opts.cache.fetchPackageFromCache(
+      locator,
+      expectedChecksum,
+      async () => {
+        opts.report.reportInfoOnce(MessageName.FETCH_NOT_CACHED, `${structUtils.prettyLocator(opts.project.configuration, locator)} can't be found in the cache and will be fetched from the disk`);
+        return await this.fetchFromDisk(locator, opts);
+      },
+    );
+
+    return {
+      packageFs,
+      releaseFs,
+      prefixPath: `/sources`,
+      localPath: this.getLocalPath(locator, opts),
+      checksum,
+    };
+  }
+
+  private async fetchFromDisk(locator: Locator, opts: FetchOptions) {
+    const {parentLocator, filePath} = this.parseLocator(locator);
+
+    // If the file target is an absolute path we can directly access it via its
+    // location on the disk. Otherwise we must go through the package fs.
+    const parentFetch = posix.isAbsolute(filePath)
+      ? {packageFs: new NodeFS(), prefixPath: `/`, localPath: `/`}
+      : await opts.fetcher.fetch(parentLocator, opts);
+
+    // If the package fs publicized its "original location" (for example like
+    // in the case of "file:" packages), we use it to derive the real location.
+    const effectiveParentFetch = parentFetch.localPath
+      ? {packageFs: new NodeFS(), prefixPath: parentFetch.localPath}
+      : parentFetch;
+
+    // Discard the parent fs unless we really need it to access the files
+    if (parentFetch !== effectiveParentFetch && parentFetch.releaseFs)
+      parentFetch.releaseFs();
+
+    const sourceFs = effectiveParentFetch.packageFs;
+    const sourcePath = posix.resolve(effectiveParentFetch.prefixPath, filePath);
+
+    const cwd = NodeFS.toPortablePath(dirSync().name);
+    const env = await scriptUtils.makeScriptEnv(opts.project);
+
+    // Execute the specified script in the temporary directory
+    await execUtils.execvp(process.execPath, [sourcePath], {cwd, env});
+
+    if (!xfs.existsSync(`${cwd}/build`))
+      throw new Error(`The script should have generated a build directory`);
+
+    return await tgzUtils.makeArchiveFromDirectory(`${cwd}/build`, {
+      prefixPath: `/sources`,
+    });
+  }
+
+  private parseLocator(locator: Locator) {
+    const qsIndex = locator.reference.indexOf(`?`);
+
+    if (qsIndex === -1)
+      throw new Error(`Invalid file-type locator`);
+
+    const filePath = posix.normalize(locator.reference.slice(PROTOCOL.length, qsIndex));
+    const queryString = querystring.parse(locator.reference.slice(qsIndex + 1));
+
+    if (typeof queryString.locator !== `string`)
+      throw new Error(`Invalid file-type locator`);
+
+    const parentLocator = structUtils.parseLocator(queryString.locator, true);
+
+    return {parentLocator, filePath};
+  }
+}
--- scripts/update-local.sh
+++ scripts/update-local.sh
@@ -16,6 +16,7 @@ make_build() {
 }
 
 make_build build:cli berry-cli berry.js
+make_build build:plugin-exec plugin-exec @berry/plugin-exec.js
 make_build build:plugin-pack plugin-pack @berry/plugin-pack.js
 make_build build:plugin-stage plugin-stage @berry/plugin-stage.js
 make_build build:plugin-typescript plugin-typescript @berry/plugin-typescript.js
