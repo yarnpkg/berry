cc18a032bc5eb22883a8ffc771a2dc9fe9407edc
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-04-19 15:55:22 +0300

    Add PnP API cached loader impl

---
+++ packages/berry-pnpify/sources/PnPApiLoader.ts
@@ -0,0 +1,93 @@
+import EventEmitter from 'events';
+import fs from 'fs';
+
+/**
+ * PnP API locator options
+ */
+export interface PnPApiLoaderOptions {
+  /**
+   * Uncached require function
+   *
+   * Default: `delete require.cache[...]; require(..)`
+   *
+   * @param modulePath modulePath
+   */
+  uncachedRequire?: (modulePath: string) => boolean;
+
+  /**
+   * Watch for PnP API file changes
+   *
+   * Default: `fs.watch`
+   */
+  watch?: (filePath: string, listener: (event: string, filename: string) => any) => any;
+}
+
+interface DefinedPnPApiLoaderOptions {
+  uncachedRequire: (modulePath: string) => boolean;
+  watch: (filePath: string, listener: (event: string, filename: string) => any) => any;
+}
+
+interface CacheEntry {
+  pnpApi: any;
+  watched: boolean;
+}
+
+/**
+ * Loads PnP API from the PnP API path in a cached way, then
+ * watches for changes to this file and if any - invalidates cache
+ * and emits an event
+ */
+export class PnPApiLoader extends EventEmitter {
+  private cachedApis: { [filePath: string]: CacheEntry } = {};
+  private options: DefinedPnPApiLoaderOptions;
+
+  /**
+   * Constructs new instance of PnP API loader
+   *
+   * @param options optional loader options
+   */
+  constructor(options?: PnPApiLoaderOptions) {
+    super();
+    const opts = options || {};
+    this.options = {
+      uncachedRequire: opts.uncachedRequire || ((modulePath: string): any => {
+        delete require.cache[require.resolve(modulePath)];
+        return require(modulePath);
+      }),
+      watch: opts.watch || fs.watch.bind(fs)
+    };
+  }
+
+  /**
+   * Requires and returns PnP API in a cached way and then
+   * watches for PnP API file changes.
+   *
+   * If changes to PnP API file will be detected next call to this function
+   * will reload PnP API afresh and returns it.
+   *
+   * On PnP API file change watch event will be emitted on this class
+   *
+   * @param pnpApiPath path to PnP API file
+   *
+   * @returns `pnpapi` instance for the given PnP API file
+   */
+  getApi(pnpApiPath: string) {
+    const cacheEntry = this.cachedApis[pnpApiPath] || {};
+    if (!cacheEntry.pnpApi) {
+      cacheEntry.pnpApi = this.options.uncachedRequire(pnpApiPath);
+      if (!cacheEntry.watched) {
+        this.options.watch(pnpApiPath, (event: string, filename: string) => {
+          delete cacheEntry.pnpApi;
+          this.emit(event, filename);
+        });
+        cacheEntry.watched = true;
+      }
+    }
+
+    if (cacheEntry.watched) {
+      this.cachedApis[pnpApiPath] = cacheEntry;
+    }
+
+    return cacheEntry.pnpApi;
+  }
+}
---
+++ packages/berry-pnpify/tests/PnPApiLoader.test.ts
@@ -0,0 +1,64 @@
+import { PnPApiLoader } from '../sources/PnPApiLoader';
+
+describe('PnPApiLoader should', () => {
+  let mockWatch: jest.Mock;
+  let mockUncachedRequire: jest.Mock;
+  let loader;
+
+  beforeEach(() => {
+    mockWatch = jest.fn();
+    mockUncachedRequire = jest.fn().mockReturnValue({});
+    loader = new PnPApiLoader({
+      watch: mockWatch,
+      uncachedRequire: mockUncachedRequire
+    });
+  });
+
+  it('loads PnP API and starts watcher', () => {
+    loader.getApi('/home/user/pnp_project/.pnp.js');
+    expect(mockWatch).toHaveBeenCalledTimes(1);
+    expect(mockUncachedRequire).toHaveBeenCalledTimes(1);
+  });
+
+  it('loads PnP API in a cached way', () => {
+    loader.getApi('/home/user/pnp_project/.pnp.js');
+    loader.getApi('/home/user/pnp_project/.pnp.js');
+    expect(mockWatch).toHaveBeenCalledTimes(1);
+    expect(mockUncachedRequire).toHaveBeenCalledTimes(1);
+  });
+
+  it('emits watch event on file change', () => {
+    mockWatch = jest.fn().mockImplementation((filename, callback) => {
+      callback('change', filename);
+    });
+    loader = new PnPApiLoader({
+      watch: mockWatch,
+      uncachedRequire: mockUncachedRequire
+    });
+    const filePath = '/home/user/pnp_project/.pnp.js';
+    const mockListener = jest.fn();
+    loader.on('change', mockListener);
+    loader.getApi(filePath);
+    expect(mockWatch).toHaveBeenCalledTimes(1);
+    expect(mockUncachedRequire).toHaveBeenCalledTimes(1);
+    expect(mockListener).toHaveBeenCalledWith(filePath);
+  });
+
+  it('requires api again on file change', () => {
+    mockWatch = jest.fn().mockImplementation((filename, callback) => {
+      callback('change', filename);
+    });
+    loader = new PnPApiLoader({
+      watch: mockWatch,
+      uncachedRequire: mockUncachedRequire
+    });
+    const filePath = '/home/user/pnp_project/.pnp.js';
+    const mockListener = jest.fn();
+    loader.on('change', mockListener);
+    loader.getApi(filePath);
+    loader.getApi(filePath);
+    expect(mockWatch).toHaveBeenCalledTimes(1);
+    expect(mockUncachedRequire).toHaveBeenCalledTimes(2);
+    expect(mockListener).toHaveBeenCalledWith(filePath);
+  });
+});
