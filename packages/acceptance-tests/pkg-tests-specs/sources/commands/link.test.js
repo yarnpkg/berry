4f2de7c224f928f0ec8390a647890881bf1a236f
Maël Nison <nison.mael@gmail.com>
2019-04-29 17:21:45 +0200

    Changes yarn link syntax (#98)

--- packages/acceptance-tests/pkg-tests-specs/sources/commands/link.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/commands/link.test.js
@@ -1,30 +1,150 @@
 import {NodeFS} from '@berry/fslib';
 
 const {
-  fs: {createTemporaryFolder, mkdirp, readJson},
+  fs: {createTemporaryFolder, mkdirp, readJson, writeJson},
 } = require('pkg-tests-core');
 
 describe(`Commands`, () => {
   describe(`link`, () => {
     test(
-      `it should work with the classic link workflow`,
+      `it should allow to link a project with another one`,
       makeTemporaryEnv({}, async ({path, run, source}) => {
         const tmp = await createTemporaryFolder();
-        await mkdirp(`${tmp}/my-package`);
 
-        await run(`init`, {
-          cwd: `${tmp}/my-package`,
+        await writeJson(`${tmp}/my-package/package.json`, {
+          name: `my-package`,
         });
 
-        await run(`link`, {
-          cwd: `${tmp}/my-package`,
-        });
-
-        await run(`link`, `my-package`);
+        await run(`link`, `${tmp}/my-package`);
 
         await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
-          dependencies: {
+          resolutions: {
             [`my-package`]: `portal:${NodeFS.toPortablePath(`${tmp}/my-package`)}`,
+          },
+        });
+      }),
+    );
+
+    test(
+      `it should add the resolution override to the top-level workspace`,
+      makeTemporaryEnv(
+        {
+          private: true,
+          workspaces: [`packages/*`],
+        },
+        async ({path, run, source}) => {
+          const tmp = await createTemporaryFolder();
+
+          await writeJson(`${tmp}/my-package/package.json`, {
+            name: `my-package`,
+          });
+    
+          await writeJson(`${path}/packages/workspace/package.json`, {
+            name: `workspace`,
+          });
+
+          await run(`link`, `${tmp}/my-package`, {
+            cwd: `${path}/packages/workspace`,
+          });
+
+          await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+            resolutions: {
+              [`my-package`]: `portal:${NodeFS.toPortablePath(`${tmp}/my-package`)}`,
+            },
+          });
+        },
+      ),
+    );
+
+    test(
+      `it should allow to link all workspaces from another project`,
+      makeTemporaryEnv({}, async ({path, run, source}) => {
+        const tmp = await createTemporaryFolder();
+
+        await writeJson(`${tmp}/my-workspace/package.json`, {
+          private: true,
+          workspaces: [`packages/*`],
+        });
+
+        await writeJson(`${tmp}/my-workspace/packages/workspace-a/package.json`, {
+          name: `workspace-a`,
+        });
+
+        await writeJson(`${tmp}/my-workspace/packages/workspace-b/package.json`, {
+          name: `workspace-b`,
+        });
+
+        await run(`link`, `${tmp}/my-workspace`, `--all`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          resolutions: {
+            [`workspace-a`]: `portal:${NodeFS.toPortablePath(`${tmp}/my-workspace/packages/workspace-a`)}`,
+            [`workspace-b`]: `portal:${NodeFS.toPortablePath(`${tmp}/my-workspace/packages/workspace-b`)}`,
+          },
+        });
+      }),
+    );
+
+    test(
+      `it should not link the private workspaces by default`,
+      makeTemporaryEnv({}, async ({path, run, source}) => {
+        const tmp = await createTemporaryFolder();
+
+        await writeJson(`${tmp}/my-workspace/package.json`, {
+          private: true,
+          workspaces: [`packages/*`],
+        });
+
+        await writeJson(`${tmp}/my-workspace/packages/workspace-a/package.json`, {
+          name: `workspace-a`,
+        });
+
+        await writeJson(`${tmp}/my-workspace/packages/workspace-b/package.json`, {
+          name: `workspace-b`,
+          private: true,
+        });
+
+        await run(`link`, `${tmp}/my-workspace`, `--all`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          resolutions: {
+            [`workspace-a`]: `portal:${NodeFS.toPortablePath(`${tmp}/my-workspace/packages/workspace-a`)}`,
+          },
+        });
+
+        await expect(readJson(`${path}/package.json`)).resolves.not.toMatchObject({
+          resolutions: {
+            [`workspace-b`]: expect.anything(),
+          },
+        });
+      }),
+    );
+
+    test(
+      `it should link the private workspaces if the right flag is used`,
+      makeTemporaryEnv({}, async ({path, run, source}) => {
+        const tmp = await createTemporaryFolder();
+
+        await writeJson(`${tmp}/my-workspace/package.json`, {
+          private: true,
+          workspaces: [`packages/*`],
+        });
+
+        await writeJson(`${tmp}/my-workspace/packages/workspace-a/package.json`, {
+          name: `workspace-a`,
+        });
+
+        await writeJson(`${tmp}/my-workspace/packages/workspace-b/package.json`, {
+          name: `workspace-b`,
+          private: true,
+        });
+
+        await run(`link`, `${tmp}/my-workspace`, `--all`, `--private`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          resolutions: {
+            [`workspace-a`]: `portal:${NodeFS.toPortablePath(`${tmp}/my-workspace/packages/workspace-a`)}`,
+            [`workspace-b`]: `portal:${NodeFS.toPortablePath(`${tmp}/my-workspace/packages/workspace-b`)}`,
           },
         });
       }),
--- packages/berry-parsers/sources/resolution.ts
+++ packages/berry-parsers/sources/resolution.ts
@@ -1,11 +1,42 @@
 import {parse} from './grammars/resolution';
 
-export function parseResolution(source: string) {
+export type Resolution = {
+  from?: {
+    fullName: string,
+    description?: string,
+  },
+  descriptor: {
+    fullName: string,
+    description?: string,
+  },
+};
+
+export function parseResolution(source: string): Resolution {
   try {
-    return parse(source);
+    return parse(source) as Resolution;
   } catch (error) {
     if (error.location)
       error.message = error.message.replace(/(\.)?$/, ` (line ${error.location.start.line}, column ${error.location.start.column})$1`);
     throw error;
   }
+}
+
+export function stringifyResolution(resolution: Resolution) {
+  let str = ``;
+
+  if (resolution.from) {
+    str += resolution.from.fullName;
+
+    if (resolution.from.description)
+      str += `@${resolution.from.description}`;
+
+    str += `/`;
+  }
+
+  str += resolution.descriptor.fullName;
+
+  if (resolution.descriptor.description)
+    str += `@${resolution.descriptor.description}`;
+
+  return str;
 }
