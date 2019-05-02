e4cd862e3470e9b26994e3cdeb04def44daf293c
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-05-01 21:08:35 -0500

    Adds tests for plugin-typescript

---
+++ packages/acceptance-tests/pkg-tests-specs/sources/plugins/plugin-typescript.test.js
@@ -0,0 +1,75 @@
+const {
+  fs: {readJson, writeFile}
+} = require('pkg-tests-core');
+
+describe(`Plugins`, () => {
+  describe(`typescript`, () => {
+    test(
+      `it should automatically add @types to development`,
+      makeTemporaryEnv({}, async ({ path, run, source }) => {
+        await writeFile(`${path}/.yarnrc`, `plugins:\n  - ${JSON.stringify(require.resolve(`@berry/monorepo/scripts/plugin-typescript.js`))}\n`);
+        await run(`add`, `no-deps`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          dependencies: {
+            [`no-deps`]: `^2.0.0`
+          },
+          devDependencies: {
+            [`@types/no-deps`]: `1.0.0`
+          }
+        });
+      })
+    );
+
+    test(
+      `it should automatically add @types for scoped packages`,
+      makeTemporaryEnv({}, async ({ path, run, source }) => {
+        await writeFile(`${path}/.yarnrc`, `plugins:\n  - ${JSON.stringify(require.resolve(`@berry/monorepo/scripts/plugin-typescript.js`))}\n`);
+        await run(`add`, `@scoped/package`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          dependencies: {
+            [`@scoped/package`]: `^1.0.0`
+          },
+          devDependencies: {
+            [`@types/scoped__package`]: `1.0.0`
+          },
+        });
+      })
+    );
+
+    test(
+      `it should automatically remove @types from development`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`no-deps`]: `^2.0.0`
+        },
+        devDependencies: {
+          [`@types/no-deps`]: `2.0.0`
+        },
+      }, async ({ path, run, source }) => {
+        await writeFile(`${path}/.yarnrc`, `plugins:\n  - ${JSON.stringify(require.resolve(`@berry/monorepo/scripts/plugin-typescript.js`))}\n`);
+        await run(`remove`, `no-deps`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.not.toHaveProperty(`devDependencies.@types/no-deps`);
+      })
+    );
+
+    test(
+      `it should automatically remove @types for scoped packages`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`@scoped/package`]: `^1.0.0`
+        },
+        devDependencies: {
+          [`@types/scoped__package`]: `1.0.0`
+        },
+      }, async ({ path, run, source }) => {
+        await writeFile(`${path}/.yarnrc`, `plugins:\n  - ${JSON.stringify(require.resolve(`@berry/monorepo/scripts/plugin-typescript.js`))}\n`);
+        await run(`remove`, `@scoped/package`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.not.toHaveProperty(`devDependencies.@types/scoped__package`);
+      })
+    );
+  });
+});
