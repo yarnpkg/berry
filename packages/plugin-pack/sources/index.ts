c751df9e5aab018adcd5815dc2e20354a2e51cf7
Bram Gotink <bram@gotink.me>
2019-05-12 14:37:41 +0200

    Replace workspace: with a version number at package time (#155)
    * Replace workspace: with a version number at package time
    
    * Update workspaces.md
    
    * Add descriptor to error message
    
    Co-Authored-By: Maël Nison <nison.mael@gmail.com>
    
    * Run eslint

--- packages/plugin-pack/sources/index.ts
+++ packages/plugin-pack/sources/index.ts
@@ -1,7 +1,8 @@
-import {Plugin, Project, Workspace}   from '@berry/core';
+import {Plugin, Project, Workspace, structUtils}   from '@berry/core';
+import {MessageName, ReportError}                  from '@berry/core';
 
-import pack                           from './commands/pack';
-import * as packUtils                 from './packUtils';
+import pack                                        from './commands/pack';
+import * as packUtils                              from './packUtils';
 
 export {packUtils};
 
@@ -17,6 +18,9 @@ export interface Hooks {
   ) => Promise<void>|void;
 }
 
+const DEPENDENCY_TYPES = [`dependencies`, `devDependencies`, `peerDependencies`];
+const WORKSPACE_PROTOCOL = `workspace:`;
+
 const beforeWorkspacePacking = (workspace: Workspace, rawManifest: any) => {
   if (rawManifest.publishConfig) {
     if (rawManifest.publishConfig.main)
@@ -24,6 +28,39 @@ const beforeWorkspacePacking = (workspace: Workspace, rawManifest: any) => {
 
     if (rawManifest.publishConfig.module) {
       rawManifest.module = rawManifest.publishConfig.module;
+    }
+  }
+
+  const project = workspace.project;
+
+  for (const dependencyType of DEPENDENCY_TYPES) {
+    for (const [identHash, descriptor] of workspace.manifest.getForScope(dependencyType)) {
+      const matchingWorkspaces = project.findWorkspacesByDescriptor(descriptor);
+      const range = structUtils.parseRange(descriptor.range);
+
+      if (range.protocol !== WORKSPACE_PROTOCOL)
+        continue;
+
+      if (matchingWorkspaces.length === 0) {
+        if (project.workspacesByIdent.has(identHash)) {
+          throw new ReportError(MessageName.WORKSPACE_NOT_FOUND, `${structUtils.prettyDescriptor(project.configuration, descriptor)}: No local workspace found for this range`);
+        }
+      } else if (matchingWorkspaces.length > 1) {
+        throw new ReportError(MessageName.TOO_MANY_MATCHING_WORKSPACES, `${structUtils.prettyDescriptor(project.configuration, descriptor)}: Too many workspaces match this range, please disambiguate`);
+      } else {
+        const [matchingWorkspace] = matchingWorkspaces;
+        let versionToWrite: string;
+
+        // For workspace:path/to/workspace and workspace:* we look up the workspace version
+        if (structUtils.areDescriptorsEqual(descriptor, matchingWorkspace.anchoredDescriptor) || range.selector === `*`)
+          versionToWrite = matchingWorkspace.manifest.version!;
+        else
+          // for workspace:version we simply strip the protocol
+          versionToWrite = range.selector;
+
+
+        rawManifest[dependencyType][structUtils.stringifyIdent(descriptor)] = versionToWrite;
+      }
     }
   }
 }
