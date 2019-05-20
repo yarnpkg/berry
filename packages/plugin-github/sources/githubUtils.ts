57477a8cdfbcc1bd51858db9001f2f44e9d30353
Maël Nison <nison.mael@gmail.com>
2019-05-20 19:59:19 +0200

    Update githubUtils.ts

--- packages/plugin-github/sources/githubUtils.ts
+++ packages/plugin-github/sources/githubUtils.ts
@@ -1,10 +1,16 @@
+type ParsedGithubUrl = {
+  username: string;
+  reponame: string;
+  branch?: string;
+};
 
 const githubPatterns = [
   /^github:([^\/#]+)\/([^\/#]+)(?:#(.+))?$/,
-  /^(?:git@github.com:|git:\/\/github\.com)\/?([^\/#]+)\/([^\/#]+)\.git(?:#(.+))?$/,
+  /^git@github.com:([^\/#]+)\/([^\/#]+)\.git(?:#(.+))?$/,
+  /^git:\/\/github\.com\/([^\/#]+)\/([^\/#]+)\.git(?:#(.+))?$/,
   /^git\+https:\/\/github.com\/([^\/#]+)\/([^\/#]+)\.git?(?:#(.+))?$/,
-  /^https:\/\/github.com\/([^\/#]+)\/([^\/#]+)\/tarball\/([^\/#]+)(?:#|$)/,
-  /^(?:http|https):\/\/.*github.com\/([^\/#]+)\/([^\/#]+)\.git?(?:#(.+))?$/,
+  /^https?:\/\/(?:[^@]+@)?github.com\/([^\/#]+)\/([^\/#]+)\/tarball\/([^\/#]+)(?:#|$)/,
+  /^https?:\/\/(?:[^@]+@)?github.com\/([^\/#]+)\/([^\/#]+)(?:\.git)?(?:#(.+))?$/,
 ];
 
 /**
@@ -14,35 +20,20 @@ export function isGithubUrl(url: string): boolean {
   return url ? githubPatterns.some(pattern => !!url.match(pattern)) : false;
 }
 
-const getBranch = (parsedBranch: string): string | undefined => {
-  return parsedBranch ? parsedBranch : undefined;
-}
-
-interface ParsedGithubUrl {
-  username: string;
-  reponame: string;
-  branch?: string;
-}
-
 /**
  * Takes a valid github repository url and parses it, returning
  * an object of type `ParsedGithubUrl`
  */
 export function parseGithubUrl(urlStr: string): ParsedGithubUrl {
-  let match: Array<string> | null = null;
-
-  for (const pattern of githubPatterns) {
-    match = urlStr.match(pattern);
-    if (match) {
-      break;
-    }
-  }
+  let match = githubPatterns.find(pattern => {
+    return urlStr.match(pattern);
+  });
 
   if (!match)
     throw new Error(invalidGithubUrlMessage(urlStr));
 
   const [, username, reponame, branch] = match;
-  return {username, reponame, branch: getBranch(branch)};
+  return {username, reponame, branch};
 }
 
 export function invalidGithubUrlMessage(url: string): string {
