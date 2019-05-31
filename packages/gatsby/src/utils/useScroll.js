80ed2dc269917f43830d7eafff65f8fd231bfb76
Andrew Lee <and.rob.lee@gmail.com>
2019-05-30 18:29:11 -0700

    Maintain sidebar scroll from page to page

---
+++ packages/gatsby/src/utils/useScroll.js
@@ -0,0 +1,32 @@
+import { useLayoutEffect, useRef } from "react";
+
+const useScroll = id => {
+  const ref = useRef();
+
+  const handleScroll = () => {
+    setBrowserStorage(id ,ref.current.scrollTop);
+  };
+
+  const readBrowserStorage = id => {
+    return sessionStorage.getItem(`gatsby:sidebar:${id}`);
+  }
+
+  const setBrowserStorage = (id ,pos) => {
+    sessionStorage.setItem(`gatsby:sidebar:${id}`, pos.toString());
+  }
+
+  // initial render
+  useLayoutEffect(() => {
+    const initPos = readBrowserStorage(id);
+    ref.current.scrollTop = initPos == null ? 0 : parseInt(initPos, 10);
+  }, []);
+
+  useLayoutEffect(() => {
+    ref.current.addEventListener("scroll", handleScroll);
+    return () => ref.current.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return ref;
+};
+
+export default useScroll;
