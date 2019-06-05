7418ba1d33e700b04a266d541cdf8507479b1b94
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-06-05 07:25:27 +0300

    Fix lint errors

--- packages/plugin-essentials/sources/commands/help.ts
+++ packages/plugin-essentials/sources/commands/help.ts
@@ -4,8 +4,8 @@ export default (clipanion: any) => clipanion
   .command(`help [...command]`)
   .describe(`print the program usage`)
 
-   .action(async ({command, ...rest}: {command: Array<string>, rest: {[key: string]: any}}) => {
-     if (command.length > 0) {
+  .action(async ({command, ...rest}: {command: Array<string>, rest: {[key: string]: any}}) => {
+    if (command.length > 0) {
       return await clipanion.run(null, [...command, `-h`], rest);
     } else {
       return await clipanion.run(null, [`-h`], rest);
