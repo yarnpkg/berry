312b37753d3b7eba0c7cd16e857a67a7f5b8de42
Maël Nison <nison.mael@gmail.com>
2019-06-13 19:13:06 +0200

    Prevents crashes when link: points to a folder without package.json (#232)

---
+++ packages/acceptance-tests/pkg-tests-specs/sources/protocols/links.test.js
@@ -0,0 +1,64 @@
+const {xfs} = require(`@berry/fslib`);
+const {
+  fs: {createTemporaryFolder, readJson, writeJson},
+  tests: {getPackageDirectoryPath},
+} = require('pkg-tests-core');
+
+describe(`Protocols`, () => {
+  describe(`portal:`, () => {
+    test(
+      `it should link a remote location into the current dependency tree`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`no-deps`]: getPackageDirectoryPath(`no-deps`, `1.0.0`).then(dirPath => `link:${dirPath}`),
+        },
+      }, async ({path, run, source}) => {
+        await run(`install`);
+
+        await expect(source(`require('no-deps/package.json')`)).resolves.toMatchObject({
+          name: `no-deps`,
+          version: `1.0.0`,
+        });
+      }),
+    );
+
+    test(
+      `it should ignore links' own dependencies`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`one-fixed-dep`]: getPackageDirectoryPath(`one-fixed-dep`, `1.0.0`).then(dirPath => `link:${dirPath}`),
+        },
+      }, async ({path, run, source}) => {
+        await run(`install`);
+
+        await expect(source(`{ try { require('one-fixed-dep') } catch (error) { return error.code } }`)).resolves.toEqual(`UNDECLARED_DEPENDENCY`);
+      }),
+    );
+
+    test(
+      `it should work even when the target doesn't have a manifest`,
+      makeTemporaryEnv(
+        {},
+        async ({path, run, source}) => {
+          const tmp = await createTemporaryFolder();
+
+          await writeJson(`${tmp}/data.json`, {
+            data: 42,
+          });
+
+          await writeJson(`${path}/package.json`, {
+            dependencies: {
+              [`foo`]: `link:${tmp}`,
+            },
+          });
+
+          await run(`install`);
+
+          await expect(source(`require('foo/data.json')`)).resolves.toMatchObject({
+            data: 42,
+          });
+        },
+      ),
+    );
+  });
+});
