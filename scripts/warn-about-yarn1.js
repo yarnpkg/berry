97cec78b182cc22796e87c0c9fd803345995fd83
Maël Nison <nison.mael@gmail.com>
2019-06-21 14:40:14 +0200

    Adds more information when the binary is wrong

--- scripts/warn-about-yarn1.js
+++ scripts/warn-about-yarn1.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
-console.log("Your global Yarn binary isn't recent enough; please upgrade to 1.17.2 or higher.");
+const {execSync} = require(`child_process`);
+
+console.log(`Your global Yarn binary isn't recent enough; please upgrade to 1.17.2 or higher.`);
+console.log(`Binary: ${execSync(`which yarn`).toString().trim()} (${execSync(`yarn --version`).toString().trim()})`);
 
 process.exitCode = 1;
