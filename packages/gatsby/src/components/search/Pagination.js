d14d6e2b26423b23044a054c2275a51ea0bfb75e
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-07-05 19:17:07 +0300

    Add pagination styling

--- packages/gatsby/src/components/search/Pagination.js
+++ packages/gatsby/src/components/search/Pagination.js
@@ -1,14 +1,66 @@
-import React                                   from 'react';
-import {Pagination}                            from 'react-instantsearch-dom';
+import React        from 'react';
+import {Pagination} from 'react-instantsearch-dom';
+import styled       from '@emotion/styled';
 
-const Pag = ({ pagination }) => (
-  <div className="d-flex">
+const PaginationContainer = styled.div`
+  height: 3rem;
+  margin: 2em auto;
+  text-align: center;
+
+  ul {
+    display: inline-block;
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    float: left;
+  }
+
+  .ais-Pagination-item--disabled {
+    visibility: visible;
+    opacity: 0.3;
+    cursor: default;
+  }
+
+  .ais-Pagination-link--selected {
+    color: #fff;
+    background-color: #2c8ebb;
+    border-color: #2c8ebb;
+    cursor: default;
+  }
+
+  a,  .ais-Pagination-item--disabled span {
+    padding: .5rem .75rem;
+    margin-left: -1px;
+    line-height: 1.25;
+    color: #117cad;
+    background-color: #fff;
+    border: 1px solid #ddd;
+ }
+
+ a.ais-Pagination-link:hover {
+    &.ais-Pagination-link--selected:hover {
+      color: #fff;
+      cursor: default;
+      background-color: #2c8ebb;
+      border-color: #2c8ebb;
+    }
+    color: #0a4a67;
+    text-decoration: none;
+    background-color: #eceeef;
+    border-color: #ddd;
+  }
+`;
+
+const PaginationComponent = ({ pagination }) => (
+  <PaginationContainer>
     {pagination ? (
       <Pagination showFirst={false} showLast={false} scrollTo={true} />
     ) : (
       <div style={{ height: '3rem' }} />
     )}
-  </div>
+  </PaginationContainer>
 );
 
-export default Pag;
+export default PaginationComponent;
