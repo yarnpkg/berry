0a5166c802ec9babc3cc75d916ccbcaf05ab10cc
Jeremy Albright <1935258+Js-Brecht@users.noreply.github.com>
2019-08-02 11:08:54 -1000

    Added redirect+response hook
    During tarball resolution, some redirects would return 400 error if authentication headers are present.  These hooks will intercept a 400 error and remove the authorization header.

--- packages/berry-core/sources/httpUtils.ts
+++ packages/berry-core/sources/httpUtils.ts
@@ -7,6 +7,17 @@ import {Configuration}         from './Configuration';
 
 const cache = new Map<string, Promise<Buffer>>();
 
+/**
+ * Populated when a request is redirected, to save time on retries
+ * string: The domain name that was redirected to.
+ * RedirectRequires: Various request modifications that must be made
+ *  for the redirected request to be made successfully.
+ */
+interface RedirectRequires {
+  noAuth: boolean;
+}
+const redirectCache = new Map<string, RedirectRequires>();
+
 const globalHttpAgent = new HttpAgent();
 const globalHttpsAgent = new HttpsAgent();
 
@@ -59,8 +70,66 @@ async function request(target: string, body: Body, {configuration, headers, json
       ? tunnel.httpsOverHttp(parseProxy(httpsProxy))
       : globalHttpsAgent;
 
+  // Track if the request is redirected.  If it is, modifications to the request
+  // might be necessary
+  let didRedirect = false;
+  let redirHost: string;
+
+  const gotOptions = {agent, body, headers, json, method, encoding: null}
+
+  //@ts-ignore
+  const gotClient = got.extend({...gotOptions, hooks: {
+    beforeRedirect: [
+      //@ts-ignore
+      (options) => {
+        // Setting this flag here means that if this redirect is unsuccessful, we will cache
+        // the request modifications for the redirected domain after we get the response.
+        // Any time this domain gets hit with a redirect again, we will know the modifications
+        // to make before the request is made.
+        didRedirect = true;
+        // Is there ever a time the host will be null in a redirect?  `got` thinks so.
+        // I assume that if that were the case, this should probably provide some feedback
+        // to the user.
+        if (options.host) {
+          redirHost = options.host;
+          if (redirectCache.has(redirHost)) {
+            // We already know the key exists in the Map, so cast to the desired type.
+            const redirModReq = (redirectCache.get(redirHost) as RedirectRequires);
+            // This redirect URL requires there be no `Authorization:` header.
+            if (redirModReq.noAuth && options.headers && options.headers.authorization) {
+              delete options.headers.authorization;
+            }
+          }
+        }
+      },
+    ],
+    afterResponse: [
+      //@ts-ignore
+      (response, retryRequest) => {
+        // Only process the response if request was redirected
+        if (didRedirect) {
+          // Invalid request was made.  One way this has been caused is by an authorization
+          // header being included in the request to the redirect URL.
+          if (response.statusCode === 400) {
+            // No reason to process if there is no headers authorization already
+            // Only retrying if the options are being changed.  If they aren't, then
+            // we don't know why this request failed...
+            if (gotOptions.headers && gotOptions.headers.authorization) {
+              redirectCache.set(redirHost, {
+                noAuth: true,
+              });
+              return retryRequest(gotOptions)
+            }
+          }
+        }
+        return response;
+      },
+    ],
+  }});
+
   // @ts-ignore
-  const res = await got(target, {agent, body, headers, json, method, encoding: null});
+  // const res = await got(target, {agent, body, headers, json, method, encoding: null});
+  const res = await gotClient(target);
 
   return await res.body;
 }
