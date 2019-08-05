4a1a16e042308e06cac90735e5f0e4c186461018
Daniel Jankowski <daniell.jankowskii@gmail.com>
2019-08-05 07:17:18 +0200

    Use mktempPromise utility method to create a temp directory. Use strict: true param to handle the errors when executing git commands.

--- packages/plugin-git/sources/gitUtils.ts
+++ packages/plugin-git/sources/gitUtils.ts
@@ -1,35 +1,18 @@
-import {execUtils, Configuration}                     from '@berry/core';
-import {xfs, NodeFS, PortablePath, ppath, Filename}   from '@berry/fslib';
-
-const tmp = require('tmp');
+import {execUtils, Configuration}          from '@berry/core';
+import {xfs}                               from '@berry/fslib';
 
 export async function clone(repo: string, configuration: Configuration) {
   if (!configuration.get(`enableNetwork`))
     throw new Error(`Network access has been disabled by configuration (${repo})`);
 
-  const directory = await createTemporaryFolder()
-  const {code, stdout} = await execUtils.execvp(`git`, [`clone`, `${repo}`, `${directory}`], {
+  const directory = await xfs.mktempPromise()
+  await execUtils.execvp(`git`, [`clone`, `${repo}`, `${directory}`], {
     cwd: directory,
     env: {
       ...process.env,
       GIT_SSH_COMMAND: `ssh -o BatchMode=yes`,
-    }
+    },
+    strict: true
   });
-  // TODO: Support git clone errors
   return directory;
 }
-
-function createTemporaryFolder(): Promise<PortablePath> {
-  return new Promise<PortablePath>((resolve, reject) => {
-    tmp.dir({unsafeCleanup: true}, (error: Error, dirPath: PortablePath) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(dirPath);
-      }
-    });
-  }).then(async dirPath => {
-    dirPath = await xfs.realpathPromise(NodeFS.toPortablePath(dirPath));
-    return dirPath;
-  });
-};
