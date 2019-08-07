3609616054c7e136ebc3e1edc908d707a1355f8e
Maël Nison <nison.mael@gmail.com>
2019-08-07 01:48:53 -0700

    Fixes support for v-prefixed semver ranges

--- packages/acceptance-tests/pkg-tests-specs/sources/protocols/npm.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/protocols/npm.test.js
@@ -8,9 +8,26 @@ describe(`Protocols`, () => {
         },
         async ({path, run, source}) => {
           await run(`install`);
-  
+
           await expect(source(`require('no-deps')`)).resolves.toMatchObject({
             name: `one-fixed-dep`,
+            version: `1.0.0`,
+          });
+        },
+      ),
+    );
+
+    test(
+      `it should allow prefixing semver ranges with "v"`,
+      makeTemporaryEnv(
+        {
+          dependencies: {[`no-deps`]: `npm:v1.0.0`},
+        },
+        async ({path, run, source}) => {
+          await run(`install`);
+
+          await expect(source(`require('no-deps')`)).resolves.toMatchObject({
+            name: `no-deps`,
             version: `1.0.0`,
           });
         },
