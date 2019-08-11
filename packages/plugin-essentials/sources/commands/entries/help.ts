20b968301b72fd9bea9c401041ad68185ccf5187
Maël Nison <nison.mael@gmail.com>
2019-08-11 22:08:33 +0200

    Implements "yarn set version from sources" (#352)
    * Implements "yarn set version from sources"
    
    * Updates the documentation
    
    * Updates the documentation

--- packages/plugin-essentials/sources/commands/entries/help.ts
+++ packages/plugin-essentials/sources/commands/entries/help.ts
@@ -3,6 +3,7 @@ import {Command}        from 'clipanion';
 
 // eslint-disable-next-line arca/no-default-export
 export default class HelpCommand extends Command<CommandContext> {
+  @Command.Path(`help`)
   @Command.Path(`--help`)
   @Command.Path(`-h`)
   async execute() {
