a5df7f9087fc9fccff234a34c71f65423349f933
Maël Nison <nison.mael@gmail.com>
2019-08-16 13:20:52 +0200

    Improves the tests

--- packages/acceptance-tests/pkg-tests-specs/sources/protocols/git.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/protocols/git.test.js
@@ -1,169 +1,48 @@
 const {
-  fs: {readJson, readFile},
-  tests: {getPackageDirectoryPath},
+  fs: {readFile},
 } = require('pkg-tests-core');
 const {parseSyml} = require('@berry/parsers');
 
+const TESTED_URLS = {
+  // We've picked util-deprecate because it doesn't have any dependency, and thus
+  // doesn't crash when installing through our mock registry. We also could have
+  // made our own repository (and maybe we will), but it was simpler this way.
+  [`git://github.com/TooTallNate/util-deprecate.git#v1.0.1`]: {version: `1.0.1`},
+  [`git+ssh://git@github.com/TooTallNate/util-deprecate.git#v1.0.1`]: {version: `1.0.1`},
+  [`https://github.com/TooTallNate/util-deprecate.git#v1.0.0`]: {version: `1.0.0`},
+  [`https://github.com/TooTallNate/util-deprecate.git#master`]: {version: `1.0.2`},
+  [`https://github.com/TooTallNate/util-deprecate.git#b3562c2`]: {version: `1.0.0`},
+};
+
 describe(`Protocols`, () => {
   describe(`git:`, () => {
-    test(
-      `it should resolve a dependency with git tag via git protocol`,
-      makeTemporaryEnv(
-        {
-          dependencies: {[`util-deprecate`]: `git://github.com/TooTallNate/util-deprecate.git#v1.0.1`},
-        },
-        async ({path, run, source}) => {
-          await run(`install`);
+    for (const [url, {version}] of Object.entries(TESTED_URLS)) {
+      test(
+        `it should resolve a git dependency (${url})`,
+        makeTemporaryEnv(
+          {
+            dependencies: {[`util-deprecate`]: url},
+          },
+          async ({path, run, source}) => {
+            await run(`install`);
 
-          await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
-            dependencies: {
-              [`util-deprecate`]: `git://github.com/TooTallNate/util-deprecate.git#v1.0.1`,
-            },
-          });
+            const content = await readFile(`${path}/yarn.lock`, `utf8`);
+            const lock = parseSyml(content);
 
-          const content = await readFile(`${path}/yarn.lock`, `utf8`);
-          const lock = parseSyml(content);
+            await expect(lock).toMatchObject({
+              [`util-deprecate@${url}`]: {
+                version,
+                resolution: `util-deprecate@${url}`,
+              },
+            });
 
-          await expect(lock).toMatchObject({
-            [`util-deprecate@git://github.com/TooTallNate/util-deprecate.git#v1.0.1`]: {
-              "version": "1.0.1",
-            },
-          });
-
-          await expect(source(`require('util-deprecate/package.json')`)).resolves.toMatchObject({
-            name: `util-deprecate`,
-            version: `1.0.1`,
-          });
-        },
-      ),
-    );
-
-    test(
-      `it should resolve a dependency with git tag via ssh protocol`,
-      makeTemporaryEnv(
-        {
-          dependencies: {[`util-deprecate`]: `git+ssh://git@github.com/TooTallNate/util-deprecate.git#v1.0.1`},
-        },
-        async ({path, run, source}) => {
-          await run(`install`);
-
-          await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
-            dependencies: {
-              [`util-deprecate`]: `git+ssh://git@github.com/TooTallNate/util-deprecate.git#v1.0.1`,
-            },
-          });
-
-          const content = await readFile(`${path}/yarn.lock`, `utf8`);
-          const lock = parseSyml(content);
-
-          await expect(lock).toMatchObject({
-            [`util-deprecate@git+ssh://git@github.com/TooTallNate/util-deprecate.git#v1.0.1`]: {
-              "version": "1.0.1",
-            },
-          });
-
-          await expect(source(`require('util-deprecate/package.json')`)).resolves.toMatchObject({
-            name: `util-deprecate`,
-            version: `1.0.1`,
-          });
-        },
-      ),
-    );
-
-    test(
-      `it should resolve a dependency with git tag via https protocol`,
-      makeTemporaryEnv(
-        {
-          dependencies: {[`util-deprecate`]: `https://github.com/TooTallNate/util-deprecate.git#v1.0.0`},
-        },
-        async ({path, run, source}) => {
-          await run(`install`);
-
-          await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
-            dependencies: {
-              [`util-deprecate`]: `https://github.com/TooTallNate/util-deprecate.git#v1.0.0`,
-            },
-          });
-
-          const content = await readFile(`${path}/yarn.lock`, `utf8`);
-          const lock = parseSyml(content);
-
-          await expect(lock).toMatchObject({
-            [`util-deprecate@https://github.com/TooTallNate/util-deprecate.git#v1.0.0`]: {
-              "version": "1.0.0",
-            },
-          });
-
-          await expect(source(`require('util-deprecate/package.json')`)).resolves.toMatchObject({
-            name: `util-deprecate`,
-            version: `1.0.0`,
-          });
-        },
-      ),
-    );
-
-    test(
-      `it should resolve a dependency with git branch via https protocol`,
-      makeTemporaryEnv(
-        {
-          dependencies: {[`util-deprecate`]: `https://github.com/TooTallNate/util-deprecate.git#master`},
-        },
-        async ({path, run, source}) => {
-          await run(`install`);
-
-          await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
-            dependencies: {
-              [`util-deprecate`]: `https://github.com/TooTallNate/util-deprecate.git#master`,
-            },
-          });
-
-          const content = await readFile(`${path}/yarn.lock`, `utf8`);
-          const lock = parseSyml(content);
-
-          await expect(lock).toMatchObject({
-            [`util-deprecate@https://github.com/TooTallNate/util-deprecate.git#master`]: {
-              "version": "1.0.2",
-            },
-          });
-
-          await expect(source(`require('util-deprecate/package.json')`)).resolves.toMatchObject({
-            name: `util-deprecate`,
-            version: `1.0.2`,
-          });
-        },
-      ),
-    );
-
-    test(
-      `it should resolve a dependency with git commit via https protocol`,
-      makeTemporaryEnv(
-        {
-          dependencies: {[`util-deprecate`]: `https://github.com/TooTallNate/util-deprecate.git#b3562c2`},
-        },
-        async ({path, run, source}) => {
-          await run(`install`);
-
-          await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
-            dependencies: {
-              [`util-deprecate`]: `https://github.com/TooTallNate/util-deprecate.git#b3562c2`,
-            },
-          });
-
-          const content = await readFile(`${path}/yarn.lock`, `utf8`);
-          const lock = parseSyml(content);
-
-          await expect(lock).toMatchObject({
-            [`util-deprecate@https://github.com/TooTallNate/util-deprecate.git#b3562c2`]: {
-              "version": "1.0.0",
-            },
-          });
-
-          await expect(source(`require('util-deprecate/package.json')`)).resolves.toMatchObject({
-            name: `util-deprecate`,
-            version: `1.0.0`,
-          });
-        },
-      ),
-    );
+            await expect(source(`require('util-deprecate/package.json')`)).resolves.toMatchObject({
+              name: `util-deprecate`,
+              version,
+            });
+          },
+        ),
+      );
+    }
   });
 });
