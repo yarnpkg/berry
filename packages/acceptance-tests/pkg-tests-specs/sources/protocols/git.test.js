c1fc0e115765b4aadafc837e6b33f1ce2bf2d25f
Maël Nison <nison.mael@gmail.com>
2019-08-16 14:16:16 +0200

    Fixes workflows

--- packages/acceptance-tests/pkg-tests-specs/sources/protocols/git.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/protocols/git.test.js
@@ -7,8 +7,8 @@ const TESTED_URLS = {
   // We've picked util-deprecate because it doesn't have any dependency, and thus
   // doesn't crash when installing through our mock registry. We also could have
   // made our own repository (and maybe we will), but it was simpler this way.
-  [`git://github.com/TooTallNate/util-deprecate.git#v1.0.1`]: {version: `1.0.1`},
-  [`git+ssh://git@github.com/TooTallNate/util-deprecate.git#v1.0.1`]: {version: `1.0.1`},
+  [`git://github.com/TooTallNate/util-deprecate.git#v1.0.1`]: {version: `1.0.1`, ci: false},
+  [`git+ssh://git@github.com/TooTallNate/util-deprecate.git#v1.0.1`]: {version: `1.0.1`, ci: false},
   [`https://github.com/TooTallNate/util-deprecate.git#v1.0.0`]: {version: `1.0.0`},
   [`https://github.com/TooTallNate/util-deprecate.git#master`]: {version: `1.0.2`},
   [`https://github.com/TooTallNate/util-deprecate.git#b3562c2`]: {version: `1.0.0`},
@@ -16,8 +16,12 @@ const TESTED_URLS = {
 
 describe(`Protocols`, () => {
   describe(`git:`, () => {
-    for (const [url, {version}] of Object.entries(TESTED_URLS)) {
-      test(
+    for (const [url, {ci = true, version}] of Object.entries(TESTED_URLS)) {
+      const testFn = !process.env.GITHUB_ACTIONS || ci
+        ? test
+        : test.skip;
+
+      testFn(
         `it should resolve a git dependency (${url})`,
         makeTemporaryEnv(
           {
