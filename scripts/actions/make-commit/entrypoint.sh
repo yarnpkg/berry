7c4165557f2d290bbcccc6f30339bb9c1d1dc753
Maël Nison <nison.mael@gmail.com>
2019-08-16 10:29:05 +0200

    Adds missing files

--- scripts/actions/make-commit/entrypoint.sh
+++ scripts/actions/make-commit/entrypoint.sh
@@ -3,15 +3,15 @@
 set -e
 echo
 
-if [[ ${YARNBOT_TOKEN:-""} == "" ]]; then
-  echo Expected a valid Github token in YARNBOT_TOKEN, got nothing instead
+if [[ ${GITHUB_TOKEN:-""} == "" ]]; then
+  echo Expected a valid Github token in GITHUB_TOKEN, got nothing instead
   exit 1
 fi
 
-git config --global user.email "nison.mael+yarnbot@gmail.com"
-git config --global user.name "Friendly Yarn Bot"
+git config user.email "nison.mael+yarnbot@gmail.com"
+git config user.name "Friendly Yarn Bot"
 
-git remote add yarnbot https://yarnbot:"${YARNBOT_TOKEN}"@github.com/"${GITHUB_REPOSITORY}".git
+git remote add yarnbot https://yarnbot:"${GITHUB_TOKEN}"@github.com/"${GITHUB_REPOSITORY}".git
 git add .
-git commit -m "${*}"
-git push yarnbot "${GITHUB_REF}"
+git commit --allow-empty -m "${INPUT_MESSAGE:-${*}}"
+git push yarnbot HEAD:master
