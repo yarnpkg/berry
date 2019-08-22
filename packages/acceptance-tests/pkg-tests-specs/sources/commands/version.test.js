842cd3406305779a9330b3e29ade95eef75ebab6
Maël Nison <nison.mael@gmail.com>
2019-08-22 22:18:54 +0200

    More `yarn version` tests & fixes (#377)
    * Fixes more things on version
    
    * wiop
    
    * Adds more tests

---
+++ packages/acceptance-tests/pkg-tests-specs/sources/commands/version.test.js
@@ -0,0 +1,165 @@
+const {
+  fs: {readJson},
+  exec: {execFile},
+} = require('pkg-tests-core');
+
+describe(`Commands`, () => {
+  describe(`version check`, () => {
+    test(
+      `it shouldn't work if the strategy isn't semver and there is no prior version`,
+      makeTemporaryEnv({}, async ({path, run, source}) => {
+        await expect(run(`version`, `patch`)).rejects.toThrow();
+      }),
+    );
+
+    test(
+      `it shouldn't work if the immediate bump would be lower than the planned version (semver strategy)`,
+      makeTemporaryEnv({
+        version: `1.0.0`,
+        nextVersion: {
+          semver: `1.1.0`,
+        },
+      }, async ({path, run, source}) => {
+        await expect(run(`version`, `1.0.1`)).rejects.toThrow();
+      }),
+    );
+
+    test(
+      `it shouldn't work if the immediate bump would be lower than the planned version (incremental strategy)`,
+      makeTemporaryEnv({
+        version: `1.0.0`,
+        nextVersion: {
+          semver: `1.1.0`,
+        },
+      }, async ({path, run, source}) => {
+        await expect(run(`version`, `patch`)).rejects.toThrow();
+      }),
+    );
+
+    test(
+      `it should work if the immediate bump is greater than the planned version (semver strategy)`,
+      makeTemporaryEnv({
+        version: `1.0.0`,
+        nextVersion: {
+          semver: `1.1.0`,
+        },
+      }, async ({path, run, source}) => {
+        await run(`version`, `2.0.0`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          version: `2.0.0`,
+        });
+      }),
+    );
+
+    test(
+      `it should work if the immediate bump is greater than the planned version (incremental strategy)`,
+      makeTemporaryEnv({
+        version: `1.0.0`,
+        nextVersion: {
+          semver: `1.1.0`,
+        },
+      }, async ({path, run, source}) => {
+        await run(`version`, `major`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          version: `2.0.0`,
+        });
+      }),
+    );
+
+    test(
+      `it should work if the immediate bump is equal to the planned version (semver strategy)`,
+      makeTemporaryEnv({
+        version: `1.0.0`,
+        nextVersion: {
+          semver: `1.1.0`,
+        },
+      }, async ({path, run, source}) => {
+        await run(`version`, `1.1.0`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          version: `1.1.0`,
+        });
+      }),
+    );
+
+    test(
+      `it should work if the immediate bump is to than the planned version (incremental strategy)`,
+      makeTemporaryEnv({
+        version: `1.0.0`,
+        nextVersion: {
+          semver: `1.1.0`,
+        },
+      }, async ({path, run, source}) => {
+        await run(`version`, `minor`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          version: `1.1.0`,
+        });
+      }),
+    );
+
+    test(
+      `it should increase the version number for a workspace`,
+      makeTemporaryEnv({
+        version: `0.0.0`,
+      }, async ({path, run, source}) => {
+        await run(`version`, `patch`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          version: `0.0.1`,
+        });
+      }),
+    );
+
+    test(
+      `it shouldn't immediatly increase the version number for a workspace when using --deferred`,
+      makeTemporaryEnv({
+        version: `0.0.0`,
+      }, async ({path, run, source}) => {
+        await run(`version`, `patch`, `--deferred`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          version: `0.0.0`,
+          nextVersion: {
+            semver: `0.0.1`,
+          },
+        });
+      }),
+    );
+
+    test(
+      `it shouldn't immediatly increase the version number for a workspace when using preferDeferredVersions`,
+      makeTemporaryEnv({
+        version: `0.0.0`,
+      }, {
+        preferDeferredVersions: true,
+      }, async ({path, run, source}) => {
+        await run(`version`, `patch`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          version: `0.0.0`,
+          nextVersion: {
+            semver: `0.0.1`,
+          },
+        });
+      }),
+    );
+
+    test(
+      `it should immediatly increase the version number for a workspace when using --immediate, even if preferDeferredVersions is set`,
+      makeTemporaryEnv({
+        version: `0.0.0`,
+      }, {
+        preferDeferredVersions: true,
+      }, async ({path, run, source}) => {
+        await run(`version`, `patch`, `--immediate`);
+
+        await expect(readJson(`${path}/package.json`)).resolves.toMatchObject({
+          version: `0.0.1`,
+        });
+      }),
+    );
+  });
+});
