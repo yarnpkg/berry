4059580e3dfc5c8e1343071eabbb5a7a095a4486
Maël Nison <nison.mael@gmail.com>
2019-08-25 17:05:19 +0200

    Fixes the release workflow

--- scripts/actions/make-commit/entrypoint.sh
+++ scripts/actions/make-commit/entrypoint.sh
@@ -11,7 +11,9 @@ fi
 git config user.email "nison.mael+yarnbot@gmail.com"
 git config user.name "Friendly Yarn Bot"
 
+git checkout -b commit-wip
 git remote add yarnbot https://yarnbot:"${GITHUB_TOKEN}"@github.com/"${GITHUB_REPOSITORY}".git
 git add .
 git commit --allow-empty -m "${INPUT_MESSAGE:-${*}}"
+git pull --rebase origin master
 git push yarnbot HEAD:master
