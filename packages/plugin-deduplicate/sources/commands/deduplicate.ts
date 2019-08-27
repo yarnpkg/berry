cb099b57c84f60b5eeb2f9e51fde942d98a0f2bc
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-08-27 18:46:37 +0200

    Fix formatting issues

--- packages/plugin-deduplicate/sources/commands/deduplicate.ts
+++ packages/plugin-deduplicate/sources/commands/deduplicate.ts
@@ -34,38 +34,38 @@ export default class DeduplicateCommand extends BaseCommand {
     }
 
     for (const descriptorHash of project.storedResolutions.keys()) {
-      const descriptor = project.storedDescriptors.get(descriptorHash)!
+      const descriptor = project.storedDescriptors.get(descriptorHash)!;
       const locatorHashes = locatorsByIdent.get(descriptor.identHash)!;
 
       if (locatorHashes !== undefined && locatorHashes.size > 1) {
         const candidates = Array.from(locatorHashes).filter(candidateHash => {
-          const pkg  = project.storedPackages.get(candidateHash)!
+          const pkg  = project.storedPackages.get(candidateHash)!;
 
           if (structUtils.isVirtualLocator(pkg))
-            return false
+            return false;
 
           if (pkg.version === null)
-            return false
+            return false;
 
           const semverMatch = descriptor.range.match(/^npm:(.*)$/);
           if (semverMatch === null)
             return false;
 
-          return semver.satisfies(pkg.version, semverMatch[1])
+          return semver.satisfies(pkg.version, semverMatch[1]);
         }).sort((a, b) => {
-          const pkgA  = project.storedPackages.get(a)!
-          const pkgB  = project.storedPackages.get(b)!
+          const pkgA  = project.storedPackages.get(a)!;
+          const pkgB  = project.storedPackages.get(b)!;
           return semver.gt(pkgA.version!, pkgB.version!) ? -1: 1;
         });
 
         if (candidates.length > 1) {
           const newLocatorHash = candidates[0];
-          const oldLocatorHash = project.storedResolutions.get(descriptorHash)!
+          const oldLocatorHash = project.storedResolutions.get(descriptorHash)!;
           const newPkg = project.storedPackages.get(newLocatorHash)!;
           const oldPkg = project.storedPackages.get(oldLocatorHash)!;
 
           if (structUtils.areLocatorsEqual(oldPkg, newPkg) === false) {
-            console.log(`${structUtils.stringifyDescriptor(descriptor)} can be deduplicated from ${oldPkg.name}@${oldPkg.version} to ${newPkg.name}@${newPkg.version}`)
+            console.log(`${structUtils.stringifyDescriptor(descriptor)} can be deduplicated from ${oldPkg.name}@${oldPkg.version} to ${newPkg.name}@${newPkg.version}`);
 
             project.storedResolutions.set(descriptorHash, newLocatorHash);
           }
