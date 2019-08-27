a8e36fe29e0abb776dec7925c7b6e42465beec1e
Maël Nison <nison.mael@gmail.com>
2019-08-27 17:48:29 +0200

    Slightly changes the wording

--- packages/acceptance-tests/pkg-tests-specs/sources/commands/version/apply.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/commands/version/apply.test.js
@@ -11,7 +11,7 @@ describe(`Commands`, () => {
           name: '@scope/test',
         },
         async ({path, run}) => {
-          await expect(run(`version`, `apply`)).rejects.toThrow(`UsageError: @scope/test@workspace:.: Can't apply the version bump if the resulting version (^2.0.0-rc.0) isn't valid semver`);
+          await expect(run(`version`, `apply`)).rejects.toThrow(`UsageError: Can't apply the version bump if the resulting version (^2.0.0-rc.0) isn't valid semver (in @scope/test@workspace:.)`);
         }
       )
     );
