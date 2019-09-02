37d2617a1aa42ff4a03d673ce4383c77842f1f94
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-09-02 21:16:00 +0200

    Full install step after deduplicate
    Updates pnp file. Has the nice side-effect of running postInstall
    hooks which in this case also clean the offline cache

--- packages/plugin-deduplicate/sources/commands/deduplicate.ts
+++ packages/plugin-deduplicate/sources/commands/deduplicate.ts
@@ -1,8 +1,8 @@
-import {BaseCommand}                                    from '@yarnpkg/cli';
-import {Configuration, LocatorHash, Project, IdentHash} from '@yarnpkg/core';
-import {structUtils}                                    from '@yarnpkg/core';
-import {Command}                                        from 'clipanion';
-import * as semver                                      from 'semver';
+import {BaseCommand}                                                                      from '@yarnpkg/cli';
+import {Cache, Configuration, LocatorHash, Project, IdentHash, StreamReport, MessageName} from '@yarnpkg/core';
+import {structUtils}                                                                      from '@yarnpkg/core';
+import {Command}                                                                          from 'clipanion';
+import * as semver                                                                        from 'semver';
 
 
 // eslint-disable-next-line arca/no-default-export
@@ -19,61 +19,85 @@ export default class DeduplicateCommand extends BaseCommand {
     const configuration = await Configuration.find(this.context.cwd, this.context.plugins);
     const {project} = await Project.find(configuration, this.context.cwd);
 
-    const locatorsByIdent: Map<IdentHash, Set<LocatorHash>> = new Map();
-    for (const [descriptorHash, locatorHash] of project.storedResolutions.entries()) {
-      const value = locatorHash;
-      const descriptor = project.storedDescriptors.get(descriptorHash)!;
-      const key = descriptor.identHash;
+    const deduplicateReport = await StreamReport.start({
+      configuration,
+      stdout: this.context.stdout,
+      includeFooter: false,
+    }, async report => {
+      report.startTimerSync('deduplication step', () => {
+        deduplicate(project, report);
+      });
+    });
 
-      const locators = locatorsByIdent.get(key);
-      if (locators === undefined) {
-        locatorsByIdent.set(key, new Set([value]));
-      } else {
-        locatorsByIdent.set(key, locators.add(value));
-      }
+    if (deduplicateReport.hasErrors())
+      return deduplicateReport.exitCode();
+
+    const cache = await Cache.find(configuration);
+    const installReport = await StreamReport.start({
+      configuration,
+      stdout: this.context.stdout,
+      includeLogs: true,
+    }, async (report: StreamReport) => {
+      await project.install({cache, report});
+    });
+
+    return installReport.exitCode();
+  }
+}
+
+function deduplicate(project: Project, report: StreamReport) {
+  const locatorsByIdent: Map<IdentHash, Set<LocatorHash>> = new Map();
+  for (const [descriptorHash, locatorHash] of project.storedResolutions.entries()) {
+    const value = locatorHash;
+    const descriptor = project.storedDescriptors.get(descriptorHash)!;
+    const key = descriptor.identHash;
+
+    const locators = locatorsByIdent.get(key);
+    if (locators === undefined) {
+      locatorsByIdent.set(key, new Set([value]));
+    } else {
+      locatorsByIdent.set(key, locators.add(value));
     }
+  }
 
-    for (const descriptorHash of project.storedResolutions.keys()) {
-      const descriptor = project.storedDescriptors.get(descriptorHash)!;
-      const locatorHashes = locatorsByIdent.get(descriptor.identHash)!;
+  for (const descriptorHash of project.storedResolutions.keys()) {
+    const descriptor = project.storedDescriptors.get(descriptorHash)!;
+    const locatorHashes = locatorsByIdent.get(descriptor.identHash)!;
 
-      const semverMatch = descriptor.range.match(/^npm:(.*)$/);
-      if (semverMatch === null)
-        continue;
+    const semverMatch = descriptor.range.match(/^npm:(.*)$/);
+    if (semverMatch === null)
+      continue;
 
-      if (locatorHashes !== undefined && locatorHashes.size > 1) {
-        const candidates = Array.from(locatorHashes).map(locatorHash => {
-          const pkg  = project.storedPackages.get(locatorHash)!;
-          if (structUtils.isVirtualLocator(pkg)) {
-            const sourceLocator = structUtils.devirtualizeLocator(pkg);
-            return project.storedPackages.get(sourceLocator.locatorHash)!;
-          }
+    if (locatorHashes !== undefined && locatorHashes.size > 1) {
+      const candidates = Array.from(locatorHashes).map(locatorHash => {
+        const pkg  = project.storedPackages.get(locatorHash)!;
+        if (structUtils.isVirtualLocator(pkg)) {
+          const sourceLocator = structUtils.devirtualizeLocator(pkg);
+          return project.storedPackages.get(sourceLocator.locatorHash)!;
+        }
 
-          return pkg;
-        }).filter(sourcePackage => {
-          if (sourcePackage.version === null)
-            return false;
+        return pkg;
+      }).filter(sourcePackage => {
+        if (sourcePackage.version === null)
+          return false;
 
-          return semver.satisfies(sourcePackage.version, semverMatch[1]);
-        }).sort((a, b) => {
-          return semver.gt(a.version!, b.version!) ? -1: 1;
-        });
+        return semver.satisfies(sourcePackage.version, semverMatch[1]);
+      }).sort((a, b) => {
+        return semver.gt(a.version!, b.version!) ? -1: 1;
+      });
 
-        if (candidates.length > 1) {
-          const newLocatorHash = candidates[0].locatorHash;
-          const oldLocatorHash = project.storedResolutions.get(descriptorHash)!;
-          const newPkg = project.storedPackages.get(newLocatorHash)!;
-          const oldPkg = project.storedPackages.get(oldLocatorHash)!;
+      if (candidates.length > 1) {
+        const newLocatorHash = candidates[0].locatorHash;
+        const oldLocatorHash = project.storedResolutions.get(descriptorHash)!;
+        const newPkg = project.storedPackages.get(newLocatorHash)!;
+        const oldPkg = project.storedPackages.get(oldLocatorHash)!;
 
-          if (structUtils.areLocatorsEqual(oldPkg, newPkg) === false) {
-            console.log(`${structUtils.stringifyDescriptor(descriptor)} can be deduplicated from ${oldPkg.name}@${oldPkg.version} to ${newPkg.name}@${newPkg.version}`);
+        if (structUtils.areLocatorsEqual(oldPkg, newPkg) === false) {
+          report.reportInfo(MessageName.UNNAMED, `${structUtils.stringifyDescriptor(descriptor)} can be deduplicated from ${oldPkg.name}@${oldPkg.version} to ${newPkg.name}@${newPkg.version}`);
 
-            project.storedResolutions.set(descriptorHash, newLocatorHash);
-          }
+          project.storedResolutions.set(descriptorHash, newLocatorHash);
         }
       }
     }
-
-    await project.persistLockfile();
   }
 }
