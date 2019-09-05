2023430e7922360a4ff2923de793cdfa547d0fdc
Maël Nison <nison.mael@gmail.com>
2019-09-05 18:27:00 +0200

    Improves the release-build script

--- scripts/release-builds.sh
+++ scripts/release-builds.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
+set -e
+
+THIS_DIR=$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+REPO_DIR="$THIS_DIR/.."
+
+if false && ! [[ -z $(git status --porcelain) ]]; then
+  echo 'This command must be executed on a clean repository'
+  exit 1
+fi
+
 RELEASE_ARGUMENTS=()
 
-release_package() {
-  git describe --match "$1/*" HEAD | while read tag; do
-    IDENT=$(git tag -l --format='%(contents)' "$tag")
+maybe_release_package() {
+  if git describe --match "$1/*" HEAD |& :; then
     RELEASE_ARGUMENTS+=(--include "$1")
-  done
+  fi
 }
 
-yarn workspaces foreach \
-  --topological "${RELEASE_ARGUMENTS[@]}" \
+
+while read ident; do
+  maybe_release_package "$ident"
+done < <(yarn constraints query --json "workspace_ident(Cwd, Ident), \+ workspace_field(Cwd, 'private', 'true')" | jq -r .Ident)
+
+if [[ ${#RELEASE_ARGUMENTS[@]} -eq 0 ]]; then
+  exit 0
+fi
+
+echo YARN_NPM_PUBLISH_REGISTRY=https://npm.pkg.github.com yarn workspaces foreach \
+  --verbose --topological --no-private ${RELEASE_ARGUMENTS[@]} \
   npm publish --tolerate-republish
