8f50cf6df377b970c0df831551c547276e6eb991
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-09-17 15:00:04 +0300

    Add proper return types

--- packages/plugin-node-modules/sources/NodeModulesLinker.ts
+++ packages/plugin-node-modules/sources/NodeModulesLinker.ts
@@ -1,21 +1,21 @@
-import {Installer, Linker, LinkOptions, MinimalLinkOptions} from '@yarnpkg/core';
-import {FetchResult, Descriptor, Locator, Package}          from '@yarnpkg/core';
-import {PortablePath}                                       from '@yarnpkg/fslib';
+import {Installer, Linker, LinkOptions, MinimalLinkOptions}       from '@yarnpkg/core';
+import {FetchResult, Descriptor, Locator, Package, InstallStatus} from '@yarnpkg/core';
+import {PortablePath}                                             from '@yarnpkg/fslib';
 
 export class NodeModulesLinker implements Linker {
-  supportsPackage(pkg: Package, opts: MinimalLinkOptions) {
+  supportsPackage(pkg: Package, opts: MinimalLinkOptions): boolean {
     return true;
   }
 
-  async findPackageLocation(locator: Locator, opts: LinkOptions) {
+  async findPackageLocation(locator: Locator, opts: LinkOptions): Promise<PortablePath> {
+    return PortablePath.root;
+  }
+
+  async findPackageLocator(location: PortablePath, opts: LinkOptions): Promise<Locator | null> {
     return null;
   }
 
-  async findPackageLocator(location: PortablePath, opts: LinkOptions) {
-    return null;
-  }
-
-  makeInstaller(opts: LinkOptions) {
+  makeInstaller(opts: LinkOptions): Installer {
     return new NodeModulesInstaller(opts);
   }
 }
@@ -27,18 +27,16 @@ class NodeModulesInstaller implements Installer {
     this.opts = opts;
   }
 
-  async installPackage(pkg: Package, fetchResult: FetchResult) {
-    return null;
+  async installPackage(pkg: Package, fetchResult: FetchResult): Promise<InstallStatus> {
+    return {packageLocation: PortablePath.root};
   }
 
-  async attachInternalDependencies(locator: Locator, dependencies: Array<[Descriptor, Locator]>) {
-    return null;
+  async attachInternalDependencies(locator: Locator, dependencies: Array<[Descriptor, Locator]>): Promise<void> {
   }
 
-  async attachExternalDependents(locator: Locator, dependentPaths: Array<PortablePath>) {
-    return null;
+  async attachExternalDependents(locator: Locator, dependentPaths: Array<PortablePath>): Promise<void> {
   }
 
-  async finalizeInstall() {
+  async finalizeInstall(): Promise<void> {
   }
 }
