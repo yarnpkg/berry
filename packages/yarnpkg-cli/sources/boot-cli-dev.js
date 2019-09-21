0b3466c4cbb475533cea177965420a669777a35b
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-09-21 13:41:24 +0300

    Set require.cache entry directly

--- packages/yarnpkg-cli/sources/boot-cli-dev.js
+++ packages/yarnpkg-cli/sources/boot-cli-dev.js
@@ -7,13 +7,14 @@ const dynamicLibs = require('@yarnpkg/builder/sources/data/dynamicLibs').dynamic
 const plugins = require('@yarnpkg/builder/sources/tools/findPlugins.ts').findPlugins({basedir: path.dirname(require.resolve(`@yarnpkg/cli/package.json`)), plugins: []});
 
 const PLUGIN_CONFIG_MODULE = './pluginConfiguration.raw.js';
-require(PLUGIN_CONFIG_MODULE);
-require.cache[require.resolve(PLUGIN_CONFIG_MODULE)].exports = {
-  modules: Array.from(dynamicLibs).concat(plugins).reduce((acc, module) => {
-    acc.set(module, require(module));
-    return acc;
-  }, new Map()),
-  plugins: new Set(plugins),
+require.cache[require.resolve(PLUGIN_CONFIG_MODULE)] = {
+  exports: {
+    modules: Array.from(dynamicLibs).concat(plugins).reduce((acc, module) => {
+      acc.set(module, require(module));
+      return acc;
+    }, new Map()),
+    plugins: new Set(plugins),
+  },
 };
 
 module.exports = require(`./cli`);
