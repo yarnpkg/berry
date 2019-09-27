e38675c109e85d9868f76f0f42fc9895418ef76a
Maël Nison <nison.mael@gmail.com>
2019-09-27 15:35:57 +0200

    Improves the index page (#496)
    * Improves the index page
    
    * Adds a QA page
    
    * Adds a new Q

---
+++ packages/gatsby/content/advanced/questions-and-answers.md
@@ -0,0 +1,45 @@
+---
+category: advanced
+path: /advanced/qa
+title: "Questions & Answers"
+---
+
+## Is Yarn operated by Facebook?
+
+**No.**
+
+Despite the first version of Yarn being implemented by [Sebastian McKenzie](https://twitter.com/sebmck) while working at Facebook, the initial design received feedbacks from various other companies (such as [Tilde](https://www.tilde.io) via [Yehuda Katz](https://yehudakatz.com/2016/10/11/im-excited-to-work-on-yarn-the-new-js-package-manager-2/)) and the project was put into its own [GitHub organization](https://github.com/yarnpkg). While Facebook kept investing in it during the following years (mostly because it proved a critical part of the RN ecosystem), major contributions came from the open-source too.
+
+Nowadays the active development team is composed exclusively from people employed by non-founders companies. Facebook employees are of course still welcome to offer contributions to the project, but they would go through the same review process as any other.
+
+## Why `registry.yarnpkg.com`? Does Facebook track us?
+
+**No.**
+
+When Yarn got created, the npm registry used to be served through Fastly. This was apparently affecting the install performances, so the initial team decided to partner with Cloudflare and setup a [reverse proxy](https://en.wikipedia.org/wiki/Reverse_proxy) that would simply better cache the requests before returning them. This setup didn't even have a backend.
+
+At some point npm switched to Cloudflare as well, and we turned off the proxy to replace it by a [CNAME](https://en.wikipedia.org/wiki/CNAME_record) ([proof](https://toolbox.googleapps.com/apps/dig/#CNAME/registry.yarnpkg.com)). We still keep the hostname for reliability reasons - while it stands to reason that the Yarn domain name will keep being maintained for as long as Yarn is being used, the same isn't necessarily true of the npm domain name. This gives us the ability to redirect to a read-only copy of the registry should the primary source become unavailable.
+
+To this day, no analytics are emitted by Yarn itself.
+
+## Is Yarn faster than other package managers?
+
+**Shrug 🤷‍♀️**
+
+At the time Yarn got released Yarn was effectively much faster than some of its competitors. Unfortunately, we failed to highlight that performances weren't the main reason why we kept working on Yarn. Performances come and go, so while we were super fast it wasn't so much because we were doing something incredibly well, but rather that the competing implementations had a serious bug. When that bug got fixed, our miscommunication became more apparent as some people thought that Yarn was all about performances.
+
+Put simply, our differences lie in our priorities. Different projects make different tradeoffs, and it's exactly what happens here. We prioritized workspaces because we felt like monorepos were providing significant value. We've spent significant resources pushing for Plug'n'Play (including through [dozens of contributions to third-party projects](https://github.com/pulls?utf8=%E2%9C%93&q=is%3Apr+author%3Aarcanis+archived%3Afalse+is%3Aclosed+pnp+-user%3Ayarnpkg+)) because we felt like this was important for the ecosystem. This is the main difference: we make our own informed decisions regarding the project roadmap.
+
+Speed is relative and a temporary state. Processes, roadmaps and core values are what sticks.
+
+## Should lockfiles be committed to the repository?
+
+**Yes**
+
+Lockfiles are meant to always be stored along with your project sources - and this regardless of whether you're writing a standalone application or a distributed library.
+
+One persisting argument against checking-in the lockfile in the repository is about being made aware of potential problems against the latest versions of the library. People saying this argue that the lockfile being present prevents contributors from seeing such issues, as all dependencies are locked and appear fine until a consumer install the library and uses more recent (and incompatible) dependencies.
+
+Although tempting, this reasoning has a fatal flaw: removing the lockfile from the repository doesn't prevent this problem from happening. Contributors won't test against new versions unless they run an install, so older projects may never even notice such incompatibilities. Then, years later, users that want to work on an old project won't even be able to install it because it's latest known good state didn't get checked-in. Even without going all the way to "years later", new contributors will always have to ponder whether things broke because of their changes or because of an incompatible dependency - decreasing the amount of contributions you'll receive.
+
+Lockfiles should **always** be kept within the repository. Continuous integration testing is a good idea, but should be left to continuous integration systems. For example, Yarn itself runs [daily tests](https://github.com/yarnpkg/berry#current-status) against the latest versions of major open-source frameworks and tools. [Dependabot](https://dependabot.com/#how-it-works) is also a good tool that allows you to track your dependencies updates in a more integrated way.
--- packages/gatsby/src/pages/index.js
+++ packages/gatsby/src/pages/index.js
@@ -1,29 +1,52 @@
+import {css}             from '@emotion/core';
 import styled            from '@emotion/styled';
+import {graphql}         from 'gatsby';
 import React, {useState} from 'react';
 
 import Header                                  from '../components/header';
 import {SearchBar, SearchResults, withUrlSync} from '../components/search';
 import {SearchProvider}                        from '../components/search';
 import Layout                                  from '../components/layout';
-import {ifMobile}                              from '../components/responsive';
+import {ifDesktop, ifMobile}                   from '../components/responsive';
 import SEO                                     from '../components/seo';
+import agendaIcon                              from '../images/homeicons/agenda.svg';
+import laptopIcon                              from '../images/homeicons/laptop.svg';
+import noteIcon                                from '../images/homeicons/note.svg';
+import puzzleIcon                              from '../images/homeicons/puzzle.svg';
+import rocketIcon                              from '../images/homeicons/rocket.svg';
+import worldwideIcon                           from '../images/homeicons/worldwide.svg';
 
-const Hero = styled.div`
+const sectionStyle = css`
   width: 100%;
 
-  padding: 5em 6em;
+  padding: 3em 6em;
+`;
+
+const Section = styled.div`
+  ${sectionStyle}
+`;
+
+const sectionContentStyle = css`
+  margin: 0 auto 0 auto;
+  width: 1140px;
+  max-width: 100%;
+`;
+
+const SectionContent = styled.div`
+  ${sectionContentStyle}
+`;
+
+const Hero = styled.div`
+  ${sectionStyle}
+
+  padding-top: 5em;
+  padding-bottom: 5em;
 
   background: #2188b6;
 
   ${ifMobile} {
     padding: .5em;
   }
-`;
-
-const HeroFrame = styled.div`
-  margin: 0 auto 0 auto;
-  width: 1140px;
-  max-width: 100%;
 `;
 
 const HeroTitle = styled.div`
@@ -45,7 +68,68 @@ const HeroSubtitle = styled.div`
   color: #ffffff;
 `;
 
-const IndexPage = ({ searchState, onSearchStateChange }) => {
+const SellingPoints = styled.div`
+  ${ifDesktop} {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+`;
+
+const SellingPointContainer = styled.div`
+  display: flex;
+
+  ${ifDesktop} {
+    width: calc(50% - 20px);
+  }
+
+  margin-top: 40px;
+
+  &:last-child {
+    margin-bottom: 40px;
+  }
+`;
+
+const SellingPointIcon = styled.img`
+  width: 100px;
+  height: 100px;
+`;
+
+const SellingPointContent = styled.div`
+  margin-left: 20px;
+
+  h3 {
+    margin-top: 0;
+  }
+`;
+
+const Copy = styled.div`
+  font-size: 14px;
+
+  ${ifDesktop} {
+    display: flex;
+
+    div {
+      margin-left: 0;
+      margin-right: 0;
+
+      &:last-child {
+        margin-left: auto;
+      }
+    }
+  }
+`;
+
+const SellingPoint = ({ imgUrl, children }) => <>
+  <SellingPointContainer>
+    <SellingPointIcon src={imgUrl}/>
+    <SellingPointContent>
+      {children}
+    </SellingPointContent>
+  </SellingPointContainer>
+</>;
+
+const IndexPage = ({ data, searchState, onSearchStateChange }) => {
   const [tags, setTags] = useState([]);
   const [owners, setOwners] = useState([]);
 
@@ -72,16 +156,55 @@ const IndexPage = ({ searchState, onSearchStateChange }) => {
         />
         {!searchState.query &&
           <Hero>
-            <HeroFrame>
+            <SectionContent>
               <HeroTitle>
                 Safe, stable, reproducible projects
               </HeroTitle>
               <HeroSubtitle>
                 Yarn is a package manager that doubles down as project manager. Whether you work on one-shot projects or large monorepos, as a hobbyist or an enterprise user, we've got you covered.
               </HeroSubtitle>
-            </HeroFrame>
+            </SectionContent>
           </Hero>
         }
+        <Section>
+          <SectionContent>
+            <SellingPoints>
+              <SellingPoint imgUrl={agendaIcon}>
+                <h3>Workspaces</h3>
+                Split your project into sub-components kept within a single repository.
+              </SellingPoint>
+              <SellingPoint imgUrl={laptopIcon}>
+                <h3>Stability</h3>
+                Yarn guarantees that an install that works now will continue to work the same way in the future.
+              </SellingPoint>
+              <SellingPoint imgUrl={noteIcon}>
+                <h3>Documentation</h3>
+                Special care is put our documentation, and we keep improving it based on your feedback.
+              </SellingPoint>
+              <SellingPoint imgUrl={puzzleIcon}>
+                <h3>Plugins</h3>
+                Yarn cannot solve all your problems - but it can be the foundation for others to do it.
+              </SellingPoint>
+              <SellingPoint imgUrl={rocketIcon}>
+                <h3>Innovation</h3>
+                We believe in challenging the status quo. What should the ideal developer experience be like?
+              </SellingPoint>
+              <SellingPoint imgUrl={worldwideIcon}>
+                <h3>Openness</h3>
+                Yarn is an independent open-source project tied to no company. Your support make us thrive.
+              </SellingPoint>
+            </SellingPoints>
+          </SectionContent>
+        </Section>
+        <hr/>
+        <Section style={{paddingTop: `1em`, paddingBottom: `1em`}}>
+          <SectionContent>
+            <Copy>
+              <div>Yarn · <a href={`https://github.com/yarnpkg/berry/blob/master/LICENSE.md`}>Distributed under BSD License</a> · <a href={`https://github.com/yarnpkg/berry/blob/master/CODE_OF_CONDUCT.md`}>Code of Conduct</a></div>
+              <div>Icons by <a href={`https://www.freepik.com`}>Freepik</a> from <a href={`https://www.flaticon.com`}>flaticon.com</a></div>
+            </Copy>
+          </SectionContent>
+        </Section>
       </Layout>
     </SearchProvider>
   </>);
