7bdc147273a2994f661f5d6b938fe1059524b2fc
Zoltán Bedi <zoltan.bedi@gmail.com>
2019-09-29 22:16:04 +0200

    remove unused import

--- packages/gatsby/src/pages/index.js
+++ packages/gatsby/src/pages/index.js
@@ -1,6 +1,5 @@
 import {css}             from '@emotion/core';
 import styled            from '@emotion/styled';
-import {graphql}         from 'gatsby';
 import React, {useState} from 'react';
 
 import Header                                  from '../components/header';
