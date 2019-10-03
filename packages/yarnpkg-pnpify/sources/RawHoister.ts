f0ae685fad4a57a945c5d164afb330bc495d6556
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-10-03 17:33:59 +0300

    Improve code quality and internal docs

--- packages/yarnpkg-pnpify/sources/RawHoister.ts
+++ packages/yarnpkg-pnpify/sources/RawHoister.ts
@@ -49,6 +49,10 @@ export type PackageMap = Map<PackageId, PackageInfo>;
  */
 type WeightMap = Map<PackageId, Weight>;
 
+type Voidable<T> = T | void;
+
+const NO_DEPS = new Set<PackageId>();
+
 /**
  * The raw hoister is responsible for transforming a tree of dependencies to reduce tree
  * height as much as possible. It uses the fact that if dependency is not found for some
@@ -61,17 +65,184 @@ type WeightMap = Map<PackageId, Weight>;
  * real dependency trees more often then not can be almost fully hoisted.
  */
 export class RawHoister {
-  private weigh(tree: PackageTree, nodeId: PackageId, packageMap: PackageMap, nohoist: Set<PackageId>, weights: WeightMap = new Map()): WeightMap {
-    if (!nohoist.has(nodeId)) {
-      weights.set(nodeId, packageMap.get(nodeId)!.weight + (weights.get(nodeId) || 0));
-      for (const depId of tree.get(nodeId) || []) {
-        this.weigh(tree, depId, packageMap, nohoist, weights);
+  /**
+   * Hoists package tree, by applying hoisting algorithm to each tree node independently.
+   * We first try to hoist all the packages from anywhere in the whole tree to the root package.
+   * Then we apply the same algorithm to the subtree that starts at one of the dependencies of the
+   * root package, we do this for each dependency, then move further down to dependencies of
+   * dependencies, etc.
+   *
+   * @param tree package tree
+   * @param packageMap package map
+   * @param nohoist package ids that should be excluded from applying hoisting algorithm. Nohoist
+   *                packages can be hoisted themselves, and their dependencies can be hoisted too,
+   *                but only to the package itself, they cannot be hoisted to the nohoist package parent
+   */
+  public hoist(tree: PackageTree, packageMap: PackageMap, nohoist: Set<PackageId> = new Set()): PackageTree {
+    // Validate the tree and package map first
+    this.validate(tree, packageMap);
+
+    // Make normalized tree copy, which will be mutated by hoisting algorithm
+    const normalizedTree = this.normalize(tree);
+
+    // Apply mutating hoisting algorithm on each tree node starting from the root
+    this.traverse(normalizedTree, 0, (nodeId) => {
+      this.hoistInplace(normalizedTree, nodeId, packageMap, nohoist);
+    });
+
+    // Take care of hoisting results to have deterministic form
+    const result = new Map();
+    const pkgIds = Array.from(normalizedTree.keys()).sort();
+    for (const pkgId of pkgIds)
+      result.set(pkgId, new Set(Array.from(normalizedTree.get(pkgId)!).sort()));
+
+    return result;
+  }
+
+  /**
+   * Performs package subtree hoisting to its root.
+   * This funtion mutates tree.
+   *
+   * @param tree package tree
+   * @param rootId package subtree root package id
+   * @param packageMap package map
+   * @param nohoist nohoist package ids
+   */
+  private hoistInplace(tree: PackageTree, rootId: PackageId, packageMap: PackageMap, nohoist: Set<PackageId>): void {
+    // Get the list of package ids that can and should be hoisted to the subtree root
+    const hoistedDepIds = this.computeHoistCandidates(tree, rootId, packageMap, nohoist);
+
+    const removeHoistedDeps = (nodeId: PackageId) => {
+      // No need to traverse past nohoist node
+      if (nohoist.has(nodeId))
+        return;
+
+      const depIds = tree.get(nodeId) || NO_DEPS;
+      for (const depId of depIds) {
+        // First traverse to deeper levels
+        removeHoistedDeps(depId);
+        // Then remove hoisted deps from current node
+        if (hoistedDepIds.has(depId)) {
+          depIds.delete(depId);
+        }
       }
+      // Remove node without deps
+      if (nodeId !== rootId && depIds.size === 0) {
+        tree.delete(nodeId);
+      }
+    };
+
+    removeHoistedDeps(rootId);
+
+    const nodeDepIds = tree.get(rootId)!;
+    for (const depId of hoistedDepIds) {
+      // Add hoisted packages to the subtree root
+      nodeDepIds.add(depId);
     }
+  }
+
+  /**
+   * Traverses the package tree by visting each package and executing callback,
+   * starting from the package with id `rootId`
+   *
+   * @param tree package tree
+   * @param rootId root node id
+   * @param visitNode visitor callback
+   */
+  private traverse(tree: PackageTree, rootId: PackageId, visitNode: (nodeId: PackageId, depIds: Set<PackageId>) => Voidable<() => void>) {
+    const depIds = tree.get(rootId) || NO_DEPS;
+    const cleanup = visitNode(rootId, depIds);
+    for (const depId of depIds)
+      this.traverse(tree, depId, visitNode);
+    if (cleanup) {
+      cleanup();
+    }
+  }
+
+  /**
+   * Checks if information about every package in the tree is available in packageMap
+   * and throws if it is not so.
+   *
+   * @param tree package tree
+   * @param packageMap package map
+   */
+  private validate(tree: PackageTree, packageMap: PackageMap): void {
+    this.traverse(tree, 0, (nodeId: PackageId) => {
+      if (!packageMap.has(nodeId)) {
+        throw new Error(`Package with id ${nodeId} must be present in package map`);
+      }
+    });
+  }
+
+  /**
+   * Creates normalized package tree copy by removing cycles, 1 -> 2 -> 1 is converted to 1 -> 2.
+   *
+   * @param tree package tree
+   *
+   * @returns normalized package tree
+   */
+  private normalize(tree: PackageTree): PackageTree {
+    // All dependent package ids (values in the map)
+    const allDepIds = new Set();
+    // Seen package ids - all seen package ids in the path from tree root to current node
+    const seenIds = new Set();
+    // Normalized tree copy
+    const normalTree = new Map();
+
+    this.traverse(tree, 0, (nodeId, depIds) => {
+      seenIds.add(nodeId);
+
+      // Normalized dependenciess
+      // We strip dependency ids that produce cycles, e.g. 1 -> 2 -> 1, we left 1 -> 2 in this case
+      const normalDepIds = new Set();
+
+      for (const depId of depIds) {
+        allDepIds.add(depId);
+        if (!seenIds.has(depId)) {
+          normalDepIds.add(depId);
+        }
+      }
+
+      return () => {
+        normalTree.set(nodeId, normalDepIds);
+        seenIds.delete(nodeId);
+      };
+    });
+
+    return normalTree;
+  }
+
+  /**
+   * Weighs all the packages in the subtree, by adding up own package weight with weights of all
+   * of its direct and transitive dependencies.
+   *
+   * @param tree package tree
+   * @param rootId root package id of the subtree
+   * @param packageMap package map
+   * @param nohoist nohoist package ids, that shouldn't be weighed
+   *
+   * @return map of package weights: package id -> total weight
+   */
+  private weighPackages(tree: PackageTree, rootId: PackageId, packageMap: PackageMap, nohoist: Set<PackageId>): WeightMap {
+    const weights: WeightMap = new Map();
+
+    this.traverse(tree, rootId, (nodeId) => {
+      if (!nohoist.has(nodeId)) {
+        weights.set(nodeId, packageMap.get(nodeId)!.weight + (weights.get(nodeId) || 0));
+      }
+    });
 
     return weights;
   }
 
+  /**
+   * Finds packages that have the max weight among the packages with the same name
+   *
+   * @param weights package weights map: package id -> total weight
+   * @param packageMap package info map: package id -> package name
+   *
+   * @returns package ids with max weights among the packages with the same name
+   */
   private getHeaviestPackages(weights: WeightMap, packageMap: PackageMap): Set<PackageId> {
     const heaviestPackages = new Map<PackageName, {weight: Weight, pkgId: PackageId}>();
     for (const [pkgId, weight] of weights) {
@@ -86,128 +257,43 @@ export class RawHoister {
       }
     }
 
-    const result = new Set<PackageId>();
+    const heavyPackageIds = new Set<PackageId>();
     for (const [,pkg] of heaviestPackages)
-      result.add(pkg.pkgId);
+      heavyPackageIds.add(pkg.pkgId);
 
-    return result;
+    return heavyPackageIds;
   }
 
   /**
-   * Returns normalized package tree copy by removing cycles, 1 -> 2 -> 1 is converted to 1 -> 2.
+   * Find the packages that can be hoisted to the subtree root `rootId`.
    *
    * @param tree package tree
-   *
-   * @returns normalized package tree
+   * @param rootId package id that should be regarded as subtree root
+   * @param packageMap package map
+   * @param nohoist nohoist package ids
    */
-  private normalize(tree: PackageTree): PackageTree {
-    // All dependent package ids (values in the map)
-    const allDepIds = new Set();
-    // Seen package ids - all seen package ids in the path from tree root to current node
-    const seenIds = new Set();
-    // Normalized tree copy
-    const normalTree = new Map();
-
-    /**
-     * Traverses package tree and fills in normalilzed tree copy
-     *
-     * @param tree source package tree
-     * @param nodeId the package id of the node in the tree, 0 - for root node
-     */
-    const traverse = (tree: PackageTree, nodeId: PackageId): void => {
-      seenIds.add(nodeId);
-
-      // Normalized dependenciess
-      // We strip dependency ids that produce cycles, e.g. 1 -> 2 -> 1, we left 1 -> 2 in this case
-      const normalDepIds = new Set();
-
-      for (const depId of tree.get(nodeId) || []) {
-        allDepIds.add(depId);
-        if (!seenIds.has(depId)) {
-          normalDepIds.add(depId);
-          traverse(tree, depId);
-        }
-      }
-      normalTree.set(nodeId, normalDepIds);
-
-      seenIds.delete(nodeId);
-    };
-
-    traverse(tree, 0);
-
-    return normalTree;
-  }
-
-  public hoist(tree: PackageTree, packageMap: PackageMap, nohoist: Set<PackageId> = new Set()): PackageTree {
-    if (!tree.has(0))
-      throw new Error('Package tree must have root element with package id 0');
-
-    const normalizedTree = this.normalize(tree);
-    const hoistToNode = (nodeId: PackageId) => {
-      this.hoistInplace(normalizedTree, nodeId, packageMap, nohoist);
-      const depIds = normalizedTree.get(nodeId);
-      for (const depId of depIds || []) {
-        hoistToNode(depId);
-      }
-    };
-
-    hoistToNode(0);
-
-    // Make hoisting results have deterministic form
-    const result = new Map();
-    const pkgIds = Array.from(normalizedTree.keys()).sort();
-    for (const pkgId of pkgIds)
-      result.set(pkgId, new Set(Array.from(normalizedTree.get(pkgId)!).sort()));
-
-    return result;
-  }
-
-  private computeHoistCandidates(tree: PackageTree, nodeId: PackageId, packageMap: PackageMap, nohoist: Set<PackageId>): Set<PackageId> {
-    const hoistCandidateWeights = this.weigh(tree, nodeId, packageMap, nohoist);
-
-    hoistCandidateWeights.delete(nodeId);
-
+  private computeHoistCandidates(tree: PackageTree, rootId: PackageId, packageMap: PackageMap, nohoist: Set<PackageId>): Set<PackageId> {
+    // Get current package dependency package names
     const curDepNames = new Map<PackageName, PackageId>();
-    for (const depId of tree.get(nodeId) || [])
+    for (const depId of tree.get(rootId) || NO_DEPS)
       curDepNames.set(packageMap.get(depId)!.name, depId);
+
+    // At first all the packages in the subtree are hoist candidates, we weigh them all
+    const hoistCandidateWeights = this.weighPackages(tree, rootId, packageMap, nohoist);
+
+    // Current package cannot be hoisted to itself, so we remove it from candidates
+    hoistCandidateWeights.delete(rootId);
 
     for (const [candidateId] of hoistCandidateWeights) {
       const pkgName = packageMap.get(candidateId)!.name;
       const curDepId = curDepNames.get(pkgName);
       if (curDepId && curDepId !== candidateId) {
+        // We cannot hoist package with the same name but different id, remove it from candidates
         hoistCandidateWeights.delete(candidateId);
       }
     }
 
+    // Among all hoist candidates left choose the heaviest
     return this.getHeaviestPackages(hoistCandidateWeights, packageMap);
-  }
-
-  private hoistInplace(tree: PackageTree, nodeId: PackageId, packageMap: PackageMap, nohoist: Set<PackageId>) {
-    const hoistedDepIds = this.computeHoistCandidates(tree, nodeId, packageMap, nohoist);
-
-    const removeHoistedDeps = (curNodeId: PackageId) => {
-      // No need to traverse past nohoist node
-      if (nohoist.has(curNodeId))
-        return;
-
-      const nodeDepIds = tree.get(curNodeId)!;
-      for (const depId of nodeDepIds || []) {
-        removeHoistedDeps(depId);
-        if (hoistedDepIds.has(depId)) {
-          nodeDepIds.delete(depId);
-        }
-      }
-      // Remove node without deps
-      if (curNodeId !== nodeId && nodeDepIds && nodeDepIds.size === 0) {
-        tree.delete(curNodeId);
-      }
-    };
-
-    removeHoistedDeps(nodeId);
-
-    const nodeDepIds = tree.get(nodeId)!;
-    for (const depId of hoistedDepIds) {
-      nodeDepIds.add(depId);
-    }
   }
 };
