eebdf71381c28b7c39e218414329c447c7d85a13
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-10-07 13:47:04 +0300

    Add minimal docs for Hoister class

--- packages/yarnpkg-pnpify/sources/Hoister.ts
+++ packages/yarnpkg-pnpify/sources/Hoister.ts
@@ -6,53 +6,77 @@ import {RawHoister, PackageTree, PackageMap, PackageId}    from './RawHoister';
 type PnpWalkApi = Pick<PnpApi, 'getPackageInformation' | 'getDependencyTreeRoots' | 'topLevel'>;
 
 /**
- * The tree of package locations. The key is a path to node_modules folder.
- * The value is a directory entry list or final package location.
+ * Node modules mapping
  */
-type LocationTree = Map<PortablePath, Set<Filename> | PortablePath>;
+interface NodeModulesMap {
+  /** Directory entries for `../node_modules` and `../node_modules/@foo` directories */
+  dirEntries: Map<PortablePath, Set<Filename>>;
+  /** Package location mapping: `../node_modules/@foo/bar` -> `pnp package location` */
+  packageLocations: Map<PortablePath, PortablePath>;
+};
 
+/** node_modules path segment */
+const NODE_MODULES = toFilename('node_modules');
+
+/** Package locator key for usage inside maps */
+type LocatorKey = string;
+
+/**
+ * Retrieves full package list from PnP API and builds hoisted `node_modules` directories
+ * representation in-memory.
+ */
 export class Hoister {
+  /** Raw hoister which does all the hoisting heavy lifting */
   private rawHoister = new RawHoister();
 
-  public hoist(pnp: PnpWalkApi): LocationTree {
-    const locationTree = new Map();
+  /**
+   * Retrieve full package list and build hoisted `node_modules` directories
+   * representation in-memory.
+   *
+   * @param pnp PnP API
+   *
+   * @returns hoisted `node_modules` directories representation in-memory
+   */
+  public hoist(pnp: PnpWalkApi): NodeModulesMap {
+    const dirEntries: Map<PortablePath, Set<Filename>> = new Map();
+    const packageLocations: Map<PortablePath, PortablePath> = new Map();
+
     const packageTree: PackageTree = new Map();
-    const packageMap: PackageMap = new Map();
-    const packageToLocator = new Map<PackageId, PackageLocator>();
-    const locatorToPackage = new Map<string, PackageId>();
+    const packageNameMap: PackageMap = new Map();
+    const packageToLocatorMap = new Map<PackageId, PackageLocator>();
+    const locatorToPackageMap = new Map<LocatorKey, PackageId>();
 
     const pnpRoots = pnp.getDependencyTreeRoots ? pnp.getDependencyTreeRoots() : [pnp.topLevel];
 
     let lastPkgId = 0;
 
-    const getLocatorKey = (locator: PackageLocator) => `${locator.name}:${locator.reference}`;
+    const getLocatorKey = (locator: PackageLocator): LocatorKey => `${locator.name}:${locator.reference}`;
 
     const assignPackageId = (locator: PackageLocator) => {
       const locatorKey = getLocatorKey(locator);
-      let pkgId = locatorToPackage.get(locatorKey);
+      let pkgId = locatorToPackageMap.get(locatorKey);
       if (!pkgId) {
         pkgId = lastPkgId++;
-        locatorToPackage.set(locatorKey, pkgId);
-        packageToLocator.set(pkgId, locator);
-        packageMap.set(pkgId, {name: locator.name!, weight: 1});
+        locatorToPackageMap.set(locatorKey, pkgId);
+        packageToLocatorMap.set(pkgId, locator);
+        packageNameMap.set(pkgId, {name: locator.name!, weight: 1});
       }
       return pkgId;
     };
 
-    const buildPackageTree = (parent: PackageLocator) => {
-      const parentPkgId = assignPackageId(parent);
-      const depIds = new Set<PackageId>();
-      packageTree.set(parentPkgId, depIds);
-
+    const addPackageToTree = (parent: PackageLocator,  parentPkgId: PackageId) => {
       const pkg = pnp.getPackageInformation(parent);
       if (pkg) {
+        const depIds = new Set<PackageId>();
+        packageTree.set(parentPkgId, depIds);
+
         for (const [name, reference] of pkg.packageDependencies) {
           if (reference) {
             const locator = typeof reference === 'string' ? {name, reference} : {name: reference[0], reference: reference[1]};
             const pkgId = assignPackageId(locator);
-            depIds.add(pkgId);
             if (!packageTree.has(pkgId)) {
-              buildPackageTree(locator);
+              depIds.add(pkgId);
+              addPackageToTree(locator, pkgId);
             }
           }
         }
@@ -60,9 +84,9 @@ export class Hoister {
     };
 
     for (const locator of pnpRoots)
-      buildPackageTree(locator);
+      addPackageToTree(locator, assignPackageId(locator));
 
-    const hoistedTree = this.rawHoister.hoist(packageTree, packageMap);
+    const hoistedTree = this.rawHoister.hoist(packageTree, packageNameMap);
 
     const NO_DEPS = new Set<PackageId>();
     const getLocation = (locator: PackageLocator): PortablePath =>
@@ -72,15 +96,14 @@ export class Hoister {
       return name ? {scope: toFilename(nameOrScope), name: toFilename(name)} : {scope: null, name: toFilename(nameOrScope)};
     };
 
-    const NODE_MODULES_SEGMENT = toFilename('node_modules');
     const buildLocationTree = (nodeId: PackageId, prefix: PortablePath) => {
       const entries = new Map<Filename, PackageLocator | null>();
       const scopeMap = new Map<Filename, Map<Filename, PackageLocator>>();
       const depIds = hoistedTree.get(nodeId) || NO_DEPS;
       for (const depId of depIds) {
-        const locator = packageToLocator.get(depId)!;
+        const locator = packageToLocatorMap.get(depId)!;
         const {name, scope} = getPackageName(locator);
-        let depPrefix = ppath.join(...([prefix, NODE_MODULES_SEGMENT].concat(scope ? [scope, name] : [name])));
+        let depPrefix = ppath.join(...([prefix, NODE_MODULES].concat(scope ? [scope, name] : [name])));
         buildLocationTree(depId, depPrefix);
         if (scope) {
           let scopeEntries = scopeMap.get(scope);
@@ -96,28 +119,24 @@ export class Hoister {
       }
 
       if (entries.size > 0) {
-        locationTree.set(prefix, new Set([NODE_MODULES_SEGMENT]));
-        const sortedEntries = Array.from(entries.keys()).sort();
-        locationTree.set(ppath.join(prefix, NODE_MODULES_SEGMENT), new Set(sortedEntries));
-        for (const entry of sortedEntries) {
-          const locator = entries.get(entry);
+        dirEntries.set(ppath.join(prefix, NODE_MODULES), new Set(Array.from(entries.keys()).sort()));
+        for (const [entry, locator] of entries) {
           if (locator) {
-            locationTree.set(ppath.join(prefix, NODE_MODULES_SEGMENT, entry), getLocation(locator));
+            packageLocations.set(ppath.join(prefix, NODE_MODULES, entry), getLocation(locator));
           } else {
             const scopeEntries = scopeMap.get(entry)!;
-            const sortedScopeEntries = Array.from(scopeEntries.keys()).sort();
-            locationTree.set(ppath.join(prefix, NODE_MODULES_SEGMENT, entry), new Set(sortedScopeEntries));
-            for (const scopeEntry of sortedScopeEntries) {
-              locationTree.set(ppath.join(prefix, NODE_MODULES_SEGMENT, entry, scopeEntry), getLocation(scopeEntries.get(scopeEntry)!));
+            dirEntries.set(ppath.join(prefix, NODE_MODULES, entry), new Set(Array.from(scopeEntries.keys()).sort()));
+            for (const [scopeEntry, locator] of scopeEntries) {
+              packageLocations.set(ppath.join(prefix, NODE_MODULES, entry, scopeEntry), getLocation(locator));
             }
           }
         }
       }
     };
 
-    const rootPrefix = getLocation(packageToLocator.get(0)!);
+    const rootPrefix = getLocation(packageToLocatorMap.get(0)!);
     buildLocationTree(0, rootPrefix);
 
-    return locationTree;
+    return {dirEntries, packageLocations};
   }
 }
