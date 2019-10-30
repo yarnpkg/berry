983689477f5d8c07b7991922be17f0d07faab757
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-10-30 09:24:26 +0200

    Rollback PR 522

--- packages/yarnpkg-pnpify/sources/generateSdk.ts
+++ packages/yarnpkg-pnpify/sources/generateSdk.ts
@@ -1,5 +1,4 @@
 import {Filename, PortablePath, npath, ppath, xfs} from '@yarnpkg/fslib';
-import json5                                       from 'json5';
 
 import {dynamicRequire}                            from './dynamicRequire';
 
@@ -28,8 +27,8 @@ const addVSCodeWorkspaceSettings = async (projectRoot: PortablePath, settings: a
   const settingsPath = ppath.join(projectRoot, `.vscode/settings.json` as PortablePath);
   const content = await xfs.existsPromise(settingsPath) ? await xfs.readFilePromise(settingsPath, `utf8`) : `{}`;
 
-  const data = json5.parse(content);
-  const patched = `${json5.stringify({...data, ...settings}, null, 2)}\n`;
+  const data = JSON.parse(content);
+  const patched = `${JSON.stringify({...data, ...settings}, null, 2)}\n`;
 
   await xfs.mkdirpPromise(ppath.dirname(settingsPath));
   await xfs.changeFilePromise(settingsPath, patched);
