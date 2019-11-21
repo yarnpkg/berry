1e88732863b588bc365977c68096cbfc81c91780
Will Griffiths <thiswill@gmail.com>
2019-11-21 15:36:17 +0100

    Remove unnecessary no node_module test cases

--- packages/yarnpkg-check/fixtures/no-node-module-strings/invalid.ts
+++ packages/yarnpkg-check/fixtures/no-node-module-strings/invalid.ts
@@ -1,14 +1,12 @@
 // StringLiteral
-var foo1 = "../../node_modules";
-var foo2 = "./node_modules/module-x/node_modules";
-var foo3 = "node_modules/module-x";
-var foo4 = "node_modules";
-var foo5 = require("../node_modules/module-x");
+var a = "../../node_modules/module-x";
+var b = "./node_modules/module-x/node_modules/module-y";
+var c = require("../node_modules/module-x");
 
 // TemplateLiteral
-var foo6 = `../node_modules/lodash`;
+var d = `../node_modules/module-x`;
 
 // TemplateExpression
-var moduleName = "lodash";
-var foo7 = `../node_modules/${moduleName}`;
-var module = require(`../node_modules/${moduleName}`) as NodeModule;
+var moduleName = "module-x";
+var e = `../node_modules/${moduleName}`;
+var f = require(`../node_modules/${moduleName}`) as NodeModule;
