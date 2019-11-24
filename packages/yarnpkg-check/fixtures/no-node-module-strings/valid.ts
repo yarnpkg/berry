6190e3631c49327d933d652dbfc0f62988ddba80
Will Griffiths <thiswill@gmail.com>
2019-11-24 22:48:30 +0100

    Add no node modules strings to yarnpkg/check (#599)
    * Add no node_module strings check
    
    * Add yarn-pkg/check readme
    
    * Remove unnecessary no node_module test cases
    
    * Update check readme
    
    * Update check valid fixtures
    
    * Format cli.js
    
    * Format cli.js
    
    * Update cli.ts
    
    * Tweaks the message
    
    * Bumps the package

---
+++ packages/yarnpkg-check/fixtures/no-node-module-strings/valid.ts
@@ -0,0 +1,7 @@
+// Comment including node_modules
+const a = "node";
+const b = "modules";
+const c = 'a random string';
+const d = `a random template literal`;
+const expression = "expression";
+const d = `a random template ${expression}`;
