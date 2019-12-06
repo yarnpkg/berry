a3ef79b2f92741124ee5ab7ec020e6f6b4b15140
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-12-05 17:04:31 -0800

    Strip byte order mark characters before parsing JSON

--- packages/yarnpkg-core/sources/Manifest.ts
+++ packages/yarnpkg-core/sources/Manifest.ts
@@ -96,7 +96,7 @@ export class Manifest {
   loadFromText(text: string) {
     let data;
     try {
-      data = JSON.parse(text || `{}`);
+      data = JSON.parse(stripBOM(text) || `{}`);
     } catch (error) {
       error.message += ` (when parsing ${text})`;
       throw error;
@@ -111,7 +111,7 @@ export class Manifest {
 
     let data;
     try {
-      data = JSON.parse(content || `{}`);
+      data = JSON.parse(stripBOM(content) || `{}`);
     } catch (error) {
       error.message += ` (when parsing ${path})`;
       throw error;
@@ -630,4 +630,11 @@ function getIndent(content: string) {
   } else {
     return `  `;
   }
+}
+
+function stripBOM(content: string) {
+  if (content.charCodeAt(0) === 0xFEFF) {
+		return content.slice(1);
+	}
+	return content;
 }
