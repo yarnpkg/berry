5a4f0c68d3679d1ba714fd0b55665bb1100f5af6
Victor Vlasenko <victor.vlasenko@sysgears.com>
2020-01-08 13:13:45 +0200

    Fix main entry point to pnpify to not use @babel/register

--- packages/yarnpkg-pnpify/sources/boot-dev.js
+++
@@ -1,5 +0,0 @@
-process.env.NODE_OPTIONS += ` --require ${require.resolve(`@yarnpkg/monorepo/scripts/setup-ts-execution`)}`;
-
-require(`@yarnpkg/monorepo/scripts/setup-ts-execution`);
-
-module.exports = require(`./index`);
