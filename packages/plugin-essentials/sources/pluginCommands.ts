012607452d76e37e27d2420f4ba44b475bd39624
Maël Nison <nison.mael@gmail.com>
2020-01-19 12:03:18 +0100

    Adds the plugin list to the run command

---
+++ packages/plugin-essentials/sources/pluginCommands.ts
@@ -0,0 +1,22 @@
+export const pluginCommands = new Map([
+  [`constraints`, [
+    [`constraints`, `query`],
+    [`constraints`, `source`],
+    [`constraints`],
+  ]],
+  [`interactive-tools`, [
+    [`upgrade-interactive`],
+  ]],
+  [`stage`, [
+    [`stage`],
+  ]],
+  [`version`, [
+    [`version`, `apply`],
+    [`version`, `check`],
+    [`version`],
+  ]],
+  [`workspace-tools`, [
+    [`workspaces`, `foreach`],
+    [`workspace`],
+  ]],
+]);
