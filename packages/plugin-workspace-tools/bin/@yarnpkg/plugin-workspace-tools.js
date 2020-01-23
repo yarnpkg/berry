b296d7bd3cf9f5fb16d629188b929c309f070926
Maël Nison <nison.mael@gmail.com>
2020-01-23 11:06:25 -0500

    Releasing 35 new packages
    | Package name | Version |
    | --- | --- |
    | `@yarnpkg/cli` | `2.0.0-rc.25` |
    | `@yarnpkg/plugin-essentials` | `2.0.0-rc.21` |
    | `@yarnpkg/core` | `2.0.0-rc.20` |
    | `@yarnpkg/plugin-compat` | `2.0.0-rc.5` |
    | `@yarnpkg/builder` | `2.0.0-rc.17` |
    | `@yarnpkg/check` | `2.0.0-rc.12` |
    | `@yarnpkg/fslib` | `2.0.0-rc.15` |
    | `@yarnpkg/json-proxy` | `2.0.0-rc.7` |
    | `@yarnpkg/libui` | `2.0.0-rc.7` |
    | `@yarnpkg/libzip` | `2.0.0-rc.8` |
    | `@yarnpkg/parsers` | `2.0.0-rc.9` |
    | `@yarnpkg/plugin-constraints` | `2.0.0-rc.12` |
    | `@yarnpkg/plugin-dlx` | `2.0.0-rc.12` |
    | `@yarnpkg/plugin-exec` | `2.0.0-rc.15` |
    | `@yarnpkg/plugin-file` | `2.0.0-rc.12` |
    | `@yarnpkg/plugin-git` | `2.0.0-rc.15` |
    | `@yarnpkg/plugin-github` | `2.0.0-rc.12` |
    | `@yarnpkg/plugin-http` | `2.0.0-rc.11` |
    | `@yarnpkg/plugin-init` | `2.0.0-rc.12` |
    | `@yarnpkg/plugin-interactive-tools` | `2.0.0-rc.14` |
    | `@yarnpkg/plugin-link` | `2.0.0-rc.11` |
    | `@yarnpkg/plugin-node-modules` | `2.0.0-rc.2` |
    | `@yarnpkg/plugin-npm` | `2.0.0-rc.14` |
    | `@yarnpkg/plugin-npm-cli` | `2.0.0-rc.13` |
    | `@yarnpkg/plugin-pack` | `2.0.0-rc.16` |
    | `@yarnpkg/plugin-patch` | `2.0.0-rc.4` |
    | `@yarnpkg/plugin-pnp` | `2.0.0-rc.15` |
    | `@yarnpkg/plugin-stage` | `2.0.0-rc.15` |
    | `@yarnpkg/plugin-typescript` | `2.0.0-rc.14` |
    | `@yarnpkg/plugin-version` | `2.0.0-rc.20` |
    | `@yarnpkg/plugin-workspace-tools` | `2.0.0-rc.15` |
    | `@yarnpkg/pnp` | `2.0.0-rc.16` |
    | `@yarnpkg/pnpify` | `2.0.0-rc.16` |
    | `@yarnpkg/shell` | `2.0.0-rc.7` |
    | `vscode-zipfs` | `0.1.1-3` |

--- packages/plugin-workspace-tools/bin/@yarnpkg/plugin-workspace-tools.js
+++ packages/plugin-workspace-tools/bin/@yarnpkg/plugin-workspace-tools.js
@@ -108,10 +108,8 @@ module.exports = {
 
   const foreach_1 = __importDefault(__webpack_require__(1));
 
-  const workspace_1 = __importDefault(__webpack_require__(9));
-
   const plugin = {
-    commands: [foreach_1.default, workspace_1.default]
+    commands: [foreach_1.default]
   }; // eslint-disable-next-line arca/no-default-export
 
   exports.default = plugin;
@@ -543,87 +541,6 @@ module.exports = {
   /***/ (function(module, exports) {
 
   module.exports = require("yup");
-
-  /***/ }),
-  /* 9 */
-  /***/ (function(module, exports, __webpack_require__) {
-
-  "use strict";
-
-
-  var __decorate = this && this.__decorate || function (decorators, target, key, desc) {
-    var c = arguments.length,
-        r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc,
-        d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-  };
-
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
-
-  const cli_1 = __webpack_require__(2);
-
-  const core_1 = __webpack_require__(3);
-
-  const core_2 = __webpack_require__(3);
-
-  const clipanion_1 = __webpack_require__(4); // eslint-disable-next-line arca/no-default-export
-
-
-  class WorkspaceCommand extends clipanion_1.Command {
-    constructor() {
-      super(...arguments);
-      this.args = [];
-    }
-
-    async execute() {
-      const configuration = await core_1.Configuration.find(this.context.cwd, this.context.plugins);
-      const {
-        project,
-        workspace: cwdWorkspace
-      } = await core_1.Project.find(configuration, this.context.cwd);
-      if (!cwdWorkspace) throw new cli_1.WorkspaceRequiredError(project.cwd, this.context.cwd);
-      const candidates = project.workspaces;
-      const candidatesByName = new Map(candidates.map(workspace => {
-        const ident = core_2.structUtils.convertToIdent(workspace.locator);
-        return [core_2.structUtils.stringifyIdent(ident), workspace];
-      }));
-      const workspace = candidatesByName.get(this.workspaceName);
-
-      if (workspace === undefined) {
-        const otherNames = Array.from(candidatesByName.keys()).sort();
-        throw new clipanion_1.UsageError(`Workspace '${this.workspaceName}' not found. Did you mean any of the following:\n  - ${otherNames.join("\n  - ")}?`);
-      }
-
-      return this.cli.run([this.commandName, ...this.args], {
-        cwd: workspace.cwd
-      });
-    }
-
-  }
-
-  WorkspaceCommand.usage = clipanion_1.Command.Usage({
-    category: `Workspace-related commands`,
-    description: `run a command within the specified workspace`,
-    details: `
-        > In order to use this command you need to add \`@yarnpkg/plugin-workspace-tools\` to your plugins.
-
-        This command will run a given sub-command on a single workspace.
-      `,
-    examples: [[`Add a package to a single workspace`, `yarn workspace components add -D react`], [`Run build script on a single workspace`, `yarn workspace components run build`]]
-  });
-
-  __decorate([clipanion_1.Command.String()], WorkspaceCommand.prototype, "workspaceName", void 0);
-
-  __decorate([clipanion_1.Command.String()], WorkspaceCommand.prototype, "commandName", void 0);
-
-  __decorate([clipanion_1.Command.Proxy()], WorkspaceCommand.prototype, "args", void 0);
-
-  __decorate([clipanion_1.Command.Path(`workspace`)], WorkspaceCommand.prototype, "execute", null);
-
-  exports.default = WorkspaceCommand;
 
   /***/ })
   /******/ ]);
