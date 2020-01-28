aacfaa5b968ab4d5c758da5e070b7d019a8eac89
Haroen Viaene <hello@haroen.me>
2020-01-28 14:05:28 +0100

    memoize client creation

--- packages/gatsby/src/components/search/SearchProvider.js
+++ packages/gatsby/src/components/search/SearchProvider.js
@@ -1,53 +1,56 @@
 import algoliasearch              from 'algoliasearch/lite';
-import {StaticQuery, graphql}     from 'gatsby';
+import {useStaticQuery, graphql}  from 'gatsby';
 import {Configure, InstantSearch} from 'react-instantsearch-dom';
-import React                      from 'react';
+import React, {useMemo}           from 'react';
 
 export const SearchProvider = ({searchState, onSearchStateChange, children}) => {
-  return (
-    <StaticQuery query={
-      graphql`query AlgoliaConfigQuery {
-        site {
-          siteMetadata {
-            algolia {
-              appId
-              apiKey
-              indexName
-            }
-          }
+  const {site: {siteMetadata: {algolia: {appId, apiKey, indexName}}}} = useStaticQuery(graphql`query AlgoliaConfigQuery {
+    site {
+      siteMetadata {
+        algolia {
+          appId
+          apiKey
+          indexName
         }
-      }`}
-    render={({site: {siteMetadata: {algolia}}}) => (
-      <InstantSearch
-        searchClient={algoliasearch(algolia.appId, algolia.apiKey)}
-        indexName={algolia.indexName}
-        searchState={searchState}
-        onSearchStateChange={onSearchStateChange}
-      >
-        <Configure
-          hitsPerPage={5}
-          facets={['keywords']}
-          analyticsTags={['yarnpkg.com']}
-          attributesToRetrieve={[
-            'deprecated',
-            'description',
-            'downloadsLast30Days',
-            'homepage',
-            'humanDownloadsLast30Days',
-            'keywords',
-            'license',
-            'modified',
-            'name',
-            'owner',
-            'repository',
-            'types',
-            'version',
-          ]}
-          attributesToHighlight={['name', 'description', 'keywords']}
-        />
-        {children}
-      </InstantSearch>
-    )}
-    />
+      }
+    }
+  }`);
+
+
+  const searchClient = useMemo(() => {
+    console.log('memo');
+    return algoliasearch(appId, apiKey);
+  }, [appId,apiKey]);
+
+  return (
+    <InstantSearch
+      searchClient={searchClient}
+      indexName={indexName}
+      searchState={searchState}
+      onSearchStateChange={onSearchStateChange}
+    >
+      <Configure
+        hitsPerPage={5}
+        facets={['keywords']}
+        analyticsTags={['yarnpkg.com']}
+        attributesToRetrieve={[
+          'deprecated',
+          'description',
+          'downloadsLast30Days',
+          'homepage',
+          'humanDownloadsLast30Days',
+          'keywords',
+          'license',
+          'modified',
+          'name',
+          'owner',
+          'repository',
+          'types',
+          'version',
+        ]}
+        attributesToHighlight={['name', 'description', 'keywords']}
+      />
+      {children}
+    </InstantSearch>
   );
 };
