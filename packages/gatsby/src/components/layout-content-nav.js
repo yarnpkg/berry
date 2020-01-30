c98652a355395a8ec537237dcff10db25d61fc52
Benjamin Koltes <benjamin.koltes@datadoghq.com>
2020-01-29 21:16:29 -0500

    [gatsby] avoid text justify because it's bad for the readability

--- packages/gatsby/src/components/layout-content-nav.js
+++ packages/gatsby/src/components/layout-content-nav.js
@@ -3,16 +3,9 @@ import React        from 'react';
 
 import {Layout}     from './layout';
 import {Navigation} from './navigation';
-import {ifDesktop}  from './responsive';
 
 const Container = styled.div`
   padding: 2em;
-
-  ${ifDesktop} {
-    border-top: 1px solid #cfdee9;
-
-    text-align: justify;
-  }
 `;
 
 export const LayoutContentNav = ({items, children}) => {
