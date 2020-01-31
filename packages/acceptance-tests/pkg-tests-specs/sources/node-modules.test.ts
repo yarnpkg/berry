5a41f6c29701685d8dcfc2ad48a75e467276ac67
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2020-01-31 23:19:09 +0100

    Add failing test for node_modules hoisting with workspace conflict

--- packages/acceptance-tests/pkg-tests-specs/sources/node-modules.test.ts
+++ packages/acceptance-tests/pkg-tests-specs/sources/node-modules.test.ts
@@ -1,7 +1,7 @@
 import {npath} from '@yarnpkg/fslib';
 import {fs}    from 'pkg-tests-core';
 
-const {writeFile} = fs;
+const {writeFile, writeJson} = fs;
 
 describe('Node_Modules', () => {
   it('should install one dependency',
@@ -21,5 +21,51 @@ describe('Node_Modules', () => {
         );
       },
     )
+  );
+
+  test(
+    `workspace packages shouldn't be hoisted if they conflict with root dependencies`,
+    makeTemporaryEnv(
+      {
+        private: true,
+        workspaces: [`packages/*`],
+        dependencies: {
+          [`no-deps`]: `*`,
+        },
+      },
+      async ({path, run, source}) => {
+        await writeFile(npath.toPortablePath(`${path}/.yarnrc.yml`), `
+          enableTransparentWorkspaces: false
+          nodeLinker: "node-modules"
+        `);
+
+        await writeFile(
+          npath.toPortablePath(`${path}/index.js`),
+          `
+            module.exports = require('no-deps/package.json');
+          `,
+        );
+
+        await writeJson(npath.toPortablePath(`${path}/packages/workspace/package.json`), {
+          name: `workspace`,
+          version: `1.0.0`,
+          dependencies: {
+            [`no-deps`]: `workspace:*`,
+          },
+        });
+
+        await writeJson(npath.toPortablePath(`${path}/packages/no-deps/package.json`), {
+          name: `no-deps`,
+          version: `1.0.0-local`,
+        });
+
+        await run(`install`);
+
+        await expect(source(`require('.')`)).resolves.toMatchObject({
+          name: `no-deps`,
+          version: `2.0.0`,
+        });
+      },
+    ),
   );
 });
