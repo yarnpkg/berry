f6d45f98768e2acaf50d5ea1f4d0bf87d4e5f77a
Maël Nison <nison.mael@gmail.com>
2020-02-01 23:14:18 +0100

    Improves extensions (#872)
    * Improves extensions
    
    * Adds versions

--- packages/plugin-compat/sources/extensions.ts
+++ packages/plugin-compat/sources/extensions.ts
@@ -6,21 +6,37 @@ export const packageExtensions: Array<[string, any]> = [
   // https://github.com/SamVerschueren/stream-to-observable/pull/5
   [`@samverschueren/stream-to-observable@*`, {
     peerDependenciesMeta: {
-      [`rxjs`]: `*`,
-      [`zenObservable`]: `*`,
+      [`rxjs`]: {
+        optional: true,
+      },
+      [`zenObservable`]: {
+        optional: true,
+      },
     },
   }],
   // https://github.com/sindresorhus/any-observable/pull/25
   [`any-observable@*`, {
     peerDependenciesMeta: {
-      [`rxjs`]: `*`,
-      [`zenObservable`]: `*`,
+      [`rxjs`]: {
+        optional: true,
+      },
+      [`zenObservable`]: {
+        optional: true,
+      },
     },
   }],
   // https://github.com/keymetrics/pm2-io-agent/pull/125
   [`@pm2/agent@*`, {
     dependencies: {
       [`debug`]: `*`,
+    },
+  }],
+  // https://github.com/visionmedia/debug/pull/727
+  [`debug@*`, {
+    peerDependenciesMeta: {
+      [`supports-color`]: {
+        optional: true,
+      },
     },
   }],
 ];
