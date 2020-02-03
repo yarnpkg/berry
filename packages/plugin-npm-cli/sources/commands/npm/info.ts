79f38d0a8374642377160f867b30d98f222dddb8
Maël Nison <nison.mael@gmail.com>
2020-02-03 10:34:52 +0100

    Some tweaks

--- packages/plugin-npm-cli/sources/commands/npm/info.ts
+++ packages/plugin-npm-cli/sources/commands/npm/info.ts
@@ -1,12 +1,12 @@
-import * as npm                              from '@npm/types';
-import {BaseCommand, WorkspaceRequiredError} from '@yarnpkg/cli';
-import {Project, Configuration, structUtils} from '@yarnpkg/core';
-import {StreamReport, MessageName}           from '@yarnpkg/core';
-import {npmHttpUtils}                        from '@yarnpkg/plugin-npm';
-import {Command, Usage, UsageError}          from 'clipanion';
-import JSON5                                 from 'json5';
-import path                                  from 'path';
-import semver                                from 'semver';
+import * as npm                                                       from '@npm/types';
+import {BaseCommand}                                                  from '@yarnpkg/cli';
+import {Project, Configuration, structUtils, ReportError, Descriptor} from '@yarnpkg/core';
+import {StreamReport, MessageName}                                    from '@yarnpkg/core';
+import {npmHttpUtils}                                                 from '@yarnpkg/plugin-npm';
+import {Command, Usage, UsageError}                                   from 'clipanion';
+import path                                                           from 'path';
+import semver                                                         from 'semver';
+import {inspect}                                                      from 'util';
 
 declare module '@npm/types' {
   /*
@@ -17,16 +17,25 @@ declare module '@npm/types' {
   }
 }
 
+/**
+ * The combined type of `Packument` (without the `versions` field) and `PackumentVersion`
+ */
+type CombinedPackument = Omit<npm.Packument, 'versions'> & npm.PackumentVersion;
+
+/**
+ * `CombinedPackument` with a `versions` field that is an array of tags
+ */
+interface PackageInformation extends CombinedPackument {
+  versions: string[];
+}
+
 // eslint-disable-next-line arca/no-default-export
 export default class InfoCommand extends BaseCommand {
-  @Command.String({required: true})
-  package!: string;
+  @Command.Rest()
+  packages!: string;
 
-  @Command.String({required: false})
-  field!: string;
-
-  @Command.Boolean(`-v,--verbose`)
-  verbose: boolean = false;
+  @Command.String(`-f,--fields`)
+  fields?: string;
 
   @Command.Boolean(`--json`)
   json: boolean = false;
@@ -34,112 +43,108 @@ export default class InfoCommand extends BaseCommand {
   static usage: Usage = Command.Usage({
     description: `show information about a package`,
     details: `
-      This command will fetch information about a package and return it in a tree format.
-      The package does not have to have been installed locally.
+      This command will fetch information about a package from the npm registry, and prints it in a tree format.
 
-      The default reporting style for this command is a single-quoted serialization (JSON5). To emit lines of valid JSON, use the \`--json\` flag.
+      The package does not have to be installed locally, but needs to have been published (in particular, local changes will be ignored even for workspaces).
 
-      Append \`@[version]\` to the package argument to provide information specific to that version. If the version is invalid, the command falls back to latest.
+      Append \`@<version>\` to the package argument to provide information specific to that version. If the version is invalid, the command will print a warning and fall back to latest.
 
-      If the optional field argument is provided, then only that part of the tree is returned.
+      If the \`-f,--fields\` option is set, it's a comma-separated list of fields which will be used to only display part of the package informations.
 
-      By default, \`yarn npm info\` will not return the \`dist\`, \`readme\`, and \`users\` fields, since they are often very long. To explicitly request those fields, use the \`-v,--verbose\` flag or the second argument: \`yarn npm info react readme\`
+      By default, this command won't return the \`dist\`, \`readme\`, and \`users\` fields, since they are often very long. To explicitly request those fields, explicitly list them with the \`--fields\` flag or request the output in JSON mode.
+
+      If the \`--json\` flag is set the output will follow a JSON-stream output also known as NDJSON (https://github.com/ndjson/ndjson-spec).
     `,
     examples: [[
       `Show all available information about react (except the \`dist\`, \`readme\`, and \`users\` fields)`,
       `yarn npm info react`,
-    ],[
-      `Show all available information about react, including the \`dist\`, \`readme\`, and \`users\` fields`,
-      `yarn npm info react --verbose`,
-    ],[
-      `Show all available information about react as valid JSON`,
+    ], [
+      `Show all available information about react as valid JSON (including the \`dist\`, \`readme\`, and \`users\` fields)`,
       `yarn npm info react --json`,
-    ],[
+    ], [
       `Show all available information about react 16.12.0`,
       `yarn npm info react@16.12.0`,
-    ],[
+    ], [
       `Show the description of react`,
-      `yarn npm info react description`,
-    ],[
+      `yarn npm info react --fields description`,
+    ], [
       `Show all available versions of react`,
-      `yarn npm info react versions`,
-    ],[
+      `yarn npm info react --fields versions`,
+    ], [
       `Show the readme of react`,
-      `yarn npm info react readme`,
+      `yarn npm info react --fields readme`,
+    ], [
+      `Show a few fields of react`,
+      `yarn npm info react --fields homepage,repository`,
     ]],
   });
 
   @Command.Path(`npm`, `info`)
   async execute() {
     const configuration = await Configuration.find(this.context.cwd, this.context.plugins);
+    const {project} = await Project.find(configuration, this.context.cwd);
 
-    /*
-     * Handle case when the package is the current workspace
-     */
-    if (this.package === `.`) {
-      const {project, workspace} = await Project.find(configuration, this.context.cwd);
+    const fields = typeof this.fields !== `undefined`
+      ? new Set(this.fields.split(/\s*,\s*/))
+      : null;
 
-      if (!workspace)
-        throw new WorkspaceRequiredError(project.cwd, this.context.cwd);
+    if (fields !== null)
+      fields.add(`name`);
 
-      if (!workspace.manifest.name)
-        throw new UsageError(`Missing \`name\` field in ${path.join(workspace.cwd, `package.json`)}`);
-
-      this.package = structUtils.stringifyIdent(workspace.manifest.name);
-    }
-
-    const descriptor = structUtils.parseDescriptor(this.package);
-    const identUrl = npmHttpUtils.getIdentUrl(descriptor);
+    const infos: Array<PackageInformation> = [];
+    let leadWithSeparator = false;
 
     const report = await StreamReport.start({
       configuration,
+      includeFooter: false,
+      json: this.json,
       stdout: this.context.stdout,
     }, async report => {
-      try {
-        /**
-         * The information from `registry.npmjs.org/<package>`
-         */
-        const result = clean(
-          await npmHttpUtils.get(identUrl, {
+      for (const identStr of this.packages) {
+        let descriptor: Descriptor;
+        if (identStr === `.`) {
+          const workspace = project.topLevelWorkspace;
+          if (!workspace.manifest.name)
+            throw new UsageError(`Missing 'name' field in ${path.join(workspace.cwd, `package.json`)}`);
+
+          descriptor = structUtils.makeDescriptor(workspace.manifest.name, `unknown`);
+        } else {
+          descriptor = structUtils.parseDescriptor(identStr);
+        }
+
+        const identUrl = npmHttpUtils.getIdentUrl(descriptor);
+
+        let result: npm.Packument;
+        try {
+          // The information from `registry.npmjs.org/<package>`
+          result = clean(await npmHttpUtils.get(identUrl, {
             configuration,
             ident: descriptor,
             json: true,
-          })
-        ) as npm.Packument;
+          })) as npm.Packument;
+        } catch (err) {
+          if (err.name !== `HTTPError`) {
+            throw err;
+          } else if (err.response.statusCode === 404) {
+            throw new ReportError(MessageName.EXCEPTION, `Package not found`);
+          } else {
+            throw new ReportError(MessageName.EXCEPTION, err.toString());
+          }
+        }
 
-        /**
-         * All version tags of a package, sorted in ascending order
-         */
+        // All version tags of a package, sorted in ascending order
         const versions = Object.keys(result.versions).sort(semver.compareLoose);
 
-        /**
-         * Check if the package range from the command arguments is a valid version
-         */
+        // Check if the package range from the command arguments is a valid version
         const isRangeValid = descriptor.range !== `unknown` && semver.valid(descriptor.range);
 
-        /**
-         * The `descriptor.range` if it is a valid version, else the latest dist-tag or the greatest version
-         */
+        // The `descriptor.range` if it is a valid version, else the latest dist-tag or the most recent version
         const version = isRangeValid
           ? descriptor.range
           : (result[`dist-tags`].latest || versions[versions.length - 1]);
 
-        /**
-         * The release corresponding to `version`
-         */
+        // The release corresponding to `version`
         const release = result.versions[version];
-
-        /**
-         * The combined type of `Packument` (without the `versions` field) and `PackumentVersion`
-         */
-        type CombinedPackument = Omit<npm.Packument, 'versions'> & npm.PackumentVersion;
-
-        /**
-         * `CombinedPackument` with a `versions` field that is an array of tags
-         */
-        interface PackageInformation extends CombinedPackument {
-          versions: string[];
-        }
 
         /**
          * The merging of
@@ -155,55 +160,53 @@ export default class InfoCommand extends BaseCommand {
           versions,
         };
 
-        /**
-         * The filtered data that the command will output
-         */
-        let data: unknown;
+        let serialized: any;
+        if (fields !== null) {
+          serialized = {};
 
-        if (this.field) {
-          /**
-           * The requested field
-           */
-          const fields = Object.keys(packageInformation).filter(key => key === this.field);
-          if (!fields.length) {
-            report.reportError(MessageName.EXCEPTION, `The \`${this.field}\` field doesn't exist inside \`${this.package}\`'s information`);
-          } else if (fields.length === 1) {
-            data = packageInformation[fields[0] as keyof PackageInformation];
+          for (const field of fields) {
+            // @ts-ignore
+            const value = packageInformation[field];
+
+            if (typeof value !== `undefined`) {
+              serialized[field] = value;
+            } else {
+              report.reportWarning(MessageName.EXCEPTION, `The '${field}' field doesn't exist inside ${structUtils.prettyIdent(configuration, descriptor)}'s informations`);
+              leadWithSeparator = true;
+              continue;
+            }
           }
         } else {
-          if (!this.verbose) {
-            /**
-             * Remove long fields
-             */
+          // Remove long fields
+          if (!this.json) {
             delete packageInformation.dist;
             delete packageInformation.readme;
             delete packageInformation.users;
           }
-          data = packageInformation;
+
+          serialized = packageInformation;
         }
 
+        report.reportJson(serialized);
 
-        if (this.json) {
-          data = JSON.stringify(data);
-        } else {
-          data = JSON5.stringify(data, {
-            space: 2,
-          });
-        }
-
-        if (typeof data === `string`) {
-          report.reportInfo(MessageName.UNNAMED, `\n${data}`);
-        }
-      } catch (err) {
-        if (err.name !== `HTTPError`) {
-          throw err;
-        } else if (err.response.statusCode === 404) {
-          report.reportError(MessageName.EXCEPTION, `Package not found`);
-        } else {
-          report.reportError(MessageName.EXCEPTION, err.toString());
+        if (!this.json) {
+          infos.push(serialized);
         }
       }
     });
+
+    inspect.styles.name = `cyan`;
+
+    for (const serialized of infos) {
+      if (serialized !== infos[0] || leadWithSeparator)
+        this.context.stdout.write(`\n`);
+
+      this.context.stdout.write(`${inspect(serialized, {
+        depth: Infinity,
+        colors: true,
+        compact: false,
+      })}\n`);
+    }
 
     return report.exitCode();
   }
@@ -214,25 +217,27 @@ function clean(value: unknown): unknown {
   if (Array.isArray(value)) {
     const result: unknown[] = [];
 
-    value.forEach((item) => {
+    for (let item of value) {
       item = clean(item);
+
       if (item) {
         result.push(item);
       }
-    });
+    }
 
     return result;
   } else if (typeof value === `object` && value !== null) {
     const result: any = {};
 
-    Object.keys(value).forEach((key) => {
+    for (const key of Object.keys(value)) {
       if (key.startsWith(`_`))
-        return;
+        continue;
+
       const item = clean(value[key as keyof typeof value]);
       if (item) {
         result[key] = item;
       }
-    });
+    }
 
     return result;
   } else if (value) {
