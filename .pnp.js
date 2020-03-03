b85cc34a02dc507a764e83819b50a470b9e87834
Paul Soporan <paul.soporan@gmail.com>
2020-03-03 23:02:34 +0200

    chore: remove json-ref-lite from packageExtensions

--- .pnp.js
+++ .pnp.js
@@ -4254,16 +4254,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
-      ["@types/concat-stream", [
-        ["npm:1.6.0", {
-          "packageLocation": "./.yarn/cache/@types-concat-stream-npm-1.6.0-46e1dc8ddf-2.zip/node_modules/@types/concat-stream/",
-          "packageDependencies": [
-            ["@types/concat-stream", "npm:1.6.0"],
-            ["@types/node", "npm:10.12.18"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
       ["@types/configstore", [
         ["npm:2.1.1", {
           "packageLocation": "./.yarn/cache/@types-configstore-npm-2.1.1-bb3144db15-2.zip/node_modules/@types/configstore/",
@@ -4353,16 +4343,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/@types-finalhandler-npm-1.1.0-bb15aba0e7-2.zip/node_modules/@types/finalhandler/",
           "packageDependencies": [
             ["@types/finalhandler", "npm:1.1.0"],
-            ["@types/node", "npm:10.12.18"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["@types/form-data", [
-        ["npm:0.0.33", {
-          "packageLocation": "./.yarn/cache/@types-form-data-npm-0.0.33-3cbbcd9710-2.zip/node_modules/@types/form-data/",
-          "packageDependencies": [
-            ["@types/form-data", "npm:0.0.33"],
             ["@types/node", "npm:10.12.18"]
           ],
           "linkType": "HARD",
@@ -4626,13 +4606,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@types/node", "npm:7.10.5"]
           ],
           "linkType": "HARD",
-        }],
-        ["npm:8.10.59", {
-          "packageLocation": "./.yarn/cache/@types-node-npm-8.10.59-a625b9fbb5-2.zip/node_modules/@types/node/",
-          "packageDependencies": [
-            ["@types/node", "npm:8.10.59"]
-          ],
-          "linkType": "HARD",
         }]
       ]],
       ["@types/prettier", [
@@ -4667,15 +4640,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/@types-q-npm-1.5.1-29b49b9eca-2.zip/node_modules/@types/q/",
           "packageDependencies": [
             ["@types/q", "npm:1.5.1"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["@types/qs", [
-        ["npm:6.9.1", {
-          "packageLocation": "./.yarn/cache/@types-qs-npm-6.9.1-2c8ccdb5dd-2.zip/node_modules/@types/qs/",
-          "packageDependencies": [
-            ["@types/qs", "npm:6.9.1"]
           ],
           "linkType": "HARD",
         }]
@@ -14637,16 +14601,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["mime-types", "npm:2.1.21"]
           ],
           "linkType": "HARD",
-        }],
-        ["npm:2.5.1", {
-          "packageLocation": "./.yarn/cache/form-data-npm-2.5.1-47256351b5-2.zip/node_modules/form-data/",
-          "packageDependencies": [
-            ["form-data", "npm:2.5.1"],
-            ["asynckit", "npm:0.4.0"],
-            ["combined-stream", "npm:1.0.7"],
-            ["mime-types", "npm:2.1.21"]
-          ],
-          "linkType": "HARD",
         }]
       ]],
       ["forwarded", [
@@ -16774,19 +16728,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
-      ["http-basic", [
-        ["npm:8.1.3", {
-          "packageLocation": "./.yarn/cache/http-basic-npm-8.1.3-ae54b14025-2.zip/node_modules/http-basic/",
-          "packageDependencies": [
-            ["http-basic", "npm:8.1.3"],
-            ["caseless", "npm:0.12.0"],
-            ["concat-stream", "npm:1.6.2"],
-            ["http-response-object", "npm:3.0.2"],
-            ["parse-cache-control", "npm:1.0.1"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
       ["http-cache-semantics", [
         ["npm:3.8.1", {
           "packageLocation": "./.yarn/cache/http-cache-semantics-npm-3.8.1-9d037f563e-2.zip/node_modules/http-cache-semantics/",
@@ -16879,16 +16820,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["is-glob", "npm:4.0.0"],
             ["lodash", "npm:4.17.11"],
             ["micromatch", "npm:3.1.10"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["http-response-object", [
-        ["npm:3.0.2", {
-          "packageLocation": "./.yarn/cache/http-response-object-npm-3.0.2-cbb68c5487-2.zip/node_modules/http-response-object/",
-          "packageDependencies": [
-            ["http-response-object", "npm:3.0.2"],
-            ["@types/node", "npm:10.12.18"]
           ],
           "linkType": "HARD",
         }]
@@ -19535,8 +19466,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/json-ref-lite-npm-1.1.0-8fcbe7edd2-2.zip/node_modules/json-ref-lite/",
           "packageDependencies": [
             ["json-ref-lite", "npm:1.1.0"],
-            ["property-expr", "npm:1.5.1"],
-            ["sync-request", "npm:6.1.0"]
+            ["property-expr", "npm:1.5.1"]
           ],
           "linkType": "HARD",
         }]
@@ -22578,15 +22508,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
-      ["parse-cache-control", [
-        ["npm:1.0.1", {
-          "packageLocation": "./.yarn/cache/parse-cache-control-npm-1.0.1-81068d3680-2.zip/node_modules/parse-cache-control/",
-          "packageDependencies": [
-            ["parse-cache-control", "npm:1.0.1"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
       ["parse-english", [
         ["npm:4.1.1", {
           "packageLocation": "./.yarn/cache/parse-english-npm-4.1.1-96a30105ac-2.zip/node_modules/parse-english/",
@@ -23978,14 +23899,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/promise-npm-7.3.1-5d81d474c0-2.zip/node_modules/promise/",
           "packageDependencies": [
             ["promise", "npm:7.3.1"],
-            ["asap", "npm:2.0.6"]
-          ],
-          "linkType": "HARD",
-        }],
-        ["npm:8.0.3", {
-          "packageLocation": "./.yarn/cache/promise-npm-8.0.3-eb5af24d14-2.zip/node_modules/promise/",
-          "packageDependencies": [
-            ["promise", "npm:8.0.3"],
             ["asap", "npm:2.0.6"]
           ],
           "linkType": "HARD",
@@ -27431,28 +27344,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD",
         }]
       ]],
-      ["sync-request", [
-        ["npm:6.1.0", {
-          "packageLocation": "./.yarn/cache/sync-request-npm-6.1.0-6ac24ab111-2.zip/node_modules/sync-request/",
-          "packageDependencies": [
-            ["sync-request", "npm:6.1.0"],
-            ["http-response-object", "npm:3.0.2"],
-            ["sync-rpc", "npm:1.3.6"],
-            ["then-request", "npm:6.0.2"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["sync-rpc", [
-        ["npm:1.3.6", {
-          "packageLocation": "./.yarn/cache/sync-rpc-npm-1.3.6-96e5b4b996-2.zip/node_modules/sync-rpc/",
-          "packageDependencies": [
-            ["sync-rpc", "npm:1.3.6"],
-            ["get-port", "npm:3.2.0"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
       ["synchronous-promise", [
         ["npm:2.0.6", {
           "packageLocation": "./.yarn/cache/synchronous-promise-npm-2.0.6-7b3c6ba0a5-2.zip/node_modules/synchronous-promise/",
@@ -27751,26 +27642,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/text-table-npm-0.2.0-d92a778b59-2.zip/node_modules/text-table/",
           "packageDependencies": [
             ["text-table", "npm:0.2.0"]
-          ],
-          "linkType": "HARD",
-        }]
-      ]],
-      ["then-request", [
-        ["npm:6.0.2", {
-          "packageLocation": "./.yarn/cache/then-request-npm-6.0.2-d89438d618-2.zip/node_modules/then-request/",
-          "packageDependencies": [
-            ["then-request", "npm:6.0.2"],
-            ["@types/concat-stream", "npm:1.6.0"],
-            ["@types/form-data", "npm:0.0.33"],
-            ["@types/node", "npm:8.10.59"],
-            ["@types/qs", "npm:6.9.1"],
-            ["caseless", "npm:0.12.0"],
-            ["concat-stream", "npm:1.6.2"],
-            ["form-data", "npm:2.5.1"],
-            ["http-basic", "npm:8.1.3"],
-            ["http-response-object", "npm:3.0.2"],
-            ["promise", "npm:8.0.3"],
-            ["qs", "npm:6.7.0"]
           ],
           "linkType": "HARD",
         }]
