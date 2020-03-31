fe62694555b5b2476fd68713414a45365a73c4c6
Paul Soporan <paul.soporan@gmail.com>
2020-03-31 20:54:31 +0300

    feat(essentials): make `yarn config get` display objects and arrays as yaml

--- packages/plugin-essentials/sources/commands/config/get.ts
+++ packages/plugin-essentials/sources/commands/config/get.ts
@@ -1,5 +1,6 @@
 import {BaseCommand}                from '@yarnpkg/cli';
 import {Configuration}              from '@yarnpkg/core';
+import {stringifySyml}              from '@yarnpkg/parsers';
 import {Command, Usage, UsageError} from 'clipanion';
 
 // eslint-disable-next-line arca/no-default-export
@@ -19,6 +20,25 @@ export default class ConfigSetCommand extends BaseCommand {
     if (typeof setting === `undefined`)
       throw new UsageError(`Couldn't find a configuration settings named "${this.name}"`);
 
-    this.context.stdout.write(`${configuration.get(this.name)}\n`);
+    const value = convertMapsToObjects(configuration.get(this.name));
+
+    this.context.stdout.write(`${stringifySyml(value)}\n`);
   }
 }
+
+/**
+ * Converts `Maps` to `Objects` recursively.
+ */
+function convertMapsToObjects(arg: unknown) {
+  if (arg instanceof Map)
+    arg = Object.fromEntries(arg);
+  if (typeof arg === 'object' && arg !== null) {
+    for (const key of Object.keys(arg)) {
+      let value = arg[key as keyof object] as unknown;
+      if (typeof value === 'object' && value !== null) {
+        (arg[key as keyof object] as unknown) = convertMapsToObjects(value);
+      }
+    }
+  }
+  return arg;
+};
