68c36499f6b494210d6688ff40392cc577612a49
Paul Soporan <paul.soporan@gmail.com>
2020-04-02 14:30:58 +0300

    test: fix `yarn config get` tests

--- packages/acceptance-tests/pkg-tests-specs/sources/commands/config/get.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/commands/config/get.test.js
@@ -3,9 +3,8 @@ describe(`Commands`, () => {
     test(
       `it should print the requested configuration value for the current directory`,
       makeTemporaryEnv({}, async ({path, run, source}) => {
-        await expect(run(`config`, `get`, `pnpShebang`)).resolves.toMatchObject({
-          stdout: `'#!/usr/bin/env node'\n`,
-        });
+        const {stdout} = await run(`config`, `get`, `pnpShebang`);
+        expect(stdout).toContain(`'#!/usr/bin/env node'`);
       }),
     );
   });
