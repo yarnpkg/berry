528a3764ebe5ac6e6615eba4a0326c263ae4af15
Paul Soporan <paul.soporan@gmail.com>
2020-04-02 19:35:31 +0300

    feat(essentials): make `yarn --clipanion=definitions` output the originating plugins of commands

--- packages/plugin-essentials/sources/commands/entries/clipanion.ts
+++ packages/plugin-essentials/sources/commands/entries/clipanion.ts
@@ -1,12 +1,52 @@
-import {CommandContext} from '@yarnpkg/core';
-import {Command}        from 'clipanion';
+import {CommandContext, Configuration} from '@yarnpkg/core';
+import {Command, Cli}                  from 'clipanion';
+
+type ClipanionDefinition = ReturnType<Cli['definitions']>[number];
+type ExtendedDefinition = ClipanionDefinition & {
+  plugin: {
+    name: string,
+    isDefault: boolean,
+  },
+};
 
 // eslint-disable-next-line arca/no-default-export
 export default class ClipanionCommand extends Command<CommandContext> {
   @Command.Path(`--clipanion=definitions`)
   async execute() {
+    const {plugins} = await Configuration.find(this.context.cwd, this.context.plugins);
+
+    const pluginDefinitions: Array<[string, ClipanionDefinition[]]>  = [];
+    for (const plugin of plugins) {
+      const {commands} = plugin[1];
+      if (commands) {
+        const cli = Cli.from(commands);
+        const definitions = cli.definitions();
+        pluginDefinitions.push([plugin[0], definitions]);
+      }
+    }
+
+    const clipanionDefinitions = this.cli.definitions() as ExtendedDefinition[];
+
+    const arePathsEqual = (path1: string, path2: string) =>
+      path1.split(' ').slice(1).join() === path2.split(' ').slice(1).join();
+
+    const defaultPlugins: string[] = require(`@yarnpkg/cli/package.json`)[`@yarnpkg/builder`].bundles.standard;
+
+    for (const pluginDefinition of pluginDefinitions) {
+      const definitions = pluginDefinition[1];
+
+      for (const definition of definitions) {
+        clipanionDefinitions
+          .find(clipanionDefinition => arePathsEqual(clipanionDefinition.path, definition.path))!
+          .plugin = {
+            name: pluginDefinition[0],
+            isDefault: defaultPlugins.includes(pluginDefinition[0]),
+          };
+      }
+    }
+
     this.context.stdout.write(`${JSON.stringify({
-      commands: this.cli.definitions(),
+      commands: clipanionDefinitions,
     }, null, 2)}\n`);
   }
 }
