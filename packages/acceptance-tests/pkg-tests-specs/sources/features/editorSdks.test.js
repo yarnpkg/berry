ccceb83aaa740b3b0c61304eece3c78b6ae7e919
Maël Nison <nison.mael@gmail.com>
2020-04-03 12:06:24 +0200

    Makes the SDK compatible with nm installs (#1153)
    * Makes the SDK compatible with nm installs
    
    * Adds versions

---
+++ packages/acceptance-tests/pkg-tests-specs/sources/features/editorSdks.test.js
@@ -0,0 +1,135 @@
+import {npath, ppath, xfs} from '@yarnpkg/fslib';
+import {spawn}             from 'child_process';
+
+describe(`Features`, () => {
+  describe(`Editor SDK`, () => {
+    test(
+      `it should allow non-PnP Node to access the binary under a PnP environment`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`eslint`]: `file:./my-eslint`,
+        },
+      }, async ({path, run, source}) => {
+        const binPath = ppath.join(path, `my-eslint/bin/eslint.js`);
+        const manifestPath = ppath.join(path, `my-eslint/package.json`);
+
+        await xfs.mkdirpPromise(ppath.dirname(binPath));
+        await xfs.writeFilePromise(binPath, `console.log(JSON.stringify({wrapper: require('no-deps')}))`);
+
+        await xfs.mkdirpPromise(ppath.dirname(manifestPath));
+        await xfs.writeJsonPromise(manifestPath, {
+          name: `eslint`,
+          version: `1.0.0`,
+          dependencies: {
+            [`no-deps`]: `1.0.0`,
+          },
+        });
+
+        await run(`install`);
+        await pnpify([`--sdk`], path);
+
+        const rawOutput = await noPnpNode([`./.vscode/pnpify/eslint/bin/eslint.js`], path);
+        const jsonOutput = JSON.parse(rawOutput);
+
+        expect(jsonOutput).toMatchObject({
+          wrapper: {
+            name: `no-deps`,
+            version: `1.0.0`,
+          },
+        });
+      }),
+    );
+
+    test(
+      `it shouldn't break under non-PnP installs`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`eslint`]: `file:./my-eslint`,
+        },
+      }, async ({path, run, source}) => {
+        const binPath = ppath.join(path, `my-eslint/bin/eslint.js`);
+        const manifestPath = ppath.join(path, `my-eslint/package.json`);
+
+        await xfs.mkdirpPromise(ppath.dirname(binPath));
+        await xfs.writeFilePromise(binPath, `console.log(JSON.stringify({wrapper: require('no-deps')}))`);
+
+        await xfs.mkdirpPromise(ppath.dirname(manifestPath));
+        await xfs.writeJsonPromise(manifestPath, {
+          name: `eslint`,
+          version: `1.0.0`,
+          dependencies: {
+            [`no-deps`]: `1.0.0`,
+          },
+        });
+
+        await run(`install`);
+        await pnpify([`--sdk`], path);
+
+        await run(`install`, {nodeLinker: `node-modules`});
+        expect(xfs.existsSync(ppath.join(path, `.pnp.js`))).toEqual(false);
+
+        const rawOutput = await noPnpNode([`./.vscode/pnpify/eslint/bin/eslint.js`], path);
+        const jsonOutput = JSON.parse(rawOutput);
+
+        expect(jsonOutput).toMatchObject({
+          wrapper: {
+            name: `no-deps`,
+            version: `1.0.0`,
+          },
+        });
+      }),
+    );
+  });
+});
+
+const noPnpNode = async (args, cwd) => {
+  return new Promise((resolve, reject) => {
+    const child = spawn(process.execPath, [...args], {
+      cwd: npath.fromPortablePath(cwd),
+      stdio: [`ignore`, `pipe`, `inherit`],
+      env: {
+        ...process.env,
+        NODE_OPTIONS: undefined,
+      },
+    });
+
+    child.on(`error`, error => {
+      reject(error);
+    });
+
+    const stdout = [];
+
+    child.stdout.on(`data`, chunk => {
+      stdout.push(chunk);
+    });
+
+    child.on(`close`, code => {
+      if (code === 0) {
+        resolve(Buffer.concat(stdout).toString());
+      } else {
+        reject(new Error(`Process exited with status code ${code}`));
+      }
+    });
+  });
+};
+
+const pnpify = async (args, cwd) => {
+  return new Promise((resolve, reject) => {
+    const child = spawn(process.execPath, [npath.join(__dirname, `../../../../yarnpkg-pnpify/sources/boot-cli-dev.js`), ...args], {
+      cwd: npath.fromPortablePath(cwd),
+      stdio: `ignore`,
+    });
+
+    child.on(`error`, error => {
+      reject(error);
+    });
+
+    child.on(`close`, code => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`Process exited with status code ${code}`));
+      }
+    });
+  });
+};
