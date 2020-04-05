af42bc8c327a5e21f3f47d68fa721531e5bc4730
Paul Soporan <paul.soporan@gmail.com>
2020-04-05 14:52:05 +0300

    test: add test for cache updating

--- packages/acceptance-tests/pkg-tests-specs/sources/protocols/execs.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/protocols/execs.test.js
@@ -63,5 +63,38 @@ describe(`Protocols`, () => {
         });
       }),
     );
+
+    test(
+      `it should update the cache`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`dynamic-pkg`]: `exec:./genpkg.js`,
+        },
+      }, async ({path, run, source}) => {
+        await xfs.writeFilePromise(`${path}/.yarnrc.yml`, stringifySyml({
+          plugins: [require.resolve(`@yarnpkg/monorepo/scripts/plugin-exec.js`)],
+        }));
+
+        await xfs.writeFilePromise(`${path}/genpkg.js`, `
+          const fs = require('fs');
+          fs.mkdirSync('build');
+          fs.writeFileSync('build/index.js', 'module.exports = 42;');
+          fs.writeFileSync('build/package.json', '{}');
+        `);
+
+        await run(`install`);
+
+        await xfs.writeFilePromise(`${path}/genpkg.js`, `
+          const fs = require('fs');
+          fs.mkdirSync('build');
+          fs.writeFileSync('build/index.js', 'module.exports = 100;');
+          fs.writeFileSync('build/package.json', '{}');
+        `);
+
+        await run(`install`);
+
+        await expect(source(`require('dynamic-pkg')`)).resolves.toEqual(100);
+      }),
+    );
   });
 });
