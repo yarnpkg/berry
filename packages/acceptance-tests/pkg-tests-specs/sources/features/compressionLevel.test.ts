f9d8c3f49b17884d2563125d0b7ec2eafcf7caee
Victor Vlasenko <victor.vlasenko@sysgears.com>
2020-04-14 14:58:50 +0300

    Add compression level integration tests

---
+++ packages/acceptance-tests/pkg-tests-specs/sources/features/compressionLevel.test.ts
@@ -0,0 +1,64 @@
+import {xfs, PortablePath, ppath} from '@yarnpkg/fslib';
+
+function computeCacheSize(cacheDir: PortablePath): number {
+  let totalSize = 0;
+  const zipFilenames = xfs.readdirSync(cacheDir);
+  for (const filename of zipFilenames)
+    totalSize += xfs.statSync(ppath.join(cacheDir, filename)).size;
+
+  return totalSize;
+};
+
+describe(`Features`, () => {
+  describe(`Compression Level`, () => {
+    test(
+      `compression level 6 cache size should be less than default max compression cache size`,
+      makeTemporaryEnv(
+        {
+          dependencies: {
+            [`various-requires`]: `1.0.0`,
+          },
+        },
+        async ({path, run}) => {
+          await xfs.writeFilePromise(`${path}/.yarnrc.yml` as PortablePath, ``);
+          const cacheDir = `${path}/.yarn/cache` as PortablePath;
+
+          await run(`install`);
+
+          const levelMaxCacheSize = computeCacheSize(cacheDir);
+
+          await xfs.writeFilePromise(`${path}/.yarnrc.yml` as PortablePath, `compressionLevel: 6\nchecksumBehavior: update\n`);
+
+          await run(`install`);
+
+          expect(levelMaxCacheSize).toBeLessThan(computeCacheSize(cacheDir));
+        },
+      ),
+    );
+
+    test(
+      `compression level 0 cache size should be less than compression level 6 cache size`,
+      makeTemporaryEnv(
+        {
+          dependencies: {
+            [`various-requires`]: `1.0.0`,
+          },
+        },
+        async ({path, run}) => {
+          await xfs.writeFilePromise(`${path}/.yarnrc.yml` as PortablePath, `compressionLevel: 6\n`);
+          const cacheDir = `${path}/.yarn/cache` as PortablePath;
+
+          await run(`install`);
+
+          let level6CacheSize = computeCacheSize(cacheDir);
+
+          await xfs.writeFilePromise(`${path}/.yarnrc.yml` as PortablePath, `compressionLevel: 0\nchecksumBehavior: update\n`);
+
+          await run(`install`);
+
+          expect(level6CacheSize).toBeLessThan(computeCacheSize(cacheDir));
+        },
+      ),
+    );
+  });
+});
