f35e31b92be1701cc06ddc21b131c10330be7e67
Victor Vlasenko <victor.vlasenko@sysgears.com>
2020-04-14 18:51:34 +0300

    Really enable compression level setting (#1197)
    * Enable compression level handling, update compression level setting
    
    * Update configuration docs
    
    * Add compression level integration tests
    
    * Change max to mixed

--- packages/yarnpkg-core/sources/Configuration.ts
+++ packages/yarnpkg-core/sources/Configuration.ts
@@ -197,7 +197,7 @@ export const coreDefinitions: {[coreSettingName: string]: SettingsDefinition} = 
     default: `./.yarn/cache`,
   },
   compressionLevel: {
-    description: `Zip files compression level, from 0 to 9`,
+    description: `Zip files compression level, from 0 to 9 or mixed (a variant of 9, which stores some files uncompressed, when compression doesn't yield good results)`,
     type: SettingsType.NUMBER,
     values: [`mixed`, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     default: DEFAULT_COMPRESSION_LEVEL,
