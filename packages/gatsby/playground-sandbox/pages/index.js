0016c5f47eb881478d4af6a9ed65a385271c0ec5
Paul Soporan <paul.soporan@gmail.com>
2020-04-16 13:57:21 +0300

    fix: fix `window not defined` in sandbox

--- packages/gatsby/playground-sandbox/pages/index.js
+++ packages/gatsby/playground-sandbox/pages/index.js
@@ -1,26 +1,36 @@
-import Head from 'next/head';
+import Head                from 'next/head';
+import {useRef, useEffect} from 'react';
 
 // TODO: replace with `https://yarnpkg.com`
 const WEBSITE_URL = `https://deploy-preview-1193--yarn2.netlify.com`;
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Sherlock Playground</title>
-    </Head>
+const Home = () => {
+  const iframeEl = useRef(null);
 
-    <main>
-      <iframe
-        title="playground"
-        src={`${WEBSITE_URL}/playground/frame${window.location.search}}`}
-        allow="fullscreen"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-      />
-    </main>
-  </div>
-);
+  useEffect(() => {
+    iframeEl.current.src += window.location.search;
+  }, []);
+
+  return (
+    <div className="container">
+      <Head>
+        <title>Sherlock Playground</title>
+      </Head>
+
+      <main>
+        <iframe
+          ref={iframeEl}
+          title="playground"
+          src={`${WEBSITE_URL}/playground/frame`}
+          allow="fullscreen"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+        />
+      </main>
+    </div>
+  );
+};
 
 // eslint-disable-next-line arca/no-default-export
 export default Home;
