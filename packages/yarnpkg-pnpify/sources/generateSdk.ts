0106d465a85c66847985df15f7f8f22edfa79efa
merceyz <merceyz@users.noreply.github.com>
2020-04-22 20:27:27 +0200

    feat(pnpify): set typescript.enablePromptUseWorkspaceTsdk to true

--- packages/yarnpkg-pnpify/sources/generateSdk.ts
+++ packages/yarnpkg-pnpify/sources/generateSdk.ts
@@ -197,6 +197,7 @@ const generateTypescriptWrapper = async (pnpApi: PnpApi, target: PortablePath) =
         ),
       ),
     ),
+    [`typescript.enablePromptUseWorkspaceTsdk`]: true,
   });
 };
 
