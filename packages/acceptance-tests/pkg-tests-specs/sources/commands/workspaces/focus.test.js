8ff96203c948a0efc1566867e7c8cbb656cbc05c
Maël Nison <nison.mael@gmail.com>
2020-04-30 12:08:42 +0200

    Adds multi-workspace focus support, production flag

--- packages/acceptance-tests/pkg-tests-specs/sources/commands/workspaces/focus.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/commands/workspaces/focus.test.js
@@ -30,6 +30,34 @@ describe(`Commands`, () => {
     );
 
     test(
+      `should install the dependencies for specified workspaces only`,
+      makeTemporaryEnv(
+        {
+          private: true,
+          workspaces: [`packages/*`],
+        },
+        async ({path, run}) => {
+          await setupProject(path);
+
+          await run(`install`);
+
+          const cacheFolder = ppath.join(path, `.yarn/cache`);
+          await xfs.removePromise(cacheFolder);
+
+          await run(`workspaces`, `focus`, `foo`, `bar`, {
+            cwd: ppath.join(path, `packages/foo`),
+          });
+
+          await expect(xfs.readdirPromise(cacheFolder)).resolves.toEqual([
+            `.gitignore`,
+            expect.stringContaining(`no-deps-npm-1.0.0-`),
+            expect.stringContaining(`no-deps-npm-2.0.0-`),
+          ]);
+        }
+      )
+    );
+
+    test(
       `should follow local workspace dependencies`,
       makeTemporaryEnv(
         {
@@ -55,18 +83,74 @@ describe(`Commands`, () => {
         }
       )
     );
+
+    test(
+      `should install development dependencies by default`,
+      makeTemporaryEnv(
+        {
+          private: true,
+          workspaces: [`packages/*`],
+        },
+        async ({path, run}) => {
+          await setupProject(path);
+
+          await run(`install`);
+
+          const cacheFolder = ppath.join(path, `.yarn/cache`);
+          await xfs.removePromise(cacheFolder);
+
+          await run(`workspaces`, `focus`, `qux`, {
+            cwd: path,
+          });
+
+          await expect(xfs.readdirPromise(cacheFolder)).resolves.toEqual([
+            `.gitignore`,
+            expect.stringContaining(`no-deps-bins-npm-1.0.0-`),
+            expect.stringContaining(`no-deps-npm-1.0.0-`),
+          ]);
+        }
+      )
+    );
+
+    test(
+      `should only install production dependencies if requested`,
+      makeTemporaryEnv(
+        {
+          private: true,
+          workspaces: [`packages/*`],
+        },
+        async ({path, run}) => {
+          await setupProject(path);
+
+          await run(`install`);
+
+          const cacheFolder = ppath.join(path, `.yarn/cache`);
+          await xfs.removePromise(cacheFolder);
+
+          await run(`workspaces`, `focus`, `qux`, `--production`, {
+            cwd: path,
+          });
+
+          await expect(xfs.readdirPromise(cacheFolder)).resolves.toEqual([
+            `.gitignore`,
+            expect.stringContaining(`no-deps-npm-1.0.0-`),
+          ]);
+        }
+      )
+    );
   });
 });
 
 async function setupProject(path) {
   await xfs.writeFilePromise(ppath.join(path, `.yarnrc.yml`), `plugins:\n  - ${JSON.stringify(require.resolve(`@yarnpkg/monorepo/scripts/plugin-workspace-tools.js`))}\n`);
 
-  const pkg = async (name, dependencies) => {
+  const pkg = async (name, dependencies, devDependencies) => {
     await xfs.mkdirpPromise(ppath.join(path, `packages/${name}`));
-    await xfs.writeJsonPromise(ppath.join(path, `packages/${name}/package.json`), {name, dependencies});
+    await xfs.writeJsonPromise(ppath.join(path, `packages/${name}/package.json`), {name, dependencies, devDependencies});
   };
 
   await pkg(`foo`, {[`no-deps`]: `1.0.0`});
   await pkg(`bar`, {[`no-deps`]: `2.0.0`});
   await pkg(`baz`, {[`bar`]: `workspace:*`});
+  await pkg(`qux`, {[`no-deps`]: `1.0.0`}, {[`no-deps-bins`]: `1.0.0`});
 }
--- packages/plugin-workspace-tools/sources/commands/focus.ts
+++ packages/plugin-workspace-tools/sources/commands/focus.ts
@@ -1,19 +1,28 @@
 import {BaseCommand, WorkspaceRequiredError}                              from '@yarnpkg/cli';
 import {Cache, Configuration, Manifest, Project, StreamReport, Workspace} from '@yarnpkg/core';
+import {structUtils}                                                      from '@yarnpkg/core';
 import {Command, Usage}                                                   from 'clipanion';
 
 // eslint-disable-next-line arca/no-default-export
 export default class WorkspacesFocus extends BaseCommand {
+  @Command.Rest()
+  workspaces: Array<string> = [];
+
   @Command.Boolean(`--json`)
   json: boolean = false;
+
+  @Command.Boolean(`--production`)
+  production: boolean = false;
 
   static usage: Usage = Command.Usage({
     category: `Workspace-related commands`,
     description: `install a single workspace and its dependencies`,
     details: `
-      This command will run an install as if the current workspace (and all other workspaces it depends on) was the only one in the project.
+      This command will run an install as if the specified workspaces (and all other workspaces they depend on) were the only ones in the project. If no workspaces are explicitly listed, the active one will be assumed.
 
       Note that this command is only very moderately useful when using zero-installs, since the cache will contain all the packages anyway - meaning that the only difference between a full install and a focused install would just be a few extra lines in the \`.pnp.js\` file, at the cost of introducing an extra complexity.
+
+      If the \`--production\` flag is set, only regular dependencies will be installed, and dev dependencies will be omitted.
 
       If the \`--json\` flag is set the output will follow a JSON-stream output also known as NDJSON (https://github.com/ndjson/ndjson-spec).
     `,
@@ -25,10 +34,17 @@ export default class WorkspacesFocus extends BaseCommand {
     const {project, workspace} = await Project.find(configuration, this.context.cwd);
     const cache = await Cache.find(configuration);
 
-    if (!workspace)
-      throw new WorkspaceRequiredError(project.cwd, this.context.cwd);
+    let requiredWorkspaces: Set<Workspace>;
+    if (this.workspaces.length === 0) {
+      if (!workspace)
+        throw new WorkspaceRequiredError(project.cwd, this.context.cwd);
 
-    const requiredWorkspaces = new Set<Workspace>([workspace]);
+      requiredWorkspaces = new Set([workspace]);
+    } else {
+      requiredWorkspaces = new Set(this.workspaces.map(name => {
+        return project.getWorkspaceByIdent(structUtils.parseIdent(name));
+      }));
+    }
 
     // First we compute the dependency chain to see what workspaces are
     // dependencies of the one we're trying to focus on.
@@ -53,11 +69,15 @@ export default class WorkspacesFocus extends BaseCommand {
     // their dependencies.
 
     for (const workspace of project.workspaces) {
-      if (requiredWorkspaces.has(workspace))
-        continue;
-
-      workspace.manifest.dependencies.clear();
-      workspace.manifest.peerDependencies.clear();
+      if (requiredWorkspaces.has(workspace)) {
+        if (this.production) {
+          workspace.manifest.devDependencies.clear();
+        }
+      } else {
+        workspace.manifest.dependencies.clear();
+        workspace.manifest.devDependencies.clear();
+        workspace.manifest.peerDependencies.clear();
+      }
     }
 
     // And finally we can run the install, but we have to make sure we don't
