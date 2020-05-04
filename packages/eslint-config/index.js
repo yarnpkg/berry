1f368b6add7c095753b6633884ceb74537b5ed3f
Paul Soporan <paul.soporan@gmail.com>
2020-05-04 17:02:04 +0300

    chore(eslint): create eslint-config package

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    `./rules/best-practices`,
+    `./rules/errors`,
+    `./rules/react`,
+    `./rules/style`,
+    `./rules/typescript`,
+  ].map(require.resolve),
+
+  parser: require.resolve(`@typescript-eslint/parser`),
+
+  env: {
+    node: true,
+    es6: true,
+  },
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: `module`,
+    ecmaFeatures: {
+      modules: true,
+    },
+  }
+};
