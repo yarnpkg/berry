1003d63816a7b7987c8bb665d9474cf06970c1f4
Bill Wanjohi <kaguru@gmail.com>
2020-05-04 16:35:45 -0400

    [gatsby] refine targets for header hyperlinks (#1283)
    * refine targets for header hyperlinks
    
    When I hover over the banner that says,
         "For the 1.x doc", I get a hyperlink, and so I clicked that
         expecting to be taken to v1 documentation.
    
    Unfortunately that part of the sentence is linked to the berry
    repository. When I arrived there, I assumed that I just needed to switch
    to a v1.x branch or tag, but no such things exist on that repository.
    
    After bouncing around a bit more, the second time I clicked on the
    correct portion of the banner and was taken to production documentation.
    
    I'm hoping that this change can help others avoid confusion.
    
    * simplify to just one banner link

--- packages/gatsby/src/components/header.js
+++ packages/gatsby/src/components/header.js
@@ -289,9 +289,9 @@ export const Header = ({children}) => {
   return <>
     <HeaderContainer>
       <NewsContainer>
-        <NewsOverlay href={`https://github.com/yarnpkg/berry`} />
+        <NewsOverlay href={`https://classic.yarnpkg.com`}/>
         <NewsInner>
-          <Highlight>Important:</Highlight> This documentation covers Yarn 2. For the 1.x doc, check <a href={`https://classic.yarnpkg.com`}>classic.yarnpkg.com</a>.
+          <Highlight>Important:</Highlight> This documentation covers Yarn 2. For 1.x docs, see classic.yarnpkg.com.
         </NewsInner>
       </NewsContainer>
 
