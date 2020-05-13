8fe9c396731f42c44b95c7a20786c77e73ffd441
Paul Soporan <paul.soporan@gmail.com>
2020-05-13 09:02:24 +0300

    feat(shell): implement shell cli (#1347)
    * feat(shell): implement shell cli
    
    * chore(release-workflow): set releases
    
    * Update packages/yarnpkg-shell/sources/commands/entry.ts
    
    Co-authored-by: Maël Nison <nison.mael@gmail.com>
    
    * refactor(shell): change location of default declaration
    
    Co-authored-by: Maël Nison <nison.mael@gmail.com>

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: [
     `@babel/plugin-transform-modules-commonjs`,
     `@babel/plugin-proposal-optional-chaining`,
+    `@babel/plugin-proposal-nullish-coalescing-operator`,
     [`@babel/plugin-proposal-decorators`, {legacy: true}],
     [`@babel/plugin-proposal-class-properties`, {loose: true}],
     `@babel/plugin-proposal-async-generator-functions`,
