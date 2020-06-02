b31718164edd895116f0c4e7df22623b2c04f9d5
Maël Nison <nison.mael@gmail.com>
2020-06-02 18:21:10 +0200

    Updates the SDK

--- .vscode/pnpify/typescript/lib/typescript.js
+++ .vscode/pnpify/typescript/lib/typescript.js
@@ -10,8 +10,10 @@ const absPnpApiPath = resolve(__dirname, relPnpApiPath);
 const absRequire = (createRequire || createRequireFromPath)(absPnpApiPath);
 
 if (existsSync(absPnpApiPath)) {
-  // Setup the environment to be able to require typescript/lib/typescript.js
-  require(absPnpApiPath).setup();
+  if (!process.versions.pnp) {
+    // Setup the environment to be able to require typescript/lib/typescript.js
+    require(absPnpApiPath).setup();
+  }
 }
 
 // Defer to the real typescript/lib/typescript.js your application uses
