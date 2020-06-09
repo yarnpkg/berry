7dfce5e82f75f7f779987686809a13a1d73dd016
Paul Soporan <paul.soporan@gmail.com>
2020-06-09 11:13:40 +0300

    chore: run pnpify sdk

--- .yarn/pnpify/typescript/lib/tsserver.js
+++ .yarn/pnpify/typescript/lib/tsserver.js
@@ -13,21 +13,26 @@ const moduleWrapper = tsserver => {
   // VSCode sends the zip paths to TS using the "zip://" prefix, that TS
   // doesn't understand. This layer makes sure to remove the protocol
   // before forwarding it to TS, and to add it back on all returned paths.
+
   const {isAbsolute} = require(`path`);
+
   const Session = tsserver.server.Session;
   const {onMessage: originalOnMessage, send: originalSend} = Session.prototype;
+
   return Object.assign(Session.prototype, {
     onMessage(/** @type {string} */ message) {
       return originalOnMessage.call(this, JSON.stringify(JSON.parse(message), (key, value) => {
         return typeof value === 'string' ? removeZipPrefix(value) : value;
       }));
     },
+
     send(/** @type {any} */ msg) {
       return originalSend.call(this, JSON.parse(JSON.stringify(msg, (key, value) => {
         return typeof value === 'string' ? addZipPrefix(value) : value;
       })));
     }
   });
+
   function addZipPrefix(str) {
     if (isAbsolute(str) && str.match(/\.zip\//) && !str.match(/^zip:/)) {
       return `zip:${str}`;
@@ -35,6 +40,7 @@ const moduleWrapper = tsserver => {
       return str;
     }
   }
+
   function removeZipPrefix(str) {
     return str.replace(/^zip:/, ``);
   }
