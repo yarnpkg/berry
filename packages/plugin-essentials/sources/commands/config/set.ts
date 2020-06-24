9cad653c5aa61a73bafe78822dea05428ea3d109
Maël Nison <nison.mael@gmail.com>
2020-06-24 19:06:16 +0200

    Makes `yarn config get` return native paths (#1514)
    * Forces config get to return native paths
    
    * Also changes set
    
    * Fixes references
    
    * Linting
    
    * Fixes two extra references
    
    * Fixes tests

--- packages/plugin-essentials/sources/commands/config/set.ts
+++ packages/plugin-essentials/sources/commands/config/set.ts
@@ -74,9 +74,12 @@ export default class ConfigSetCommand extends BaseCommand {
     });
 
     const updatedConfiguration = await Configuration.find(this.context.cwd, this.context.plugins);
-    const redactedValue = updatedConfiguration.getRedacted(name);
+    const displayedValue = updatedConfiguration.getSpecial(name, {
+      hideSecrets: true,
+      getNativePaths: true,
+    });
 
-    const asObject = convertMapsToObjects(redactedValue);
+    const asObject = convertMapsToObjects(displayedValue);
     const requestedObject = path
       ? getPath(asObject, path)
       : asObject;
