132abe01499936611d8dd03e4a48a2ce7a7cf768
merceyz <merceyz@users.noreply.github.com>
2020-07-10 23:06:34 +0200

    test(fslib): refactor ZipOpenFs tests

--- packages/yarnpkg-fslib/tests/ZipOpenFS.test.ts
+++ packages/yarnpkg-fslib/tests/ZipOpenFS.test.ts
@@ -3,32 +3,23 @@ import {getLibzipSync}          from '@yarnpkg/libzip';
 import {ppath, npath, Filename} from '../sources/path';
 import {ZipOpenFS}              from '../sources';
 
+const ZIP_FILE1 = ppath.join(npath.toPortablePath(__dirname),`fixtures/foo.zip/foo.txt` as Filename);
+const ZIP_FILE2 = ppath.join(npath.toPortablePath(__dirname), `fixtures/folder.zip/foo.zip/foo.txt` as Filename);
+
 describe(`ZipOpenFS`, () => {
   it(`can read from a zip file`, () => {
     const fs = new ZipOpenFS({libzip: getLibzipSync()});
 
-    const content = fs.readFileSync(
-      ppath.join(
-        npath.toPortablePath(__dirname),
-        `fixtures/foo.zip/foo.txt` as Filename
-      ),
-      `utf8`
-    );
+    expect(fs.readFileSync(ZIP_FILE1, `utf8`)).toEqual(`foo\n`);
 
-    expect(content).toEqual(`foo\n`);
+    fs.discardAndClose();
   });
 
   it(`can read from a zip file in a path containing .zip`, () => {
     const fs = new ZipOpenFS({libzip: getLibzipSync()});
 
-    const content = fs.readFileSync(
-      ppath.join(
-        npath.toPortablePath(__dirname),
-        `fixtures/folder.zip/foo.zip/foo.txt` as Filename
-      ),
-      `utf8`
-    );
+    expect(fs.readFileSync(ZIP_FILE2, `utf8`)).toEqual(`foo\n`);
 
-    expect(content).toEqual(`foo\n`);
+    fs.discardAndClose();
   });
 });
