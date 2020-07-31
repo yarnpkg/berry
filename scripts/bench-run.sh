8d51758ec42629fc0cf1585db4cee4028ecee4bf
Maël Nison <nison.mael@gmail.com>
2020-07-31 11:48:58 +0200

    Adds basic benchmark framework

---
+++ scripts/bench-run.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+
+HERE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+TEMP_DIR="$(mktemp -d)"
+
+cd "${TEMP_DIR}"
+
+# Otherwise git commit doesn't work, and some tools require it
+git config --global user.email "you@example.com"
+git config --global user.name "John Doe"
+
+prepare_yarn() {
+  (cd "$HERE_DIR" && node ./run-yarn.js build:cli --no-minify))
+  echo "yarnPath: '${HERE_DIR}/../packages/yarnpkg-cli/bundles/yarn.js'" >> .yarnrc.yml
+}
+
+wget https://github.com/sharkdp/hyperfine/releases/download/v1.10.0/hyperfine_1.10.0_amd64.deb
+sudo dpkg -i hyperfine_1.10.0_amd64.deb
+
+bench() {
+  hyperfine "$@"
+}
+
+cp "$HERE_DIR"/benchmarks/"$2".json package.json
+
+case $1 in
+  yarn)
+    prepare_yarn
+    bench install-full-cold --min-runs=5 -p 'rm -rf .yarn .pnp.* && yarn cache clean --all' 'yarn install'
+    ;;
+  yarn-nm)
+    prepare_yarn
+    bench install-full-cold --min-runs=5 -p 'rm -rf .yarn node_modules && yarn cache clean --all' 'YARN_NODE_LINKER=node-modules yarn install'
+    ;;
+  npm)
+    npm install -g npm
+    bench install-full-cold --min-runs=5 -p 'rm -rf node_modules && npm cache clean' 'npm ci'
+    ;;
+  pnpm)
+    npm install -g pnpm
+    bench install-full-cold --min-runs=5 -p 'rm -rf node_modules && pnpm cache clean' 'pnpm install'
+    ;;
+  *)
+    echo "Invalid package manager ${$1}"
+    return 1;;
+esac
