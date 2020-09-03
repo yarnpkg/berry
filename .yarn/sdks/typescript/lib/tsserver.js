cd38a98e70f4e36bbd585377f9c0da0f0e158aff
merceyz <merceyz@users.noreply.github.com>
2020-09-03 23:41:53 +0200

    fix(pnpify): prefix zip protocol with a caret for VSCode 1.48

--- .yarn/sdks/typescript/lib/tsserver.js
+++ .yarn/sdks/typescript/lib/tsserver.js
@@ -35,11 +35,13 @@ const moduleWrapper = tsserver => {
 
   function addZipPrefix(str) {
     // We add the `zip:` prefix to both `.zip/` paths and virtual paths
-    if (isAbsolute(str) && !str.match(/^zip:/) && (str.match(/\.zip\//) || str.match(/\$\$virtual\//))) {
+    if (isAbsolute(str) && !str.match(/^\^zip:/) && (str.match(/\.zip\//) || str.match(/\$\$virtual\//))) {
       // Absolute VSCode `Uri.fsPath`s need to start with a slash.
       // VSCode only adds it automatically for supported schemes,
       // so we have to do it manually for the `zip` scheme.
-      return `zip:${str.replace(/^\/?/, `/`)}`;
+      // The path needs to start with a caret otherwise VSCode doesn't handle the protocol
+      // https://github.com/microsoft/vscode/issues/105014#issuecomment-686760910
+      return `^zip:${str.replace(/^\/?/, `/`)}`;
     } else {
       return str;
     }
