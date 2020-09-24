13b00d71afe9fd9c3179c208e1a93b9bb5ad8e4c
Yoann Moinet <yoann.moinet@datadoghq.com>
2020-09-24 18:18:39 +0200

    Remove useless configuration

--- packages/yarnpkg-builder/sources/tools/makeConfig.ts
+++ packages/yarnpkg-builder/sources/tools/makeConfig.ts
@@ -80,8 +80,6 @@ export const makeConfig = (config: webpack.Configuration): webpack.Configuration
 
   plugins: [
     process.env.BUILD_MONITORING_ENABLED && new (require(`@datadog/build-plugin/dist/webpack`).BuildPlugin)({
-      disabled: !process.env.BUILD_MONITORING_ENABLED,
-      output: true,
       context: npath.join(process.cwd(), `../..`),
       datadog: {
         apiKey: process.env.DD_API_KEY,
