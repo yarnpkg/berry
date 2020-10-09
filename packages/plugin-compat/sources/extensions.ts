99aead861301ca97b54075f0d2ea866904d33c2c
Naser Mohd Baig <56202963+naser-baig-au4@users.noreply.github.com>
2020-10-09 15:04:43 +0530

    Update extensions.ts
    Adding new entries to `plugin-compat database` with the PR's I made

--- packages/plugin-compat/sources/extensions.ts
+++ packages/plugin-compat/sources/extensions.ts
@@ -149,4 +149,17 @@ export const packageExtensions: Array<[string, any]> = [
       "react-dom": `^15.0.0 || ^16.0.0`,
     },
   }],
+  // https://github.com/react-bootstrap-table/react-bootstrap-table2/pull/1491
+  [`react-bootstrap-table2-paginator@*`, {
+    dependencies: {
+      "classnames": "^2.2.6"
+    },
+  }],
+  // https://github.com/STRML/react-draggable/pull/525
+  [`react-draggable@*`, {
+    dependencies: {
+    "react": "^16.13.1",
+    "react-dom": "^16.13.1"
+    },
+  }],
 ];
