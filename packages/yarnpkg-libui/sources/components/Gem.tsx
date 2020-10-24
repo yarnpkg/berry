459d1c3eaa5547a3dccc8e7a5f833cb8056205b1
Chris Trevino <darthtrevino@gmail.com>
2020-10-24 13:02:25 -0700

    use memo(), extract gem props into interface

--- packages/yarnpkg-libui/sources/components/Gem.tsx
+++ packages/yarnpkg-libui/sources/components/Gem.tsx
@@ -1,7 +1,10 @@
-import {Text} from 'ink';
-import React  from 'react';
+import {Text}        from 'ink';
+import React, {memo} from 'react';
 
-export const Gem: React.FC<{active: boolean}> = ({active}) => {
+export interface GemProps {
+  active: boolean
+}
+export const Gem: React.FC<GemProps> = memo(({active}) => {
   return active ? <Text color="green">{`◉`}</Text>
     :  <Text color="yellow">{`◯`}</Text>;
-};
+});
