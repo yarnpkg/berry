93d260d8029d3c282bccb725232c4e61acd81ccf
larry1123 <larry1123@larry1123.net>
2020-10-26 04:18:09 -0400

    supports `#source=true&workspace=name`
    supports the same workspace logic provided in `plugin-git`

--- packages/plugin-http/sources/TarballHttpFetcher.ts
+++ packages/plugin-http/sources/TarballHttpFetcher.ts
@@ -1,8 +1,10 @@
-import {Fetcher, FetchOptions, MinimalFetchOptions} from '@yarnpkg/core';
-import {Locator}                                    from '@yarnpkg/core';
-import {httpUtils, structUtils, tgzUtils}           from '@yarnpkg/core';
+import {Fetcher, FetchOptions, MinimalFetchOptions}    from '@yarnpkg/core';
+import {Locator}                                       from '@yarnpkg/core';
+import {httpUtils, scriptUtils, structUtils, tgzUtils} from '@yarnpkg/core';
+import {PortablePath, CwdFS, ppath, xfs}               from '@yarnpkg/fslib';
+import querystring                                     from 'querystring';
 
-import {TARBALL_REGEXP, PROTOCOL_REGEXP}            from './constants';
+import {TARBALL_REGEXP, PROTOCOL_REGEXP}               from './constants';
 
 export class TarballHttpFetcher implements Fetcher {
   supports(locator: Locator, opts: MinimalFetchOptions) {
@@ -38,14 +40,50 @@ export class TarballHttpFetcher implements Fetcher {
   }
 
   async fetchFromNetwork(locator: Locator, opts: FetchOptions) {
-    const sourceBuffer = await httpUtils.get(locator.reference, {
+    let source = false;
+    let workspace: string | undefined;
+    let url = locator.reference;
+    const hashIndex = url.indexOf(`#`);
+    if (hashIndex !== -1) {
+      url = url.slice(0, hashIndex);
+      const extra = querystring.parse(url.slice(hashIndex + 1));
+      source = !!extra?.source;
+      if (source && extra.workspace) {
+        workspace = Array.isArray(extra.workspace) ? extra.workspace[0] : extra.workspace;
+      }
+    }
+
+    let sourceBuffer = await httpUtils.get(url, {
       configuration: opts.project.configuration,
     });
+
+    if (source)
+      sourceBuffer = await this.packageWorkspace(sourceBuffer, workspace, opts);
 
     return await tgzUtils.convertToZip(sourceBuffer, {
       compressionLevel: opts.project.configuration.get(`compressionLevel`),
       prefixPath: structUtils.getIdentVendorPath(locator),
       stripComponents: 1,
+    });
+  }
+
+  async packageWorkspace(sourceBuffer: Buffer, workspace: string | undefined, opts: FetchOptions) {
+    return await xfs.mktempPromise(async extractPath => {
+      const extractTarget = new CwdFS(extractPath);
+
+      await tgzUtils.extractArchiveTo(sourceBuffer, extractTarget, {
+        stripComponents: 1,
+      });
+
+      const packagePath = ppath.join(extractPath, `package.tgz` as PortablePath);
+
+      await scriptUtils.prepareExternalProject(extractPath, packagePath, {
+        configuration: opts.project.configuration,
+        report: opts.report,
+        workspace,
+      });
+
+      return await xfs.readFilePromise(packagePath);
     });
   }
 }
