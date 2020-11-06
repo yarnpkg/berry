5cd0a00817be38fb3ac5209fc31ccd760dd634aa
merceyz <merceyz@users.noreply.github.com>
2020-11-06 23:12:22 +0100

    test: check glob is not expanded

--- packages/acceptance-tests/pkg-tests-specs/sources/commands/exec.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/commands/exec.test.js
@@ -1,3 +1,5 @@
+const {xfs} = require(`@yarnpkg/fslib`);
+
 describe(`Commands`, () => {
   describe(`exec`, () => {
     test(
@@ -7,6 +9,20 @@ describe(`Commands`, () => {
         await expect(run(`exec`, `run`, `foo`)).rejects.toMatchObject({
           code: 1,
           stdout: expect.stringContaining(`Usage Error: Couldn't find a script named "foo"`),
+        });
+      })
+    );
+
+    test(
+      `it should not expand glob patterns`,
+      makeTemporaryEnv({}, async ({path, run, source}) => {
+        await run(`install`);
+
+        await xfs.writeFilePromise(`${path}/echo.js`, `process.stdout.write(process.argv[2])`);
+
+        await expect(run(`exec`, `node`, `echo.js`, `*.js`)).resolves.toMatchObject({
+          code: 0,
+          stdout: expect.stringContaining(`*.js`),
         });
       })
     );
