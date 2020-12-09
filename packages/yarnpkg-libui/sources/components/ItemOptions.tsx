8f341d754e3a5fe896ecc95fcb42304413e1192b
Ross Hendry <rhendry@googlemail.com>
2020-12-09 14:46:36 +0000

    fix: Display of latest and range matches (#2221)
    * fix: Display of latest and range matches
    
    When we don't have a range match, but we do have a latest match, show it
    in the correct column.
    
    fixes #2200
    
    * Patch bumps
    
    * fix: treat options differently to provided values
    
    Always pass in an equal number of options for consistency, and only
    consider those options that have a label to be selectable.
    
    * Remove misleading comment
    
    * Stylistic change
    
    * Linting
    
    Co-authored-by: Maël Nison <nison.mael@gmail.com>

--- packages/yarnpkg-libui/sources/components/ItemOptions.tsx
+++ packages/yarnpkg-libui/sources/components/ItemOptions.tsx
@@ -21,8 +21,12 @@ export const ItemOptions = function <T>({
   onChange: (value: T) => void,
   sizes?: Array<number>
 }) {
-  const values = options.map(({value}) => value);
-  const selectedIndex = values.indexOf(value);
+  // Our possible values are those that have been provided with a label so that
+  // the user can see what they're selecting.
+  const values = options.filter(({label}) => !!label).map(({value}) => value);
+  const selectedIndex = options.findIndex(o => {
+    return o.value === value && o.label != ``;
+  });
 
   useListInput(value, values, {
     active,
@@ -35,18 +39,26 @@ export const ItemOptions = function <T>({
     {options.map(({label}, index) => {
       const isGemActive = index === selectedIndex;
       const boxWidth = sizes[index] -1 || 0;
+
       const simpleLabel = label
         // https://stackoverflow.com/a/29497680
         // eslint-disable-next-line no-control-regex
         .replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, ``);
 
       const padWidth = Math.max(0, boxWidth - (simpleLabel).length - 2);
+
+      if (!label) {
+        return (
+          <Box key={`spacer-${index}`} width={boxWidth} marginLeft={1}/>
+        );
+      }
+
       return (
         <Box key={label} width={boxWidth} marginLeft={1}>
-          <Text wrap="truncate">
+          <Text wrap={`truncate`}>
             <Gem active={isGemActive} />{` `}{label}
           </Text>
-          { skewer ? <Pad active={active} length={padWidth}/> : null }
+          {skewer ? <Pad active={active} length={padWidth}/> : null}
         </Box>
       );
     })}
