ddccbb6aa1002af67173788836b555eed37299a9
merceyz <merceyz@users.noreply.github.com>
2020-12-24 16:29:21 +0100

    fix(builder): ensure version segment does not start with zero

--- packages/yarnpkg-builder/sources/commands/build/bundle.ts
+++ packages/yarnpkg-builder/sources/commands/build/bundle.ts
@@ -23,7 +23,7 @@ const pkgJsonVersion = (basedir: string): string => {
 const suggestHash = async (basedir: string) => {
   try {
     const unique = await execFile(`git`, [`show`, `-s`, `--pretty=format:%ad.%t`, `--date=short`], {cwd: basedir});
-    return `git.${unique.stdout.trim().replace(/-/g, ``)}`;
+    return `git.${unique.stdout.trim().replace(/-/g, ``).replace(`.`, `.hash-`)}`;
   } catch {
     return null;
   }
