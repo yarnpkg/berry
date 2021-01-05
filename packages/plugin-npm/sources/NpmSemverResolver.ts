095e7a035148613d4abedae7329afe3cec284c08
Cheng Liu <liucheng.tech@outlook.com>
2021-01-05 19:54:47 +0800

    refact(plugin-npm): use mapAndFilter instead of three iterations

--- packages/plugin-npm/sources/NpmSemverResolver.ts
+++ packages/plugin-npm/sources/NpmSemverResolver.ts
@@ -1,12 +1,12 @@
-import {ReportError, MessageName, Resolver, ResolveOptions, MinimalResolveOptions, Manifest, DescriptorHash, Package} from '@yarnpkg/core';
-import {Descriptor, Locator, semverUtils}                                                                             from '@yarnpkg/core';
-import {LinkType}                                                                                                     from '@yarnpkg/core';
-import {structUtils}                                                                                                  from '@yarnpkg/core';
-import semver                                                                                                         from 'semver';
+import {ReportError, MessageName, Resolver, ResolveOptions, MinimalResolveOptions, Manifest, DescriptorHash, Package, miscUtils} from '@yarnpkg/core';
+import {Descriptor, Locator, semverUtils}                                                                                        from '@yarnpkg/core';
+import {LinkType}                                                                                                                from '@yarnpkg/core';
+import {structUtils}                                                                                                             from '@yarnpkg/core';
+import semver                                                                                                                    from 'semver';
 
-import {NpmSemverFetcher}                                                                                             from './NpmSemverFetcher';
-import {PROTOCOL}                                                                                                     from './constants';
-import * as npmHttpUtils                                                                                              from './npmHttpUtils';
+import {NpmSemverFetcher}                                                                                                        from './NpmSemverFetcher';
+import {PROTOCOL}                                                                                                                from './constants';
+import * as npmHttpUtils                                                                                                         from './npmHttpUtils';
 
 const NODE_GYP_IDENT = structUtils.makeIdent(null, `node-gyp`);
 const NODE_GYP_MATCH = /\b(node-gyp|prebuild-install)\b/;
@@ -53,10 +53,17 @@ export class NpmSemverResolver implements Resolver {
       jsonResponse: true,
     });
 
-    const candidates = Object.keys(registryData.versions)
-      .filter(version => !!semverUtils.valid(version))
-      .map(version => new semverUtils.SemVer(version))
-      .filter(version => range.test(version));
+    const candidates = miscUtils.mapAndFilter(Object.keys(registryData.versions), version => {
+      try {
+        const candidate = new semverUtils.SemVer(version);
+        if (range.test(candidate))
+          return candidate;
+
+        return miscUtils.mapAndFilter.skip;
+      } catch (_e) {
+        return miscUtils.mapAndFilter.skip;
+      }
+    });
 
     const noDeprecatedCandidates = candidates.filter(version => {
       return !registryData.versions[version.raw].deprecated;
