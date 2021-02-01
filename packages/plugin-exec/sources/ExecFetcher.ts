ddff62733f39339678bd2cfbf0bcddbb1fb34e0c
Maël Nison <nison.mael@gmail.com>
2021-02-01 17:07:16 +0100

    We cannot access the project in fetchers, because they might execute during resolution

--- packages/plugin-exec/sources/ExecFetcher.ts
+++ packages/plugin-exec/sources/ExecFetcher.ts
@@ -88,7 +88,7 @@ export class ExecFetcher implements Fetcher {
 
   private async generatePackage(cwd: PortablePath, locator: Locator, generatorPath: PortablePath, opts: FetchOptions) {
     return await xfs.mktempPromise(async binFolder => {
-      const env = await scriptUtils.makeScriptEnv({project: opts.project, locator, binFolder});
+      const env = await scriptUtils.makeScriptEnv({project: opts.project, binFolder});
       const runtimeFile = ppath.join(cwd, `runtime.js` as Filename);
 
       return await xfs.mktempPromise(async logDir => {
