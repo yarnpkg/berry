1e6d3f7c479a1b37e4698dada44283ac87be65dd
Maël Nison <nison.mael@gmail.com>
2021-02-14 12:36:00 +0100

    Adds the compat layer directly in the pluginConfiguration

---
+++ packages/yarnpkg-cli/sources/tools/backportClipanionCompatibility.ts
@@ -0,0 +1,12 @@
+export function backportClipanionCompatibility(clipanion: any) {
+  clipanion.Command.Path = (...p: Array<any>) => (instance: any) => {
+    instance.paths = instance.paths || [];
+    instance.paths.push(p);
+  };
+
+  for (const fn of [`Array`, `Boolean`, `String`]) {
+    clipanion.Command[fn] = (...args: Array<any>) => (instance: any, propertyName: any) => {
+      instance[propertyName] = clipanion.Option[fn](...args);
+    };
+  }
+}
