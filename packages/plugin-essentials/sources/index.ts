9cfa71b47a452355d4004d54980a3a260438c2fd
Paul Soporan <paul.soporan@gmail.com>
2021-02-27 15:04:26 +0200

    feat: make enableImmutableInstalls default to true on ci

--- packages/plugin-essentials/sources/index.ts
+++ packages/plugin-essentials/sources/index.ts
@@ -1,5 +1,6 @@
 import {Descriptor, Plugin, SettingsType, Package, formatUtils} from '@yarnpkg/core';
 import {Workspace}                                              from '@yarnpkg/core';
+import {isCI}                                                   from 'ci-info';
 
 import add                                                      from './commands/add';
 import bin                                                      from './commands/bin';
@@ -86,7 +87,7 @@ const plugin: Plugin = {
     enableImmutableInstalls: {
       description: `If true, prevents the install command from modifying the lockfile`,
       type: SettingsType.BOOLEAN,
-      default: false,
+      default: isCI,
     },
 
     defaultSemverRangePrefix: {
