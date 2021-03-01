1ed86ad406c3c675e98caa7d6f713759c4aafdea
Maël Nison <nison.mael@gmail.com>
2021-03-01 12:33:46 +0100

    Removes stop-commands

--- scripts/actions/local-yarn-command/entrypoint.sh
+++ scripts/actions/local-yarn-command/entrypoint.sh
@@ -6,8 +6,4 @@ echo
 # The version of libvips available on alpine doesn't support svg
 export SHARP_IGNORE_GLOBAL_LIBVIPS=1
 
-NONCE="$(cat /proc/sys/kernel/random/uuid | sha256sum | head -c 64)"
-
-echo "::stop-commands::$NONCE"
 exec node ./scripts/run-yarn.js "$@"
-echo "::$NONCE::"
