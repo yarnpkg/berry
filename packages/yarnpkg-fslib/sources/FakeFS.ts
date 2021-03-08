6032f94913f3609e7e32ed84e73a4bdaf37318e0
Maël Nison <nison.mael@gmail.com>
2021-03-08 11:14:00 +0100

    Adds hardlink support to copyPromise (#2573)
    * Adds an option to support copying using hardlinks if needed
    
    * Adds ENOSUP
    
    * Uses null
    
    * Fixes chmod to the destination, not source
    
    * NOT, not NO
    
    * Versions

--- packages/yarnpkg-fslib/sources/FakeFS.ts
+++ packages/yarnpkg-fslib/sources/FakeFS.ts
@@ -2,7 +2,7 @@ import {EventEmitter}                                                           
 import {Dirent as NodeDirent, ReadStream, Stats, WriteStream, NoParamCallback, BigIntStats} from 'fs';
 import {EOL}                                                                                from 'os';
 
-import {copyPromise}                                                                        from './algorithms/copyPromise';
+import {copyPromise, LinkStrategy}                                                          from './algorithms/copyPromise';
 import {FSPath, Path, PortablePath, PathUtils, Filename}                                    from './path';
 import {convertPath, ppath}                                                                 from './path';
 
@@ -392,10 +392,10 @@ export abstract class FakeFS<P extends Path> {
     }
   }
 
-  copyPromise(destination: P, source: P, options?: {baseFs?: undefined, overwrite?: boolean, stableSort?: boolean, stableTime?: boolean}): Promise<void>;
-  copyPromise<P2 extends Path>(destination: P, source: P2, options: {baseFs: FakeFS<P2>, overwrite?: boolean, stableSort?: boolean, stableTime?: boolean}): Promise<void>;
-  async copyPromise<P2 extends Path>(destination: P, source: P2, {baseFs = this as any, overwrite = true, stableSort = false, stableTime = false}: {baseFs?: FakeFS<P2>, overwrite?: boolean, stableSort?: boolean, stableTime?: boolean} = {}) {
-    return await copyPromise(this, destination, baseFs, source, {overwrite, stableSort, stableTime});
+  copyPromise(destination: P, source: P, options?: {baseFs?: undefined, overwrite?: boolean, stableSort?: boolean, stableTime?: boolean, linkStrategy?: LinkStrategy}): Promise<void>;
+  copyPromise<P2 extends Path>(destination: P, source: P2, options: {baseFs: FakeFS<P2>, overwrite?: boolean, stableSort?: boolean, stableTime?: boolean, linkStrategy?: LinkStrategy}): Promise<void>;
+  async copyPromise<P2 extends Path>(destination: P, source: P2, {baseFs = this as any, overwrite = true, stableSort = false, stableTime = false, linkStrategy = null}: {baseFs?: FakeFS<P2>, overwrite?: boolean, stableSort?: boolean, stableTime?: boolean, linkStrategy?: LinkStrategy | null} = {}) {
+    return await copyPromise(this, destination, baseFs, source, {overwrite, stableSort, stableTime, linkStrategy});
   }
 
   /** @deprecated Prefer using `copyPromise` instead */
