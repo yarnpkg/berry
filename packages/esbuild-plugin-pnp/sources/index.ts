0fca0c452f2b0e2c4dd9d6e76957f82da3794a12
merceyz <merceyz@users.noreply.github.com>
2021-03-20 15:36:32 +0100

    fix(esbuild): fallback to default resolver when path isn't under pnp

--- packages/esbuild-plugin-pnp/sources/index.ts
+++ packages/esbuild-plugin-pnp/sources/index.ts
@@ -48,11 +48,10 @@ export function pnpPlugin({
           ? args.importer
           : `${baseDir}/`;
 
-        // In theory we should delegate to the real resolution, but ESBuild
-        // doesn't currently offer any way to do that.
         const pnpApi = findPnpApi(effectiveImporter) as PnpApi | null;
         if (!pnpApi)
-          throw new Error(`Path is external to the project`);
+          // Path isn't controlled by PnP so delegate to the next resolver in the chain
+          return undefined;
 
         const path = pnpApi.resolveRequest(args.path, effectiveImporter, {
           considerBuiltins: true,
