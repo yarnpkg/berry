3adecffddd9318c0f6ad4f41cc1113fa5b83ef11
Maël Nison <nison.mael@gmail.com>
2021-03-29 16:03:11 +0200

    Adds --prerelease

--- packages/plugin-version/sources/commands/version/apply.ts
+++ packages/plugin-version/sources/commands/version/apply.ts
@@ -15,9 +15,16 @@ export default class VersionApplyCommand extends BaseCommand {
     category: `Release-related commands`,
     description: `apply all the deferred version bumps at once`,
     details: `
-      This command will apply the deferred version changes (scheduled via \`yarn version major|minor|patch\`) on the current workspace (or all of them if \`--all\`) is specified.
+      This command will apply the deferred version changes and remove their definitions from the repository.
 
-      It will also update the \`workspace:\` references across all your local workspaces so that they keep refering to the same workspace even after the version bump.
+      Note that if \`--prerelease\` is set, the given prerelease identifier (by default \`rc.%d\`) will be used on all new versions and the version definitions will be kept as-is.
+
+      By default only the current workspace will be bumped, but you can configure this behavior by using one of:
+
+      - \`--recursive\` to also apply the version bump on its dependencies
+      - \`--all\` to apply the version bump on all packages in the repository
+
+      Note that this command will also update the \`workspace:\` references across all your local workspaces, thus ensuring that they keep refering to the same workspaces even after the version bump.
     `,
     examples: [[
       `Apply the version change to the local workspace`,
@@ -30,6 +37,11 @@ export default class VersionApplyCommand extends BaseCommand {
 
   all = Option.Boolean(`--all`, false, {
     description: `Apply the deferred version changes on all workspaces`,
+  });
+
+  prerelease = Option.String(`--prerelease`, {
+    description: `Optionally use a rc identifier when setting versions`,
+    tolerateBoolean: true,
   });
 
   json = Option.Boolean(`--json`, false, {
@@ -53,7 +65,11 @@ export default class VersionApplyCommand extends BaseCommand {
       json: this.json,
       stdout: this.context.stdout,
     }, async report => {
-      let releases = await versionUtils.resolveVersionFiles(project);
+      const prerelease = this.prerelease
+        ? typeof this.prerelease !== `boolean` ? this.prerelease : `rc.%d`
+        : null;
+
+      let releases = await versionUtils.resolveVersionFiles(project, {prerelease});
 
       if (!this.all) {
         const release = releases.get(workspace);
@@ -66,12 +82,15 @@ export default class VersionApplyCommand extends BaseCommand {
         ]]);
       }
 
-      await versionUtils.applyReleases(project, releases, {report});
+      versionUtils.applyReleases(project, releases, {report, prerelease});
 
-      if (this.all)
-        await versionUtils.clearVersionFiles(project);
-      else
-        await versionUtils.updateVersionFiles(project);
+      if (prerelease) {
+        if (this.all) {
+          await versionUtils.clearVersionFiles(project);
+        } else {
+          await versionUtils.updateVersionFiles(project);
+        }
+      }
 
       await project.install({cache, report});
     });
