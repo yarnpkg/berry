867ee7243c517d891acf76da9b5b2adea73b07b2
merceyz <merceyz@users.noreply.github.com>
2021-04-04 20:26:06 +0200

    chore: style

--- packages/plugin-workspace-tools/sources/commands/focus.ts
+++ packages/plugin-workspace-tools/sources/commands/focus.ts
@@ -1,7 +1,7 @@
-import {BaseCommand, WorkspaceRequiredError}                                                from '@yarnpkg/cli';
-import {Cache, Configuration, Manifest, HardDependencies, Project, StreamReport, Workspace} from '@yarnpkg/core';
-import {structUtils}                                                                        from '@yarnpkg/core';
-import {Command, Option, Usage}                                                             from 'clipanion';
+import {BaseCommand, WorkspaceRequiredError}                              from '@yarnpkg/cli';
+import {Cache, Configuration, Manifest, Project, StreamReport, Workspace} from '@yarnpkg/core';
+import {structUtils}                                                      from '@yarnpkg/core';
+import {Command, Option, Usage}                                           from 'clipanion';
 
 // eslint-disable-next-line arca/no-default-export
 export default class WorkspacesFocus extends BaseCommand {
@@ -64,9 +64,8 @@ export default class WorkspacesFocus extends BaseCommand {
     // Note: remember that new elements can be added in a set even while
     // iterating over it (because they're added at the end)
 
-    const dependencyTypes: Array<HardDependencies> = this.production ? [`dependencies`] : Manifest.hardDependencies;
     for (const workspace of requiredWorkspaces) {
-      for (const dependencyType of dependencyTypes) {
+      for (const dependencyType of this.production ? [`dependencies`] : Manifest.hardDependencies) {
         for (const descriptor of workspace.manifest.getForScope(dependencyType).values()) {
           const matchingWorkspace = project.tryWorkspaceByDescriptor(descriptor);
 
