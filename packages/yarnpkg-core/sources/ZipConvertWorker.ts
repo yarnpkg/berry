f31d0161816d3627527c74ce066e3be8e6a42633
Maël Nison <nison.mael@gmail.com>
2021-04-07 09:30:29 +0200

    Update ZipConvertWorker.ts

--- packages/yarnpkg-core/sources/ZipConvertWorker.ts
+++ packages/yarnpkg-core/sources/ZipConvertWorker.ts
@@ -7,18 +7,24 @@ import {extractArchiveTo, ExtractBufferOptions} from './tgzUtils';
 export type ConvertToZipPayload = {tmpFile: PortablePath, tgz: Buffer, opts: ExtractBufferOptions};
 
 if (!parentPort)
-  throw new Error(`Expected parentPort to be set`);
+  throw new Error(`Assertion failed: Expected parentPort to be set`);
 
 parentPort.on(`message`, async (data: ConvertToZipPayload) => {
   const {opts, tgz, tmpFile} = data;
   const {compressionLevel, ...bufferOpts} = opts;
 
   try {
-    const zipfs = await extractArchiveTo(Buffer.from(tgz), new ZipFS(tmpFile, {create: true, libzip: await getLibzipPromise(), level: compressionLevel}), bufferOpts);
+    const zipFs = new ZipFS(tmpFile, {create: true, libzip: await getLibzipPromise(), level: compressionLevel});
+
+    // Buffers sent through Node are turned into regular Uint8Arrays
+    const tgzBuffer = Buffer.from(tgz);
+    await extractArchiveTo(tgzBuffer, zipFs, bufferOpts);
+
     zipfs.saveAndClose();
+
     parentPort!.postMessage(data.tmpFile);
   } catch (err) {
-    console.error(err);
+    console.error(err.stack);
     process.exit(1);
   }
 });
