e81dc0d29bb2f41818d9c5c1c74bab1406fb979b
Maël Nison <nison.mael@gmail.com>
2021-04-09 13:18:54 +0200

    Updates the npm release script

--- scripts/release/03-release-npm.sh
+++ scripts/release/03-release-npm.sh
@@ -10,18 +10,19 @@ if ! [[ -z $(git status --porcelain) ]]; then
   exit 1
 fi
 
+VERSION=$(YARN_IGNORE_PATH=1 node packages/yarnpkg-cli/bin/yarn.js --version)
+
 TEMP_DIR="$(mktemp -d)"
 mkdir "$TEMP_DIR"/bin
 
-cp "$THIS_DIR"/package.json.template "$TEMP_DIR"/package.json
+jq > "$TEMP_DIR"/package.json \
+  '{name: "@yarnpkg/cli-dist", version: "'"$VERSION"'", "bin": {"yarn": "bin/yarn.js", "yarnpkg": "bin/yarn.js"}} + (. | {license,repository,engines})' \
+  "$REPO_DIR"/packages/yarnpkg-cli/package.json
+
 cp "$REPO_DIR"/packages/yarnpkg-cli/bin/yarn.js "$TEMP_DIR"/bin/yarn.js
-
-VERSION=$(YARN_IGNORE_PATH=1 node packages/yarnpkg-cli/bin/yarn.js --version)
-perl -pi -e "s#{{version}}#$VERSION#" "$TEMP_DIR"/package.json
-
 chmod +x "$TEMP_DIR"/bin/yarn.js
 
 cd "$TEMP_DIR"
 
 node "$TEMP_DIR"/bin/yarn.js
-node "$TEMP_DIR"/bin/yarn.js npm publish --tag=berry
+node "$TEMP_DIR"/bin/yarn.js npm publish
