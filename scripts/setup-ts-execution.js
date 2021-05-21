feeb45fcd99437dca3af6d0fad03ef2e2888f8a0
Kristoffer K <merceyz@users.noreply.github.com>
2021-05-21 15:32:15 +0200

    chore: grammar
    Co-authored-by: Paul Soporan <paul.soporan@gmail.com>

--- scripts/setup-ts-execution.js
+++ scripts/setup-ts-execution.js
@@ -3,7 +3,7 @@ const babel = require(`@babel/core`);
 const os = require(`os`);
 const root = path.dirname(__dirname);
 
-// The cache in @babel/register never clears itself and will therefor grow
+// The cache in @babel/register never clears itself and will therefore grow
 // forever causing massive slowdowns if left unchecked for a while
 // this ensures a new cache key is generated every week
 const weeksSinceUNIXEpoch = Math.floor(Date.now() / 604800000);
