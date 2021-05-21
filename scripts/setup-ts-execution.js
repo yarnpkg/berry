2fb1d142b0f2beace1c2867bb639f680ddc43807
Kristoffer K <merceyz@users.noreply.github.com>
2021-05-21 16:17:05 +0200

    chore: clear babel cache on a weekly basis (#2909)
    Co-authored-by: Paul Soporan <paul.soporan@gmail.com>

--- scripts/setup-ts-execution.js
+++ scripts/setup-ts-execution.js
@@ -3,8 +3,13 @@ const babel = require(`@babel/core`);
 const os = require(`os`);
 const root = path.dirname(__dirname);
 
+// The cache in @babel/register never clears itself and will therefore grow
+// forever causing massive slowdowns if left unchecked for a while
+// this ensures a new cache key is generated every week
+const weeksSinceUNIXEpoch = Math.floor(Date.now() / 604800000);
+
 if (!process.env.BABEL_CACHE_PATH)
-  process.env.BABEL_CACHE_PATH = path.join(os.tmpdir(), `babel`, `.babel.${babel.version}.${babel.getEnv()}.json`);
+  process.env.BABEL_CACHE_PATH = path.join(os.tmpdir(), `babel`, `.babel.${babel.version}.${babel.getEnv()}.${weeksSinceUNIXEpoch}.json`);
 
 require(`@babel/register`)({
   root,
