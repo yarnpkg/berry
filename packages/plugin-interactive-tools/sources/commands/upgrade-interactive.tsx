15f536d5540157bcfd825e6798d7d27d2d0e4529
Ethan Wu <ethanwu10@gmail.com>
2021-06-01 05:00:14 -0400

    fix: filter out up-to-date packages correctly (#2937)
    * fix: filter out up-to-date packages correctly
    
    * Update upgrade-interactive.tsx
    
    Co-authored-by: Maël Nison <nison.mael@gmail.com>

--- packages/plugin-interactive-tools/sources/commands/upgrade-interactive.tsx
+++ packages/plugin-interactive-tools/sources/commands/upgrade-interactive.tsx
@@ -230,7 +230,9 @@ export default class UpgradeInteractiveCommand extends BaseCommand {
             const mappedToSuggestions = dependencies.map((descriptor, i) => {
               const suggestionsForDescriptor = allSuggestions[i];
               return [descriptor, suggestionsForDescriptor] as const;
-            }).filter(([_, suggestions]) => suggestions.length > 1);
+            }).filter(([_, suggestions]) => {
+              return suggestions.filter(suggestion => suggestion.label !== ``).length > 1;
+            });
 
             if (mountedRef.current) {
               setSuggestions(mappedToSuggestions);
