0f25f69d54aca89e1e674b359a350413209c3c52
Maël Nison <nison.mael@gmail.com>
2021-07-17 19:10:08 +0200

    Fixes typo

--- packages/acceptance-tests/pkg-tests-specs/sources/features/plugins.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/features/plugins.test.js
@@ -1,50 +1,84 @@
 import {xfs}           from '@yarnpkg/fslib';
 import {stringifySyml} from '@yarnpkg/parsers';
 
-const PLUGIN_A = `
-const factory = () => { console.log('Hello world A'); return {default:{}}; };
-const name = '@yarnpkg/plugin-a';
-module.exports = {factory, name};
-`;
+const PLUGIN = (name, {async = false, printOnBoot = false} = {}) => `
+const factory = ${async ? `async` : ``} r => {
+  const {Command} = r('clipanion');
 
-const PLUGIN_B = `
-const factory = () => { console.log('Hello world B'); return {default:{}}; };
-const name = '@yarnpkg/plugin-b';
+  if (${printOnBoot})
+    console.log('Booting ${name.toUpperCase()}');
+
+  return {
+    default: {
+      commands: [
+        class MyCommand extends Command {
+          static paths = [['${name}']];
+
+          async execute() {
+            this.context.stdout.write('Executing ${name.toUpperCase()}\\n');
+          }
+        },
+      ],
+    },
+  };
+};
+
+const name = '@yarnpkg/plugin-${name}';
 module.exports = {factory, name};
 `;
 
 describe(`Features`, () => {
   describe(`Plugins`, () => {
     test(`it should properly load a plugin via the local rc file`, makeTemporaryEnv({
-      scripts: {
-        nothing: ``,
-      },
     }, async ({path, run, source}) => {
-      await xfs.writeFilePromise(`${path}/plugin-a.js`, PLUGIN_A);
+      await xfs.writeFilePromise(`${path}/plugin-a.js`, PLUGIN(`a`, {printOnBoot: true}));
 
       await xfs.writeFilePromise(`${path}/.yarnrc.yml`, stringifySyml({
         plugins: [`./plugin-a.js`],
       }));
 
       await expect(run(`node`, `-e`, ``)).resolves.toMatchObject({
-        stdout: `Hello world A\nHello world A\n`,
+        stdout: `Booting A\nBooting A\n`,
+      });
+    }));
+
+    test(`it should accept asynchronous plugins`, makeTemporaryEnv({
+    }, async ({path, run, source}) => {
+      await xfs.writeFilePromise(`${path}/plugin-a.js`, PLUGIN(`a`, {async: true}));
+
+      await xfs.writeFilePromise(`${path}/.yarnrc.yml`, stringifySyml({
+        plugins: [`./plugin-a.js`],
+      }));
+
+      await expect(run(`a`)).resolves.toMatchObject({
+        stdout: `Executing A\n`,
+      });
+    }));
+
+    test(`it should properly load a plugin via the local rc file`, makeTemporaryEnv({
+    }, async ({path, run, source}) => {
+      await xfs.writeFilePromise(`${path}/plugin-a.js`, PLUGIN(`A`, {printOnBoot: true}));
+
+      await xfs.writeFilePromise(`${path}/.yarnrc.yml`, stringifySyml({
+        plugins: [`./plugin-a.js`],
+      }));
+
+      await expect(run(`node`, `-e`, ``)).resolves.toMatchObject({
+        stdout: `Booting A\nBooting A\n`,
       });
     }));
 
     test(`it should properly load multiple plugins via the local rc file, in the right order`, makeTemporaryEnv({
-      scripts: {
-        nothing: ``,
-      },
     }, async ({path, run, source}) => {
-      await xfs.writeFilePromise(`${path}/plugin-a.js`, PLUGIN_A);
-      await xfs.writeFilePromise(`${path}/plugin-b.js`, PLUGIN_B);
+      await xfs.writeFilePromise(`${path}/plugin-a.js`, PLUGIN(`A`, {printOnBoot: true}));
+      await xfs.writeFilePromise(`${path}/plugin-b.js`, PLUGIN(`B`, {printOnBoot: true}));
 
       await xfs.writeFilePromise(`${path}/.yarnrc.yml`, stringifySyml({
         plugins: [`./plugin-a.js`, `./plugin-b.js`],
       }));
 
       await expect(run(`node`, `-e`, ``)).resolves.toMatchObject({
-        stdout: `Hello world A\nHello world B\nHello world A\nHello world B\n`,
+        stdout: `Booting A\nBooting B\nBooting A\nBooting B\n`,
       });
     }));
   });
