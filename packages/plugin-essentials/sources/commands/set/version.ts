dbdbe52a5e60799f2a52a14cc733adcb2ff01e48
Paul Soporan <paul.soporan@gmail.com>
2021-07-18 03:23:48 +0300

    feat(set-version): reimplement range support (#3093)
    * feat(set-version): reimplement range support
    
    * refactor: get rid of try/catch
    
    * refactor: change prerelease logic
    
    * docs: document the version specifier
    
    * docs: add clarification
    
    * chore: update clipanion
    
    * chore: dedupe typanion
    
    * chore: versions

--- packages/plugin-essentials/sources/commands/set/version.ts
+++ packages/plugin-essentials/sources/commands/set/version.ts
@@ -5,6 +5,11 @@ import {Filename, PortablePath, ppath, xfs, npath}                              
 import {Command, Option, Usage, UsageError}                                                  from 'clipanion';
 import semver                                                                                from 'semver';
 
+export type Tags = {
+  latest: Record<string, string>;
+  tags: Array<string>;
+};
+
 // eslint-disable-next-line arca/no-default-export
 export default class SetVersionCommand extends BaseCommand {
   static paths = [
@@ -17,19 +22,46 @@ export default class SetVersionCommand extends BaseCommand {
       This command will download a specific release of Yarn directly from the Yarn GitHub repository, will store it inside your project, and will change the \`yarnPath\` settings from your project \`.yarnrc.yml\` file to point to the new file.
 
       A very good use case for this command is to enforce the version of Yarn used by the any single member of your team inside a same project - by doing this you ensure that you have control on Yarn upgrades and downgrades (including on your deployment servers), and get rid of most of the headaches related to someone using a slightly different version and getting a different behavior than you.
+
+      The version specifier can be:
+
+      - a tag:
+        - \`latest\` / \`berry\` / \`stable\` -> the most recent stable berry (\`>=2.0.0\`) release
+        - \`canary\` -> the most recent canary (release candidate) berry (\`>=2.0.0\`) release
+        - \`classic\` -> the most recent classic (\`^0.x || ^1.x\`) release
+
+      - a semver range (e.g. \`2.x\`) -> the most recent version satisfying the range (limited to berry releases)
+
+      - a semver version (e.g. \`2.4.1\`, \`1.22.1\`)
+
+      - a local file referenced through either a relative or absolute path
+
+      - \`self\` -> the version used to invoke the command
     `,
     examples: [[
       `Download the latest release from the Yarn repository`,
       `$0 set version latest`,
     ], [
+      `Download the latest canary release from the Yarn repository`,
+      `$0 set version canary`,
+    ], [
       `Download the latest classic release from the Yarn repository`,
       `$0 set version classic`,
+    ], [
+      `Download the most recent Yarn 3 build`,
+      `$0 set version 3.x`,
     ], [
       `Download a specific Yarn 2 build`,
       `$0 set version 2.0.0-rc.30`,
     ], [
       `Switch back to a specific Yarn 1 release`,
       `$0 set version 1.22.1`,
+    ], [
+      `Use a release from the local filesystem`,
+      `$0 set version ./yarn.cjs`,
+    ], [
+      `Download the version used to invoke the command`,
+      `$0 set version self`,
     ]],
   });
 
@@ -55,9 +87,9 @@ export default class SetVersionCommand extends BaseCommand {
     if (this.version === `self`)
       bundleUrl = getBundlePath();
     else if (this.version === `latest` || this.version === `berry` || this.version === `stable`)
-      bundleUrl = `https://repo.yarnpkg.com/${await findVersion(configuration, `stable`)}/packages/yarnpkg-cli/bin/yarn.js`;
+      bundleUrl = `https://repo.yarnpkg.com/${await resolveTag(configuration, `stable`)}/packages/yarnpkg-cli/bin/yarn.js`;
     else if (this.version === `canary`)
-      bundleUrl = `https://repo.yarnpkg.com/${await findVersion(configuration, `canary`)}/packages/yarnpkg-cli/bin/yarn.js`;
+      bundleUrl = `https://repo.yarnpkg.com/${await resolveTag(configuration, `canary`)}/packages/yarnpkg-cli/bin/yarn.js`;
     else if (this.version === `classic`)
       bundleUrl = `https://nightly.yarnpkg.com/latest.js`;
     else if (this.version.match(/^\.{0,2}[\\/]/) || npath.isAbsolute(this.version))
@@ -67,7 +99,7 @@ export default class SetVersionCommand extends BaseCommand {
     else if (semverUtils.satisfiesWithPrereleases(this.version, `^0.x || ^1.x`))
       bundleUrl = `https://github.com/yarnpkg/yarn/releases/download/v${this.version}/yarn-${this.version}.js`;
     else if (semverUtils.validRange(this.version))
-      throw new UsageError(`Support for ranges got removed - please use the exact version you want to install, or 'latest' to get the latest build available`);
+      bundleUrl = `https://repo.yarnpkg.com/${await resolveRange(configuration, this.version)}/packages/yarnpkg-cli/bin/yarn.js`;
     else
       throw new UsageError(`Invalid version descriptor "${this.version}"`);
 
@@ -94,10 +126,21 @@ export default class SetVersionCommand extends BaseCommand {
   }
 }
 
-export async function findVersion(configuration: Configuration, request: `stable` | `canary`) {
-  const data = await httpUtils.get(`https://repo.yarnpkg.com/tags`, {configuration, jsonResponse: true});
+export async function resolveRange(configuration: Configuration, request: string) {
+  const data: Tags = await httpUtils.get(`https://repo.yarnpkg.com/tags`, {configuration, jsonResponse: true});
+
+  const candidates = data.tags.filter(version => semverUtils.satisfiesWithPrereleases(version, request));
+  if (candidates.length === 0)
+    throw new UsageError(`No matching release found for range ${formatUtils.pretty(configuration, request, formatUtils.Type.RANGE)}.`);
+
+  // The tags on the website are sorted by semver descending
+  return candidates[0];
+}
+
+export async function resolveTag(configuration: Configuration, request: `stable` | `canary`) {
+  const data: Tags = await httpUtils.get(`https://repo.yarnpkg.com/tags`, {configuration, jsonResponse: true});
   if (!data.latest[request])
-    throw new UsageError(`Tag '${request}' not found`);
+    throw new UsageError(`Tag ${formatUtils.pretty(configuration, request, formatUtils.Type.RANGE)} not found`);
 
   return data.latest[request];
 }
