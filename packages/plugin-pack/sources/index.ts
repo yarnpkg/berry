1aaeb5bcab8b92869d196bf66cc84d28b629a63f
王清雨 <colinwang.0616@gmail.com>
2021-08-09 13:09:17 +0800

    Update packages/plugin-pack/sources/index.ts
    Co-authored-by: Paul Soporan <paul.soporan@gmail.com>

--- packages/plugin-pack/sources/index.ts
+++ packages/plugin-pack/sources/index.ts
@@ -58,7 +58,7 @@ const beforeWorkspacePacking = (workspace: Workspace, rawManifest: any) => {
         // For workspace:path/to/workspace and workspace:* we look up the workspace version
         if (structUtils.areDescriptorsEqual(descriptor, matchingWorkspace.anchoredDescriptor) || range.selector === `*`)
           versionToWrite = matchingWorkspace.manifest.version ?? `0.0.0`;
-        // For workspace:~ and workspace:^ we add selector in front of workspace version
+        // For workspace:~ and workspace:^ we add the selector in front of the workspace version
         else if (range.selector === `~` || range.selector === `^`)
           versionToWrite =  `${range.selector}${matchingWorkspace.manifest.version ?? `0.0.0`}`;
         else
