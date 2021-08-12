20c67b98908f5fb5449eed57c41d0b47b1aa7ba2
Paul Soporan <paul.soporan@gmail.com>
2021-08-12 11:18:04 +0300

    fix: make all cli commands extend BaseCommand (#3257)

--- packages/plugin-essentials/sources/commands/entries/help.ts
+++ packages/plugin-essentials/sources/commands/entries/help.ts
@@ -1,8 +1,7 @@
-import {CommandContext} from '@yarnpkg/core';
-import {Command}        from 'clipanion';
+import {BaseCommand} from "@yarnpkg/cli";
 
 // eslint-disable-next-line arca/no-default-export
-export default class HelpCommand extends Command<CommandContext> {
+export default class HelpCommand extends BaseCommand {
   static paths = [
     [`help`],
     [`--help`],
