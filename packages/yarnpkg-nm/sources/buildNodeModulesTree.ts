1a3d28464961013f7e7f5e83ed144b460c5a946a
Victor Vlasenko <victor.vlasenko@sysgears.com>
2021-08-17 15:48:28 +0300

    fix(nm): Maximizes chances to end-up with only top-level node_modules (#3267)
    * Maximizes chances to end-up with only top-level node_modules
    
    * Updates versions
    
    * Runs lint fix
    
    * Fixes hoist priority updating, adds integration test for checking workspace deps priority
    
    * Runs lint
    
    * Updates hoist priority in initial graph to the max priority

--- packages/yarnpkg-nm/sources/buildNodeModulesTree.ts
+++ packages/yarnpkg-nm/sources/buildNodeModulesTree.ts
@@ -238,7 +238,7 @@ const buildPackageTree = (pnp: PnpApi, options: NodeModulesTreeOptions): { packa
     return ppath.contains(options.project.cwd, realSoftLinkPath) === null;
   };
 
-  const addPackageToTree = (name: string, pkg: PackageInformation<NativePath>, locator: PhysicalPackageLocator, parent: HoisterTree, parentDependencies: Map<string, DependencyTarget>, parentRelativeCwd: PortablePath, isHoistBorder: boolean) => {
+  const addPackageToTree = (name: string, pkg: PackageInformation<NativePath>, locator: PhysicalPackageLocator, parent: HoisterTree, parentPkg: PackageInformation<NativePath>, parentDependencies: Map<string, DependencyTarget>, parentRelativeCwd: PortablePath, isHoistBorder: boolean) => {
     const nodeKey = getNodeKey(name, locator);
     let node = nodes.get(nodeKey);
 
@@ -248,8 +248,6 @@ const buildPackageTree = (pnp: PnpApi, options: NodeModulesTreeOptions): { packa
       nodes.set(nodeKey, packageTree);
     }
 
-    const isExternalSoftLinkPackage = isExternalSoftLink(pkg, locator);
-
     if (!node) {
       node = {
         name,
@@ -257,11 +255,22 @@ const buildPackageTree = (pnp: PnpApi, options: NodeModulesTreeOptions): { packa
         reference: locator.reference,
         dependencies: new Set(),
         peerNames: pkg.packagePeers,
-        hoistPriority: isExternalSoftLinkPackage ? 1 : 0,
       };
 
       nodes.set(nodeKey, node);
     }
+
+    let hoistPriority;
+    const isExternalSoftLinkPackage = isExternalSoftLink(pkg, locator);
+    if (isExternalSoftLinkPackage)
+      // External soft link dependencies have the highest priority - we don't want to install inside them
+      hoistPriority = 2;
+    else if (parentPkg.linkType === LinkType.SOFT)
+      // Internal soft link dependencies should have priority over transitive dependencies - to maximize chances having only one top-level node_modules
+      hoistPriority = 1;
+    else
+      hoistPriority = 0;
+    node.hoistPriority = Math.max(node.hoistPriority || 0, hoistPriority);
 
     if (isHoistBorder && !isExternalSoftLinkPackage) {
       const parentLocatorKey = stringifyLocator({name: parent.identName, reference: parent.reference});
@@ -362,13 +371,13 @@ const buildPackageTree = (pnp: PnpApi, options: NodeModulesTreeOptions): { packa
             || depHoistingLimits === NodeModulesHoistingLimits.DEPENDENCIES
             || depHoistingLimits === NodeModulesHoistingLimits.WORKSPACES;
 
-          addPackageToTree(stringifyLocator(depLocator) === stringifyLocator(locator) ? name : depName, depPkg, depLocator, node, allDependencies, relativeDepCwd, isHoistBorder);
+          addPackageToTree(stringifyLocator(depLocator) === stringifyLocator(locator) ? name : depName, depPkg, depLocator, node, pkg, allDependencies, relativeDepCwd, isHoistBorder);
         }
       }
     }
   };
 
-  addPackageToTree(topLocator.name, topPkg, topLocator, packageTree, topPkg.packageDependencies, PortablePath.dot, false);
+  addPackageToTree(topLocator.name, topPkg, topLocator, packageTree, topPkg, topPkg.packageDependencies, PortablePath.dot, false);
 
   return {packageTree, hoistingLimits, errors, preserveSymlinksRequired};
 };
