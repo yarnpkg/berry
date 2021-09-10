8ffef3aa415769df3a7f93921ee3dee47f6a8ef8
merceyz <merceyz@users.noreply.github.com>
2021-09-10 18:35:08 +0200

    fix: add types for loader artifact

--- packages/yarnpkg-pnp/sources/index.ts
+++ packages/yarnpkg-pnp/sources/index.ts
@@ -3,7 +3,6 @@ export * from './generatePnpScript';
 export * from './hydratePnpApi';
 export * from './makeRuntimeApi';
 
-// @ts-expect-error No types
 import getESMLoaderTemplate from './esm-loader/built-loader';
 
 export {getESMLoaderTemplate};
