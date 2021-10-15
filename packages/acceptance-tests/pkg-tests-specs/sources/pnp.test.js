23a87a5e8d421d70641cdba9f98eafc860efb0c8
Maël Nison <nison.mael@gmail.com>
2021-10-15 20:31:22 +0200

    Fixes tests

--- packages/acceptance-tests/pkg-tests-specs/sources/pnp.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/pnp.test.js
@@ -1652,7 +1652,7 @@ describe(`Plug'n'Play`, () => {
         const stdout = (await run(`install`)).stdout;
 
         expect(stdout).not.toContain(`Shall not be run`);
-        expect(stdout).toMatch(new RegExp(`dep@file:./dep.*The platform ${process.platform} is incompatible with this module, build skipped.`));
+        expect(stdout).toMatch(new RegExp(`dep@file:./dep.*The ${process.platform}-${process.arch} architecture is incompatible with this module, build skipped.`));
 
         await expect(source(`require('dep')`)).resolves.toMatchObject({
           name: `dep`,
