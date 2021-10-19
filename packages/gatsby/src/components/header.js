7e2fccc39c9a8c93cd188ad11e47211eedd0f1bc
Robert <treboryx@gmail.com>
2021-10-19 21:53:21 +0300

    fix icon

--- packages/gatsby/src/components/header.js
+++ packages/gatsby/src/components/header.js
@@ -345,7 +345,7 @@ export const Header = ({children}) => {
             <InlineIcon icon={githubIcon} height={25}/>
           </a>
           <a href={`https://discord.com/invite/yarnpkg`}>
-            <InlineIcon icon={discordIcon} height={25}/>
+            <discordIcon height={25}/>
           </a>
         </MenuSearchBox>
       </MenuContainer>
