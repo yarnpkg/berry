2c83533f4dd00d77c3fba77cb2d7a80418b9619f
Paul Soporan <paul.soporan@gmail.com>
2021-10-29 01:58:24 +0300

    test: bump timeout on big-endian systems

--- packages/acceptance-tests/yarn.setup.ts
+++ packages/acceptance-tests/yarn.setup.ts
@@ -1,8 +1,14 @@
+import os      from 'os';
 import {tests} from 'pkg-tests-core';
 
 const {startPackageServer, getPackageRegistry} = tests;
 
-jest.setTimeout(30000);
+jest.setTimeout(
+  // Testing things inside a big-endian container takes forever
+  os.endianness() === `BE`
+    ? 100000
+    : 30000,
+);
 
 beforeEach(async () => {
   await startPackageServer();
