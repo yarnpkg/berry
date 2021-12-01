04f97d6dcb2fff3a727cf42d32a578e8573fbcc8
Paul Soporan <paul.soporan@gmail.com>
2021-12-01 16:13:08 +0200

    feat(vscode-zipfs): unmount (#3820)

--- packages/vscode-zipfs/sources/index.ts
+++ packages/vscode-zipfs/sources/index.ts
@@ -15,6 +15,29 @@ function mount(uri: vscode.Uri) {
   }
 }
 
+function unmount(uri: vscode.Uri): void {
+  const zipUri = vscode.Uri.parse(`zip:${uri.fsPath}`);
+
+  const workspaceFolder = vscode.workspace.getWorkspaceFolder(zipUri);
+  if (!workspaceFolder) {
+    vscode.window.showErrorMessage(`Cannot unmount ${zipUri.fsPath}: not mounted`);
+    return;
+  }
+
+  if (!vscode.workspace.workspaceFolders)
+    throw new Error(`Assertion failed: workspaceFolders is undefined`);
+
+  // When calling `updateWorkspaceFolders`, vscode still keeps the "workspace mode" even if a single folder remains which is quite annoying.
+  // Because of this, we execute `vscode.openFolder` to open the workspace folder.
+  if (vscode.workspace.workspaceFolders.length === 2) {
+    const otherFolder = vscode.workspace.workspaceFolders.find(folder => folder.index !== workspaceFolder.index)!;
+
+    vscode.commands.executeCommand(`vscode.openFolder`, otherFolder.uri, {forceNewWindow: false});
+  } else {
+    vscode.workspace.updateWorkspaceFolders(workspaceFolder.index, 1);
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
   // Until a more specific activation event exists this requires onStartupFinished
   context.subscriptions.push(registerTerminalLinkProvider());
@@ -25,6 +48,10 @@ export function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(vscode.commands.registerCommand(`zipfs.mountZipFile`, (uri: vscode.Uri) => {
     mount(uri);
+  }));
+
+  context.subscriptions.push(vscode.commands.registerCommand(`zipfs.unmountZipFile`, (uri: vscode.Uri) => {
+    unmount(uri);
   }));
 
   context.subscriptions.push(vscode.commands.registerCommand(`zipfs.mountZipEditor`, () => {
