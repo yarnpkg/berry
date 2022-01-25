aa4ee11a183cfabbe650988eb09977064660561e
Paul Soporan <paul.soporan@gmail.com>
2022-01-25 17:23:45 +0200

    test: feedback

--- packages/acceptance-tests/pkg-tests-specs/sources/commands/npm/info.test.ts
+++ packages/acceptance-tests/pkg-tests-specs/sources/commands/npm/info.test.ts
@@ -1,46 +1,44 @@
 import {tests} from 'pkg-tests-core';
 
-const npmInfo = async (run: tests.Run, ...args: Array<string>) => {
-  const {stdout} = await run(`npm`, `info`, `--json`, ...args);
-
-  return JSON.parse(stdout);
-};
-
 describe(`Commands`, () => {
   describe(`npm info`, () => {
     test(
       `it should return information about the latest version of a package if no range is specified`,
       makeTemporaryEnv({}, async ({path, run, source}) => {
-        await expect(npmInfo(run, `no-deps`)).resolves.toMatchObject({
+        const {stdout} = await run(`npm`, `info`, `no-deps`, `--json`);
+        expect(stdout).toMatchJSON(expect.objectContaining({
           version: `2.0.0`,
-        });
+        }));
       }),
     );
 
     test(
       `it should return information about a specific version if requested`,
       makeTemporaryEnv({}, async ({path, run, source}) => {
-        await expect(npmInfo(run, `no-deps@1.0.0`)).resolves.toMatchObject({
+        const {stdout} = await run(`npm`, `info`, `no-deps@1.0.0`, `--json`);
+        expect(stdout).toMatchJSON(expect.objectContaining({
           version: `1.0.0`,
-        });
+        }));
       }),
     );
 
     test(
       `it should return information about the highest version that satisfies a range`,
       makeTemporaryEnv({}, async ({path, run, source}) => {
-        await expect(npmInfo(run, `no-deps@^1.0.0`)).resolves.toMatchObject({
+        const {stdout} = await run(`npm`, `info`, `no-deps@^1.0.0`, `--json`);
+        expect(stdout).toMatchJSON(expect.objectContaining({
           version: `1.1.0`,
-        });
+        }));
       }),
     );
 
     test(
       `it should return information about a tagged version if requested`,
       makeTemporaryEnv({}, async ({path, run, source}) => {
-        await expect(npmInfo(run, `no-deps-tags@rc`)).resolves.toMatchObject({
+        const {stdout} = await run(`npm`, `info`, `no-deps-tags@rc`, `--json`);
+        expect(stdout).toMatchJSON(expect.objectContaining({
           version: `1.0.0-rc.1`,
-        });
+        }));
       }),
     );
   });
