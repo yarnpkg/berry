7f6f2b0bf6e24b6d0d591dfb4640f5107b288e29
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2022-02-26 19:19:32 -0800

    chore(pack): use const for variable versionToWrite

--- packages/plugin-pack/sources/index.ts
+++ packages/plugin-pack/sources/index.ts
@@ -58,17 +58,15 @@ const beforeWorkspacePacking = (workspace: Workspace, rawManifest: any) => {
           throw new ReportError(MessageName.WORKSPACE_NOT_FOUND, `${structUtils.prettyDescriptor(project.configuration, descriptor)}: No local workspace found for this range`);
         }
       } else {
-        let versionToWrite: string;
-
-        // For workspace:path/to/workspace and workspace:* we look up the workspace version
-        if (structUtils.areDescriptorsEqual(descriptor, matchingWorkspace.anchoredDescriptor) || range.selector === `*`)
-          versionToWrite = matchingWorkspace.manifest.version ?? `0.0.0`;
-        // For workspace:~ and workspace:^ we add the selector in front of the workspace version
-        else if (range.selector === `~` || range.selector === `^`)
-          versionToWrite =  `${range.selector}${matchingWorkspace.manifest.version ?? `0.0.0`}`;
-        else
-          // for workspace:version we simply strip the protocol
-          versionToWrite = range.selector;
+        const versionToWrite =
+          // For workspace:path/to/workspace and workspace:* we look up the workspace version
+          structUtils.areDescriptorsEqual(descriptor, matchingWorkspace.anchoredDescriptor) || range.selector === `*`
+            ? matchingWorkspace.manifest.version ?? `0.0.0`
+            // For workspace:~ and workspace:^ we add the selector in front of the workspace version
+            : range.selector === `~` || range.selector === `^`
+              ? `${range.selector}${matchingWorkspace.manifest.version ?? `0.0.0`}`
+              // for workspace:version we simply strip the protocol
+              : range.selector;
 
         // Ensure optional dependencies are handled as well
         const identDescriptor = dependencyType === `dependencies`
