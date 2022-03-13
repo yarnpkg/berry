50f19b9e954da5da4cbcfb59fa64c845b01f1ae2
Paul Soporan <paul.soporan@gmail.com>
2022-03-13 20:29:10 +0200

    refactor: move and document getNpmAuthenticationHeader (#3888)
    * refactor: move and document getNpmAuthenticationHeader
    
    * refactor: feedback
    
    * chore: versions
    
    * revert: revert leftovers

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,25 @@ module.exports = {
     require.resolve(`@yarnpkg/eslint-config`),
     require.resolve(`@yarnpkg/eslint-config/react`),
   ],
-  ignorePatterns: [
-    `packages/plugin-compat/extra/fsevents/fsevents-*.js`,
+  overrides: [
+    {
+      files: [`!packages/*/sources/{index,Plugin}.ts`],
+      rules: {
+        '@typescript-eslint/naming-convention': [`error`, {
+          selector: `typeLike`,
+          format: [`PascalCase`],
+          custom: {
+            regex: `^Hooks$`,
+            match: false,
+          },
+        }],
+      },
+    },
+    {
+      files: [`packages/acceptance-tests/pkg-tests-specs/**/*.test.{js,ts}`],
+      globals: {
+        makeTemporaryEnv: `readonly`,
+      },
+    },
   ],
 };
