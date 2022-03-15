276eef865dd42a0f34fbf34545e115d5b0f05c13
Maël Nison <nison.mael@gmail.com>
2022-03-15 14:40:58 +0100

    Fixes linting

--- packages/plugin-patch/sources/commands/patch.ts
+++ packages/plugin-patch/sources/commands/patch.ts
@@ -1,9 +1,9 @@
-import {BaseCommand, WorkspaceRequiredError}                                                                     from '@yarnpkg/cli';
-import {Cache, Configuration, Project, formatUtils, structUtils, StreamReport, MessageName, miscUtils, Manifest} from '@yarnpkg/core';
-import {npath}                                                                                                   from '@yarnpkg/fslib';
-import {Command, Option, Usage, UsageError}                                                                      from 'clipanion';
+import {BaseCommand, WorkspaceRequiredError}                                                           from '@yarnpkg/cli';
+import {Cache, Configuration, Project, formatUtils, structUtils, StreamReport, MessageName, miscUtils} from '@yarnpkg/core';
+import {npath}                                                                                         from '@yarnpkg/fslib';
+import {Command, Option, Usage, UsageError}                                                            from 'clipanion';
 
-import * as patchUtils                                                                                           from '../patchUtils';
+import * as patchUtils                                                                                 from '../patchUtils';
 
 // eslint-disable-next-line arca/no-default-export
 export default class PatchCommand extends BaseCommand {
