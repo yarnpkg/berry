76b3c0f0343035ff115de6103eac490442d6cba9
merceyz <merceyz@users.noreply.github.com>
2022-03-19 16:52:57 +0100

    test: disable flaky tests

--- packages/acceptance-tests/pkg-tests-specs/sources/features/installArtifactCleanup.test.ts
+++ packages/acceptance-tests/pkg-tests-specs/sources/features/installArtifactCleanup.test.ts
@@ -4,7 +4,8 @@ const lsStore = async (path: PortablePath) => {
   return await xfs.readdirPromise(`${path}/${Filename.nodeModules}/.store` as PortablePath);
 };
 
-describe(`Install Artifact Cleanup`, () => {
+// These tests randomly fail on the CI and until we figure out why they're just noise
+describe.skip(`Install Artifact Cleanup`, () => {
   describe(`pnpm linker`, () => {
     it(`should not generate a node_modules folder if it has nothing to put inside it`, makeTemporaryEnv({}, {
       nodeLinker: `pnpm`,
