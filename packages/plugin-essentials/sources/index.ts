5ba3bd073751412b80b69b47faaeed8d13f76a61
Mario Nebl <marionebl@users.noreply.github.com>
2022-03-31 20:27:09 +1100

    docs: improve explanation
    Co-authored-by: Kristoffer K. <merceyz@users.noreply.github.com>

--- packages/plugin-essentials/sources/index.ts
+++ packages/plugin-essentials/sources/index.ts
@@ -128,7 +128,7 @@ const plugin: Plugin = {
     },
 
     preferReuse: {
-      description: `If true, the CLI will use the hightest version of existing, latest for new dependencies`,
+      description: `If true, `yarn add` will attempt to reuse the most common dependency range in other workspaces.`,
       type: SettingsType.BOOLEAN,
       default: false,
     },
