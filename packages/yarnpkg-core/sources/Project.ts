86a7a5ac6da89f0fe15718fa869558887a7e09f4
stevenxu-db <steven.xu@databricks.com>
2022-04-27 12:55:22 -0700

    missed this in cleanup

--- packages/yarnpkg-core/sources/Project.ts
+++ packages/yarnpkg-core/sources/Project.ts
@@ -1838,7 +1838,7 @@ export class Project {
     }
   }
 
-  async cacheCleanup({cache, mode, persistProject, report}: InstallOptions)  {
+  async cacheCleanup({cache, report}: InstallOptions)  {
     if (this.configuration.get(`enableGlobalCache`)) return;
 
     const PRESERVED_FILES = new Set([
