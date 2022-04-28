d9920810ee4c6c0165a212ffe69552d3668bbc34
Maël Nison <nison.mael@gmail.com>
2022-04-28 09:53:29 +0200

    Update Project.ts

--- packages/yarnpkg-core/sources/Project.ts
+++ packages/yarnpkg-core/sources/Project.ts
@@ -1839,7 +1839,8 @@ export class Project {
   }
 
   async cacheCleanup({cache, report}: InstallOptions)  {
-    if (this.configuration.get(`enableGlobalCache`)) return;
+    if (this.configuration.get(`enableGlobalCache`))
+      return;
 
     const PRESERVED_FILES = new Set([
       `.gitignore`,
