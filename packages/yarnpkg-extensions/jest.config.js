dd371dbf633906a7af42052260e45c8f72c014a1
merceyz <merceyz@users.noreply.github.com>
2022-05-06 19:28:57 +0200

    chore: remove unnecessary test

--- packages/yarnpkg-extensions/jest.config.js
+++
@@ -1,6 +0,0 @@
-// plugin-compat is tested separately, as it relies on external resources
-// and it takes a long time for them to be fetched
-
-module.exports = Object.assign({}, require(`@yarnpkg/monorepo/jest.config.js`), {
-  modulePathIgnorePatterns: [],
-});
--- packages/yarnpkg-extensions/tests/extensions.test.ts
+++
@@ -1,7 +0,0 @@
-import {packageExtensions} from '../sources/index';
-
-describe(`packageExtensions`, () => {
-  it(`should return entries`, () => {
-    expect(packageExtensions.length).toBeGreaterThan(0);
-  });
-});
