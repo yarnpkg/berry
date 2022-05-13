c685fa5ae8bb57450c104cbe97dfcdc95217be03
Maël Nison <nison.mael@gmail.com>
2022-05-13 14:48:59 +0200

    Re-releases broken packages

--- scripts/release/01-release-tags.sh
+++ scripts/release/01-release-tags.sh
@@ -5,6 +5,9 @@ set -ex
 THIS_DIR=$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 REPO_DIR="$THIS_DIR/../.."
 
+export BABEL_CACHE_PATH=$(mktemp -d)/cache.json
+mkdir -p "$(dirname "$BABEL_CACHE_PATH")"
+
 if ! [[ -z $(git status --porcelain) ]]; then
   echo 'This command must be executed on a clean repository'
   exit 1
