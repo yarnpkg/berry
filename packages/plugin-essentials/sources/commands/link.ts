42ab01d654d063610d4c092a0b02e2b689fa341b
Noah <noah.negin-ulster@tophatmonocle.com>
2022-06-24 05:36:53 -0400

    feat(plugin-essentials): support multiple workspaces in yarn link command (#4573)
    (cherry picked from commit 2ba6e59bebb813db45f55e9a0d4061fc438db30a)

--- packages/plugin-essentials/sources/commands/link.ts
+++ packages/plugin-essentials/sources/commands/link.ts
@@ -15,8 +15,8 @@ export default class LinkCommand extends BaseCommand {
       This command will set a new \`resolutions\` field in the project-level manifest and point it to the workspace at the specified location (even if part of another project).
     `,
     examples: [[
-      `Register a remote workspace for use in the current project`,
-      `$0 link ~/ts-loader`,
+      `Register one or more remote workspaces for use in the current project`,
+      `$0 link ~/ts-loader ~/jest`,
     ], [
       `Register all workspaces from a remote project for use in the current project`,
       `$0 link ~/jest --all`,
@@ -24,18 +24,18 @@ export default class LinkCommand extends BaseCommand {
   });
 
   all = Option.Boolean(`-A,--all`, false, {
-    description: `Link all workspaces belonging to the target project to the current one`,
+    description: `Link all workspaces belonging to the target projects to the current one`,
   });
 
   private = Option.Boolean(`-p,--private`, false, {
-    description: `Also link private workspaces belonging to the target project to the current one`,
+    description: `Also link private workspaces belonging to the target projects to the current one`,
   });
 
   relative = Option.Boolean(`-r,--relative`, false, {
     description: `Link workspaces using relative paths instead of absolute paths`,
   });
 
-  destination = Option.String();
+  destinations = Option.Rest();
 
   async execute() {
     const configuration = await Configuration.find(this.context.cwd, this.context.plugins);
@@ -49,36 +49,42 @@ export default class LinkCommand extends BaseCommand {
       restoreResolutions: false,
     });
 
-    const absoluteDestination = ppath.resolve(this.context.cwd, npath.toPortablePath(this.destination));
-
-    const configuration2 = await Configuration.find(absoluteDestination, this.context.plugins, {useRc: false, strict: false});
-    const {project: project2, workspace: workspace2} = await Project.find(configuration2, absoluteDestination);
-
-    if (project.cwd === project2.cwd)
-      throw new UsageError(`Invalid destination; Can't link the project to itself`);
-
-    if (!workspace2)
-      throw new WorkspaceRequiredError(project2.cwd, absoluteDestination);
-
     const topLevelWorkspace = project.topLevelWorkspace;
     const linkedWorkspaces = [];
 
-    if (this.all) {
-      for (const workspace of project2.workspaces)
-        if (workspace.manifest.name && (!workspace.manifest.private || this.private))
-          linkedWorkspaces.push(workspace);
+    for (const destination of this.destinations) {
+      const absoluteDestination = ppath.resolve(this.context.cwd, npath.toPortablePath(destination));
 
-      if (linkedWorkspaces.length === 0) {
-        throw new UsageError(`No workspace found to be linked in the target project`);
+      const configuration2 = await Configuration.find(absoluteDestination, this.context.plugins, {useRc: false, strict: false});
+      const {project: project2, workspace: workspace2} = await Project.find(configuration2, absoluteDestination);
+
+      if (project.cwd === project2.cwd)
+        throw new UsageError(`Invalid destination '${destination}'; Can't link the project to itself`);
+
+      if (!workspace2)
+        throw new WorkspaceRequiredError(project2.cwd, absoluteDestination);
+
+      if (this.all) {
+        let found = false;
+        for (const workspace of project2.workspaces) {
+          if (workspace.manifest.name && (!workspace.manifest.private || this.private)) {
+            linkedWorkspaces.push(workspace);
+            found = true;
+          }
+        }
+
+        if (!found) {
+          throw new UsageError(`No workspace found to be linked in the target project: ${destination}`);
+        }
+      } else {
+        if (!workspace2.manifest.name)
+          throw new UsageError(`The target workspace at '${destination}' doesn't have a name and thus cannot be linked`);
+
+        if (workspace2.manifest.private && !this.private)
+          throw new UsageError(`The target workspace at '${destination}' is marked private - use the --private flag to link it anyway`);
+
+        linkedWorkspaces.push(workspace2);
       }
-    } else {
-      if (!workspace2.manifest.name)
-        throw new UsageError(`The target workspace doesn't have a name and thus cannot be linked`);
-
-      if (workspace2.manifest.private && !this.private)
-        throw new UsageError(`The target workspace is marked private - use the --private flag to link it anyway`);
-
-      linkedWorkspaces.push(workspace2);
     }
 
     for (const workspace of linkedWorkspaces) {
