55c3bacae168b005a4aea489673c0a3bacd549ce
Paul Soporan <paul.soporan@gmail.com>
2022-07-12 17:06:58 +0300

    fix(dlx): don't report `UNUSED_PACKAGE_EXTENSION` warnings (#4583)
    * fix(dlx): don't report `UNUSED_PACKAGE_EXTENSION` warnings
    
    * chore: versions
    
    * refactor: tweaks

--- packages/yarnpkg-core/tests/miscUtils.test.ts
+++ packages/yarnpkg-core/tests/miscUtils.test.ts
@@ -1,3 +1,5 @@
+import CJSON          from 'comment-json';
+
 import * as miscUtils from '../sources/miscUtils';
 
 describe(`miscUtils`, () => {
@@ -54,6 +56,101 @@ describe(`miscUtils`, () => {
       `~`,
     ])(`should return false for %s`, pathLike => {
       expect(miscUtils.isPathLike(pathLike)).toBe(false);
+    });
+  });
+
+  describe(`toMerged`, () => {
+    it(`should merge 2 shallow objects without mutating any arguments`, () => {
+      const a = {a: 1};
+      const b = {b: 2};
+      const c = miscUtils.toMerged(a, b);
+
+      expect(a).toStrictEqual({a: 1});
+      expect(b).toStrictEqual({b: 2});
+      expect(c).toStrictEqual({a: 1, b: 2});
+    });
+
+    it(`should merge 2 deep objects without mutating any arguments`, () => {
+      const a = {n: {a: 1}};
+      const b = {n: {b: 2}};
+      const c = miscUtils.toMerged(a, b);
+
+      expect(a).toStrictEqual({n: {a: 1}});
+      expect(b).toStrictEqual({n: {b: 2}});
+      expect(c).toStrictEqual({n: {a: 1, b: 2}});
+    });
+
+    it(`should merge 2 arrays by concatenating their contents without mutating any arguments`, () => {
+      const a = [1, 2, 3];
+      const b = [4, 5, 6];
+      const c = miscUtils.toMerged(a, b);
+
+      expect(a).toStrictEqual([1, 2, 3]);
+      expect(b).toStrictEqual([4, 5, 6]);
+      expect(c).toStrictEqual([1, 2, 3, 4, 5, 6]);
+    });
+
+    it(`should merge 2 objects of arrays by concatenating their contents without mutating any arguments`, () => {
+      const a = {n: [1, 2, 3]};
+      const b = {n: [4, 5, 6]};
+      const c = miscUtils.toMerged(a, b);
+
+      expect(a).toStrictEqual({n: [1, 2, 3]});
+      expect(b).toStrictEqual({n: [4, 5, 6]});
+      expect(c).toStrictEqual({n: [1, 2, 3, 4, 5, 6]});
+    });
+
+    it(`should merge identical elements in arrays (primitives)`, () => {
+      const a = {n: [1, 2, 3, 4]};
+      const b = {n: [4, 5, 6]};
+      const c = miscUtils.toMerged(a, b);
+
+      expect(c).toStrictEqual({n: [1, 2, 3, 4, 5, 6]});
+    });
+
+    it(`should merge identical elements in arrays (objects)`, () => {
+      const a = {n: [{a: 1}, {b: 2}]};
+      const b = {n: [{b: 2}, {c: 3}]};
+      const c = miscUtils.toMerged(a, b);
+
+      expect(c).toStrictEqual({n: [{a: 1}, {b: 2}, {c: 3}]});
+    });
+  });
+
+  describe(`mergeIntoTarget`, () => {
+    it(`should preserve comments when the target is an object created by comment-json`, () => {
+      const a = CJSON.parse(`{
+        // n
+        "n":
+        // array
+        [
+          // 1
+          1,
+          // 2
+          2,
+          // 3
+          3
+        ]
+      }`);
+      const b = {n: [4, 5, 6]};
+      const c = miscUtils.mergeIntoTarget(a, b);
+
+      expect(CJSON.stringify(c, null, 2)).toStrictEqual(CJSON.stringify(CJSON.parse(`{
+        // n
+        "n":
+        // array
+        [
+          // 1
+          1,
+          // 2
+          2,
+          // 3
+          3,
+          4,
+          5,
+          6
+        ]
+      }`), null, 2));
     });
   });
 });
