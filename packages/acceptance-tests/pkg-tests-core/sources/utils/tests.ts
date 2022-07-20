2c41672f23e9c431eb1b3479cf89f5164e8f40da
Paul Soporan <paul.soporan@gmail.com>
2022-07-20 06:14:05 +0300

    test: don't hardcode timeout interval

--- packages/acceptance-tests/pkg-tests-core/sources/utils/tests.ts
+++ packages/acceptance-tests/pkg-tests-core/sources/utils/tests.ts
@@ -636,7 +636,7 @@ export type RunFunction = (
     run: Run;
     source: Source;
   }
-) => void;
+) => Promise<void>;
 
 export const generatePkgDriver = ({
   getName,
@@ -721,12 +721,12 @@ export const generatePkgDriver = ({
         try {
           // To pass [citgm](https://github.com/nodejs/citgm), we need to suppress timeout failures
           // So add env variable TEST_IGNORE_TIMEOUT_FAILURES to turn on this suppression
+          // TODO: investigate whether this is still needed.
           if (process.env.TEST_IGNORE_TIMEOUT_FAILURES) {
             await Promise.race([
               new Promise(resolve => {
-                // Maybe we should not hard code the timeout here
-                // resolve 1s ahead the jest timeout
-                setTimeout(resolve, 30000 - 1000);
+                // Resolve 1s ahead of the jest timeout
+                setTimeout(resolve, jasmine.DEFAULT_TIMEOUT_INTERVAL - 1000);
               }),
               fn!({path, run, source}),
             ]);
