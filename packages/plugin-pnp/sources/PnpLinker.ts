aaf7ef3543b06247dc1dbfd365bd07434e8dd98c
Paul Soporan <paul.soporan@gmail.com>
2022-08-18 12:30:09 +0300

    fix: remove unplugged folder when switching to a different linker (#4772)

--- packages/plugin-pnp/sources/PnpLinker.ts
+++ packages/plugin-pnp/sources/PnpLinker.ts
@@ -256,6 +256,7 @@ export class PnpInstaller implements Installer {
       await xfs.removePromise(pnpPath.cjs);
       await xfs.removePromise(this.opts.project.configuration.get(`pnpDataPath`));
       await xfs.removePromise(pnpPath.esmLoader);
+      await xfs.removePromise(this.opts.project.configuration.get(`pnpUnpluggedFolder`));
 
       return undefined;
     }
