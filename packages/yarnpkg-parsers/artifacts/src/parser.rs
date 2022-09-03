728167a8c30e3b5070584370b80c08c12ad4dea2
Paul Soporan <paul.soporan@gmail.com>
2022-09-03 04:03:29 +0300

    refactor: extract input type to make it easier to change it

--- packages/yarnpkg-parsers/artifacts/src/parser.rs
+++ packages/yarnpkg-parsers/artifacts/src/parser.rs
@@ -13,13 +13,15 @@ use serde_json::{json, Value};
 // TODO:: Automatically detect indentation from input.
 const INDENT_STEP: usize = 2;
 
-pub type ParseResult<'input, O> = IResult<&'input str, O, VerboseError<&'input str>>;
+pub type Input<'a> = &'a str;
 
-pub fn parse(input: &str) -> ParseResult<Value> {
+pub type ParseResult<'input, O> = IResult<Input<'input>, O, VerboseError<Input<'input>>>;
+
+pub fn parse(input: Input) -> ParseResult<Value> {
   property_statements(input, 0)
 }
 
-fn property_statements(input: &str, indent: usize) -> ParseResult<Value> {
+fn property_statements(input: Input, indent: usize) -> ParseResult<Value> {
   fold_many0(
     alt((map(comment, |_| Default::default()), |input| {
       property_statement(input, indent)
@@ -36,7 +38,7 @@ fn property_statements(input: &str, indent: usize) -> ParseResult<Value> {
   )(input)
 }
 
-fn property_statement(input: &str, indent: usize) -> ParseResult<(Value, Value)> {
+fn property_statement(input: Input, indent: usize) -> ParseResult<(Value, Value)> {
   preceded(
     |input| indentation(input, indent),
     separated_pair(scalar, delimited(space0, char(':'), space0), |input| {
@@ -45,11 +47,11 @@ fn property_statement(input: &str, indent: usize) -> ParseResult<(Value, Value)>
   )(input)
 }
 
-fn comment(input: &str) -> ParseResult<Option<&str>> {
+fn comment(input: Input) -> ParseResult<Option<Input>> {
   delimited(space0, opt(preceded(char('#'), not_line_ending)), eol_any)(input)
 }
 
-fn item_statements(input: &str, indent: usize) -> ParseResult<Value> {
+fn item_statements(input: Input, indent: usize) -> ParseResult<Value> {
   map(
     fold_many1(
       |input| item_statement(input, indent),
@@ -63,7 +65,7 @@ fn item_statements(input: &str, indent: usize) -> ParseResult<Value> {
   )(input)
 }
 
-fn item_statement(input: &str, indent: usize) -> ParseResult<Value> {
+fn item_statement(input: Input, indent: usize) -> ParseResult<Value> {
   preceded(
     |input| indentation(input, indent),
     preceded(terminated(char('-'), space1), |input| {
@@ -72,7 +74,7 @@ fn item_statement(input: &str, indent: usize) -> ParseResult<Value> {
   )(input)
 }
 
-fn expression(input: &str, indent: usize) -> ParseResult<Value> {
+fn expression(input: Input, indent: usize) -> ParseResult<Value> {
   alt((
     preceded(line_ending, |input| {
       item_statements(input, indent + INDENT_STEP)
@@ -84,19 +86,19 @@ fn expression(input: &str, indent: usize) -> ParseResult<Value> {
   ))(input)
 }
 
-fn indentation(input: &str, indent: usize) -> ParseResult<Vec<char>> {
+fn indentation(input: Input, indent: usize) -> ParseResult<Vec<char>> {
   count(char(' '), indent)(input)
 }
 
-fn scalar(input: &str) -> ParseResult<Value> {
+fn scalar(input: Input) -> ParseResult<Value> {
   map(alt((double_quoted_scalar, plain_scalar)), Value::String)(input)
 }
 
-fn double_quoted_scalar(input: &str) -> ParseResult<String> {
+fn double_quoted_scalar(input: Input) -> ParseResult<String> {
   delimited(char('"'), double_quoted_scalar_text, char('"'))(input)
 }
 
-fn double_quoted_scalar_text(input: &str) -> ParseResult<String> {
+fn double_quoted_scalar_text(input: Input) -> ParseResult<String> {
   escaped_transform(
     // TODO: "\0-\x1F" was part of the original regexp
     is_not("\"\\\x7f"),
@@ -117,7 +119,7 @@ fn double_quoted_scalar_text(input: &str) -> ParseResult<String> {
   )(input)
 }
 
-fn unicode_escape_digits(input: &str) -> ParseResult<char> {
+fn unicode_escape_digits(input: Input) -> ParseResult<char> {
   map_opt(
     map_res(take_while_m_n(4, 4, |ch: char| ch.is_hex_digit()), |hex| {
       u32::from_str_radix(hex, 16)
@@ -126,16 +128,16 @@ fn unicode_escape_digits(input: &str) -> ParseResult<char> {
   )(input)
 }
 
-fn plain_scalar(input: &str) -> ParseResult<String> {
+fn plain_scalar(input: Input) -> ParseResult<String> {
   map(
     recognize(preceded(
       is_not("\r\n\t ?:,][{}#&*!|>'\"%@`-"),
       many0_count(preceded(space0, is_not("\r\n\t ,][{}:#\"'"))),
     )),
-    |scalar: &str| scalar.to_owned(),
+    |scalar: Input| scalar.to_owned(),
   )(input)
 }
 
-fn eol_any(input: &str) -> ParseResult<&str> {
+fn eol_any(input: Input) -> ParseResult<Input> {
   terminated(line_ending, many0_count(preceded(space0, line_ending)))(input)
 }
