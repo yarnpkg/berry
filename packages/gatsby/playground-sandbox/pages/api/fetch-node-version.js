844f22db82b33a3821564bcb367ed19ab6561b9a
Paul Soporan <paul.soporan@gmail.com>
2022-09-16 23:48:41 +0300

    chore(playground): fix cors issue

--- packages/gatsby/playground-sandbox/pages/api/fetch-node-version.js
+++ packages/gatsby/playground-sandbox/pages/api/fetch-node-version.js
@@ -1,7 +1,9 @@
+import {setupCors} from '../../cors';
+
 // eslint-disable-next-line arca/no-default-export
-export default async (req, res) => {
+export default setupCors(async (req, res) => {
   return res.status(200).json({
     status: `success`,
     nodeVersion: process.version,
   });
-};
+});
