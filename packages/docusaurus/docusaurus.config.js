765d3eb38f49b88c90339c3ddbc9bb392ba91b39
Reece Dunham <me@rdil.rocks>
2022-09-29 10:18:12 -0400

    Dedupe packages

--- packages/docusaurus/docusaurus.config.js
+++ packages/docusaurus/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require(`prism-react-renderer/themes/dracula`);
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: `My Site`,
+  title: `Yarn`,
   tagline: `Dinosaurs are cool`,
   url: `https://your-docusaurus-test-site.com`,
   baseUrl: `/`,
