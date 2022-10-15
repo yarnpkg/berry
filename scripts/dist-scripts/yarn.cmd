2d92317a50eabeee6cb9d87912d85ddd802d014b
ryan_chu <jj811208@gmail.com>
2022-10-16 02:00:10 +0800

    revert: "Build Debian package (#379)"
    This reverts commit f533868352340b1e5631242866d65a22909ecbca.

--- scripts/dist-scripts/yarn.cmd
+++
@@ -1,1 +0,0 @@
-@goto #_undefined_# 2>NUL || @title %COMSPEC% & @setlocal & node "%~dp0\yarn.js" %*
--- scripts/dist-scripts/yarn.ps1
+++
@@ -1,6 +0,0 @@
-function Get-ScriptDirectory {
-	$Invocation = (Get-Variable MyInvocation -Scope 1).Value
-	Split-Path $Invocation.MyCommand.Path
-}
-
-node "$(Get-ScriptDirectory)/yarn.js" $args
