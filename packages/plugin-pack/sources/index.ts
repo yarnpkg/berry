b2af7b224d4915badc9705d6688b4f1e633d5e79
Sojin Park <raon0211@toss.im>
2022-11-01 01:34:43 +0900

    perf(pack): remove duplicated assignment of `browser` in `publishConfig` (#5004)

--- packages/plugin-pack/sources/index.ts
+++ packages/plugin-pack/sources/index.ts
@@ -32,9 +32,6 @@ const beforeWorkspacePacking = (workspace: Workspace, rawManifest: any) => {
     if (rawManifest.publishConfig.module)
       rawManifest.module = rawManifest.publishConfig.module;
 
-    if (rawManifest.publishConfig.browser)
-      rawManifest.browser = rawManifest.publishConfig.browser;
-
     if (rawManifest.publishConfig.exports)
       rawManifest.exports = rawManifest.publishConfig.exports;
 
