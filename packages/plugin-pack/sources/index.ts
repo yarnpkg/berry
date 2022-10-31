444fad3a14b741926d13d9c11fd39071acd6be1a
Sojin Park <raon0211@outlook.com>
2022-11-01 00:00:50 +0900

    Update index.ts

--- packages/plugin-pack/sources/index.ts
+++ packages/plugin-pack/sources/index.ts
@@ -35,10 +35,7 @@ const beforeWorkspacePacking = (workspace: Workspace, rawManifest: any) => {
 
     if (rawManifest.publishConfig.module)
       rawManifest.module = rawManifest.publishConfig.module;
-
-    if (rawManifest.publishConfig.browser)
-      rawManifest.browser = rawManifest.publishConfig.browser;
-
+    
     if (rawManifest.publishConfig.exports)
       rawManifest.exports = rawManifest.publishConfig.exports;
 
