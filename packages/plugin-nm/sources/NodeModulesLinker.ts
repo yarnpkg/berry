fb0237e4b911de3f449d1c15eebce726e0a40199
Victor Vlasenko <victor.vlasenko@sysgears.com>
2022-11-08 10:15:19 +0200

    Adds sorting as well

--- packages/plugin-nm/sources/NodeModulesLinker.ts
+++ packages/plugin-nm/sources/NodeModulesLinker.ts
@@ -68,8 +68,11 @@ export class NodeModulesLinker implements Linker {
       throw err;
     }
 
+    // Sort locations from shallowest to deepest in terms of directory nesting
+    const sortedLocations = locatorInfo.locations.sort((loc1, loc2) => loc1.split(ppath.sep).length - loc2.split(ppath.sep).length);
+    // Find the location with shallowest directory nesting that starts inside node_modules of cwd
     const startingCwdModules = ppath.join(opts.project.configuration.startingCwd, NODE_MODULES);
-    return locatorInfo.locations.find(location => ppath.contains(startingCwdModules, location)) || locatorInfo.locations[0];
+    return sortedLocations.find(location => ppath.contains(startingCwdModules, location)) || locatorInfo.locations[0];
   }
 
   async findPackageLocator(location: PortablePath, opts: LinkOptions) {
