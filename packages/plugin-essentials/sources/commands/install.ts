e4aead4326d88b5db2f8d0c81bd739998868242d
Maël Nison <nison.mael@gmail.com>
2022-11-29 12:27:28 +0100

    Adds cherry-pick support

--- packages/plugin-essentials/sources/commands/install.ts
+++ packages/plugin-essentials/sources/commands/install.ts
@@ -370,8 +370,14 @@ async function autofixMergeConflicts(configuration: Configuration, immutable: bo
     });
   }
 
+  if (commits.code !== 0) {
+    commits = await execUtils.execvp(`git`, [`rev-parse`, `CHERRY_PICK_HEAD`, `HEAD`], {
+      cwd: configuration.projectCwd,
+    });
+  }
+
   if (commits.code !== 0)
-    throw new ReportError(MessageName.AUTOMERGE_GIT_ERROR, `Git returned an error when trying to find the commits pertaining to the merge or rebase conflict`);
+    throw new ReportError(MessageName.AUTOMERGE_GIT_ERROR, `Git returned an error when trying to find the commits pertaining to the conflict`);
 
   let variants = await Promise.all(commits.stdout.trim().split(/\n/).map(async hash => {
     const content = await execUtils.execvp(`git`, [`show`, `${hash}:./${Filename.lockfile}`], {
