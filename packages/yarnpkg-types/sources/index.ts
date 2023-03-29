e4fa9b0c3e8e8b118079bbfbc2947a880fc3b9ad
Maël Nison <nison.mael@gmail.com>
2023-03-29 11:05:03 +0200

    Adds a defineConfig helper

--- packages/yarnpkg-types/sources/index.ts
+++ packages/yarnpkg-types/sources/index.ts
@@ -1,4 +1,10 @@
+import {Config} from './yarn';
+
 // Strangely, the tslib error sometimes disappear in the IDE ... so we use ts-ignore rather than ts-expect-error
 // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
 // @ts-ignore: TS incorrectly thinks it needs tslib for this type export
 export type * as Yarn from './yarn';
+
+export function defineConfig(config: Config) {
+  return config;
+}
