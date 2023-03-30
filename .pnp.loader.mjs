b4f469a019ec97c7d9169b906e0720b8547e9440
Liz Fong-Jones <lizf@honeycomb.io>
2023-03-30 15:31:00 -0700

    address null warning

--- .pnp.loader.mjs
+++ .pnp.loader.mjs
@@ -1979,6 +1979,7 @@ async function resolvePrivateRequest(specifier, issuer, context, nextResolve) {
   }
 }
 async function resolve$1(originalSpecifier, context, nextResolve) {
+  var _a;
   const { findPnpApi } = moduleExports;
   if (!findPnpApi || isBuiltinModule(originalSpecifier))
     return nextResolve(originalSpecifier, context, nextResolve);
@@ -1990,7 +1991,7 @@ async function resolve$1(originalSpecifier, context, nextResolve) {
     specifier = fileURLToPath(url);
   }
   const { parentURL, conditions = [] } = context;
-  const issuer = parentURL && tryParseURL(parentURL).protocol === `file:` ? fileURLToPath(parentURL) : process.cwd();
+  const issuer = parentURL && ((_a = tryParseURL(parentURL)) == null ? void 0 : _a.protocol) === `file:` ? fileURLToPath(parentURL) : process.cwd();
   const pnpapi = findPnpApi(issuer) ?? (url ? findPnpApi(specifier) : null);
   if (!pnpapi)
     return nextResolve(originalSpecifier, context, nextResolve);
