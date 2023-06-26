cdc780289b0c95f6f69fa03e005226d42ed85b08
Maël Nison <nison.mael@gmail.com>
2023-06-26 11:38:07 +0200

    Disables sharp

--- packages/gatsby/gatsby-config.js
+++ packages/gatsby/gatsby-config.js
@@ -61,8 +61,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
