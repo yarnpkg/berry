yarnpkg/berry

commit d13213280191f064785cf0bc19c92c76c60581bd

feat(core): add updateLockfileOnly configuration (#2913)
* feat(core): add updateLockfileOnly configuration

If true, Yarn will only fetch packages of which checksum doesn't exist
and skip the link step during the install.

* docs: improve description

* Renames updateLockfileOnly into --mode=lockfile-update

* Feedback

* feat: add --mode to add, remove, up, and dedupe

* refactor: rename to `skip-build` and `update-lockfile`

Co-authored-by: Maël Nison <nison.mael@gmail.com>

packages/plugin-essentials/sources/commands/up.ts
@@ -1,6 +1,6 @@
 import {BaseCommand, WorkspaceRequiredError}                                                                        from '@yarnpkg/cli';
 import {IdentHash, structUtils}                                                                                     from '@yarnpkg/core';
-import {Project, StreamReport, Workspace}                                                                           from '@yarnpkg/core';
+import {Project, StreamReport, Workspace, InstallMode}                                                              from '@yarnpkg/core';
 import {Cache, Configuration, Descriptor, LightReport, MessageName, MinimalResolveOptions, formatUtils, FormatType} from '@yarnpkg/core';
 import {Command, Option, Usage, UsageError}                                                                         from 'clipanion';
 import {prompt}                                                                                                     from 'enquirer';
@@ -26,6 +26,12 @@ export default class UpCommand extends BaseCommand {
       If \`-i,--interactive\` is set (or if the \`preferInteractive\` settings is toggled on) the command will offer various choices, depending on the detected upgrade paths. Some upgrades require this flag in order to resolve ambiguities.
 
       The, \`-C,--caret\`, \`-E,--exact\` and  \`-T,--tilde\` options have the same meaning as in the \`add\` command (they change the modifier used when the range is missing or a tag, and are ignored when the range is explicitly set).
+
+      If the \`--mode=<mode>\` option is set, Yarn will change which artifacts are generated. The modes currently supported are:
+
+      - \`skip-build\` will not run the build scripts at all. Note that this is different from setting \`enableScripts\` to false because the later will disable build scripts, and thus affect the content of the artifacts generated on disk, whereas the former will just disable the build step - but not the scripts themselves, which just won't run.
+
+      - \`update-lockfile\` will skip the link step altogether, and only fetch packages that are missing from the lockfile (or that have no associated checksums). This mode is typically used by tools like Renovate or Dependabot to keep a lockfile up-to-date without incurring the full install cost.
 
       Generally you can see \`yarn up\` as a counterpart to what was \`yarn upgrade --latest\` in Yarn 1 (ie it ignores the ranges previously listed in your manifests), but unlike \`yarn upgrade\` which only upgraded dependencies in the current workspace, \`yarn up\` will upgrade all workspaces at the same time.
 
@@ -72,6 +78,11 @@ export default class UpCommand extends BaseCommand {
 
   recursive = Option.Boolean(`-R,--recursive`, false, {
     description: `Resolve again ALL resolutions for those packages`,
+  });
+
+  mode = Option.String(`--mode`, {
+    description: `Change what artifacts installs generate`,
+    validator: t.isEnum(InstallMode),
   });
 
   patterns = Option.Rest();
@@ -328,7 +339,7 @@ export default class UpCommand extends BaseCommand {
       configuration,
       stdout: this.context.stdout,
     }, async report => {
-      await project.install({cache, report});
+      await project.install({cache, report, mode: this.mode});
     });
 
     return installReport.exitCode();

.yarn/versions/fa5cc541.yml
@@ -1,0 +1,30 @@
+releases:
+  "@yarnpkg/cli": major
+  "@yarnpkg/core": minor
+  "@yarnpkg/plugin-essentials": major
+
+declined:
+  - "@yarnpkg/plugin-compat"
+  - "@yarnpkg/plugin-constraints"
+  - "@yarnpkg/plugin-dlx"
+  - "@yarnpkg/plugin-exec"
+  - "@yarnpkg/plugin-file"
+  - "@yarnpkg/plugin-git"
+  - "@yarnpkg/plugin-github"
+  - "@yarnpkg/plugin-http"
+  - "@yarnpkg/plugin-init"
+  - "@yarnpkg/plugin-interactive-tools"
+  - "@yarnpkg/plugin-link"
+  - "@yarnpkg/plugin-node-modules"
+  - "@yarnpkg/plugin-npm"
+  - "@yarnpkg/plugin-npm-cli"
+  - "@yarnpkg/plugin-pack"
+  - "@yarnpkg/plugin-patch"
+  - "@yarnpkg/plugin-pnp"
+  - "@yarnpkg/plugin-stage"
+  - "@yarnpkg/plugin-typescript"
+  - "@yarnpkg/plugin-version"
+  - "@yarnpkg/plugin-workspace-tools"
+  - "@yarnpkg/builder"
+  - "@yarnpkg/doctor"
+  - "@yarnpkg/pnpify"

packages/acceptance-tests/pkg-tests-specs/sources/commands/__snapshots__/install.test.js.snap
@@ -18,6 +18,23 @@ Object {
 }
 `;
 
+exports[`Commands install it should fetch only required packages when using \`--mode=update-lockfile\` 1`] = `
+Object {
+  "code": 0,
+  "stderr": "",
+  "stdout": "➤ YN0000: ┌ Resolution step
+➤ YN0000: └ Completed
+➤ YN0000: ┌ Fetch step
+➤ YN0013: │ no-deps@npm:2.0.0 can't be found in the cache and will be fetched from the remote registry
+➤ YN0000: └ Completed
+➤ YN0000: ┌ Link step
+➤ YN0073: │ Skipped due to mode=update-lockfile
+➤ YN0000: └ Completed
+➤ YN0000: Done with warnings
+",
+}
+`;
+
 exports[`Commands install it should print the logs to the standard output when using --inline-builds 1`] = `
 "➤ YN0000: ┌ Resolution step
 ➤ YN0000: └ Completed
@@ -37,7 +54,7 @@ exports[`Commands install it should print the logs to the standard output when u
 "
 `;
 
-exports[`Commands install it should skip build scripts when using --skip-builds 1`] = `
+exports[`Commands install it should skip build scripts when using --mode=skip-build 1`] = `
 "➤ YN0000: ┌ Resolution step
 ➤ YN0000: └ Completed
 ➤ YN0000: ┌ Fetch step

packages/acceptance-tests/pkg-tests-specs/sources/commands/install.test.js
@@ -20,20 +20,20 @@ describe(`Commands`, () => {
     );
 
     test(
-      `it should skip build scripts when using --skip-builds`,
+      `it should skip build scripts when using --mode=skip-build`,
       makeTemporaryEnv({
         dependencies: {
           [`no-deps-scripted`]: `1.0.0`,
         },
       }, async ({path, run, source}) => {
-        const {stdout} = await run(`install`, `--inline-builds`, `--skip-builds`);
+        const {stdout} = await run(`install`, `--inline-builds`, `--mode=skip-build`);
 
         await expect(stdout).toMatchSnapshot();
       }),
     );
 
     test(
-      `it shouldn't impact how artifacts are generated when using --skip-builds`,
+      `it shouldn't impact how artifacts are generated when using --mode=skip-build`,
       makeTemporaryEnv({
         dependencies: {
           [`no-deps-scripted`]: `1.0.0`,
@@ -46,7 +46,7 @@ describe(`Commands`, () => {
 
         await xfs.removePromise(pnpPath);
 
-        await run(`install`, `--skip-builds`);
+        await run(`install`, `--mode=skip-build`);
         const pnpFileWithoutBuilds = await xfs.readFilePromise(pnpPath);
 
         expect(pnpFileWithBuilds).toEqual(pnpFileWithoutBuilds);
@@ -347,6 +347,39 @@ describe(`Commands`, () => {
         const {stdout} = await run(`install`, `--inline-builds`);
         expect(stdout).toMatch(/YN0005/g);
         expect(stdout).not.toMatch(/YN0004/g);
+      }),
+    );
+
+    test(
+      `it should fetch only required packages when using \`--mode=update-lockfile\``,
+      makeTemporaryEnv({
+        dependencies: {
+          [`one-fixed-dep`]: `1.0.0`,
+          [`no-deps`]: `1.0.0`,
+        },
+      }, async ({path, run, source}) => {
+        await run(`install`, `--mode=update-lockfile`);
+
+        const cacheBefore = await xfs.readdirPromise(`${path}/.yarn/cache`);
+        expect(cacheBefore.find(entry => entry.includes(`one-fixed-dep-npm-1.0.0`))).toBeDefined();
+        expect(cacheBefore.find(entry => entry.includes(`no-deps-npm-1.0.0`))).toBeDefined();
+
+        await xfs.writeJsonPromise(`${path}/package.json`, {
+          dependencies: {
+            [`one-fixed-dep`]: `1.0.0`,
+            [`no-deps`]: `2.0.0`,
+          },
+        });
+        await xfs.removePromise(`${path}/.yarn/cache`);
+        await xfs.mkdirPromise(`${path}/.yarn/cache`, {recursive: true});
+
+        const {code, stdout, stderr} = await run(`install`, `--mode=update-lockfile`);
+        await expect({code, stdout, stderr}).toMatchSnapshot();
+
+        const cacheAfter = await xfs.readdirPromise(`${path}/.yarn/cache`);
+        expect(cacheAfter.find(entry => entry.includes(`one-fixed-dep-npm-1.0.0`))).toBeUndefined();
+        expect(cacheAfter.find(entry => entry.includes(`no-deps-npm-1.0.0`))).toBeUndefined();
+        expect(cacheAfter.find(entry => entry.includes(`no-deps-npm-2.0.0`))).toBeDefined();
       }),
     );
   });

packages/plugin-essentials/sources/commands/add.ts
@@ -1,10 +1,11 @@
 import {BaseCommand, WorkspaceRequiredError}                        from '@yarnpkg/cli';
 import {Cache, Configuration, Descriptor, LightReport, MessageName} from '@yarnpkg/core';
-import {Project, StreamReport, Workspace, Ident}                    from '@yarnpkg/core';
+import {Project, StreamReport, Workspace, Ident, InstallMode}       from '@yarnpkg/core';
 import {structUtils}                                                from '@yarnpkg/core';
 import {PortablePath}                                               from '@yarnpkg/fslib';
 import {Command, Option, Usage, UsageError}                         from 'clipanion';
 import {prompt}                                                     from 'enquirer';
+import * as t                                                       from 'typanion';
 
 import * as suggestUtils                                            from '../suggestUtils';
 import {Hooks}                                                      from '..';
@@ -35,6 +36,12 @@ export default class AddCommand extends BaseCommand {
       If the \`--cached\` option is used, Yarn will preferably reuse the highest version already used somewhere within the project, even if through a transitive dependency.
 
       If the \`-i,--interactive\` option is used (or if the \`preferInteractive\` settings is toggled on) the command will first try to check whether other workspaces in the project use the specified package and, if so, will offer to reuse them.
+
+      If the \`--mode=<mode>\` option is set, Yarn will change which artifacts are generated. The modes currently supported are:
+
+      - \`skip-build\` will not run the build scripts at all. Note that this is different from setting \`enableScripts\` to false because the later will disable build scripts, and thus affect the content of the artifacts generated on disk, whereas the former will just disable the build step - but not the scripts themselves, which just won't run.
+
+      - \`update-lockfile\` will skip the link step altogether, and only fetch packages that are missing from the lockfile (or that have no associated checksums). This mode is typically used by tools like Renovate or Dependabot to keep a lockfile up-to-date without incurring the full install cost.
 
       For a compilation of all the supported protocols, please consult the dedicated page from our website: https://yarnpkg.com/features/protocols.
     `,
@@ -97,6 +104,11 @@ export default class AddCommand extends BaseCommand {
 
   cached = Option.Boolean(`--cached`, false, {
     description: `Reuse the highest version already used somewhere within the project`,
+  });
+
+  mode = Option.String(`--mode`, {
+    description: `Change what artifacts installs generate`,
+    validator: t.isEnum(InstallMode),
   });
 
   packages = Option.Rest();
@@ -290,7 +302,7 @@ export default class AddCommand extends BaseCommand {
       stdout: this.context.stdout,
       includeLogs: !this.context.quiet,
     }, async report => {
-      await project.install({cache, report});
+      await project.install({cache, report, mode: this.mode});
     });
 
     return installReport.exitCode();

packages/plugin-essentials/sources/commands/dedupe.ts
@@ -12,12 +12,12 @@
  * (which also provides a safe-guard in case virtual descriptors ever make their way into the dedupe algorithm).
  */
 
-import {BaseCommand}                                 from '@yarnpkg/cli';
-import {Configuration, Project, Cache, StreamReport} from '@yarnpkg/core';
-import {Command, Option}                             from 'clipanion';
-import * as t                                        from 'typanion';
+import {BaseCommand}                                              from '@yarnpkg/cli';
+import {Configuration, Project, Cache, StreamReport, InstallMode} from '@yarnpkg/core';
+import {Command, Option}                                          from 'clipanion';
+import * as t                                                     from 'typanion';
 
-import * as dedupeUtils                              from '../dedupeUtils';
+import * as dedupeUtils                                           from '../dedupeUtils';
 
 // eslint-disable-next-line arca/no-default-export
 export default class DedupeCommand extends BaseCommand {
@@ -37,6 +37,12 @@ export default class DedupeCommand extends BaseCommand {
       **Note:** Even though it never produces a wrong dependency tree, this command should be used with caution, as it modifies the dependency tree, which can sometimes cause problems when packages don't strictly follow semver recommendations. Because of this, it is recommended to also review the changes manually.
 
       If set, the \`-c,--check\` flag will only report the found duplicates, without persisting the modified dependency tree. If changes are found, the command will exit with a non-zero exit code, making it suitable for CI purposes.
+
+      If the \`--mode=<mode>\` option is set, Yarn will change which artifacts are generated. The modes currently supported are:
+
+      - \`skip-build\` will not run the build scripts at all. Note that this is different from setting \`enableScripts\` to false because the later will disable build scripts, and thus affect the content of the artifacts generated on disk, whereas the former will just disable the build step - but not the scripts themselves, which just won't run.
+
+      - \`update-lockfile\` will skip the link step altogether, and only fetch packages that are missing from the lockfile (or that have no associated checksums). This mode is typically used by tools like Renovate or Dependabot to keep a lockfile up-to-date without incurring the full install cost.
 
       This command accepts glob patterns as arguments (if valid Idents and supported by [micromatch](https://github.com/micromatch/micromatch)). Make sure to escape the patterns, to prevent your own shell from trying to expand them.
 
@@ -81,6 +87,11 @@ export default class DedupeCommand extends BaseCommand {
     description: `Format the output as an NDJSON stream`,
   });
 
+  mode = Option.String(`--mode`, {
+    description: `Change what artifacts installs generate`,
+    validator: t.isEnum(InstallMode),
+  });
+
   patterns = Option.Rest();
 
   async execute() {
@@ -113,7 +124,7 @@ export default class DedupeCommand extends BaseCommand {
         stdout: this.context.stdout,
         json: this.json,
       }, async report => {
-        await project.install({cache, report});
+        await project.install({cache, report, mode: this.mode});
       });
 
       return installReport.exitCode();

packages/plugin-essentials/sources/commands/install.ts
@@ -1,8 +1,9 @@
-import {BaseCommand, WorkspaceRequiredError}                                                from '@yarnpkg/cli';
-import {Configuration, Cache, MessageName, Project, ReportError, StreamReport, formatUtils} from '@yarnpkg/core';
-import {xfs, ppath, Filename}                                                               from '@yarnpkg/fslib';
-import {parseSyml, stringifySyml}                                                           from '@yarnpkg/parsers';
-import {Command, Option, Usage}                                                             from 'clipanion';
+import {BaseCommand, WorkspaceRequiredError}                                                             from '@yarnpkg/cli';
+import {Configuration, Cache, MessageName, Project, ReportError, StreamReport, formatUtils, InstallMode} from '@yarnpkg/core';
+import {xfs, ppath, Filename}                                                                            from '@yarnpkg/fslib';
+import {parseSyml, stringifySyml}                                                                        from '@yarnpkg/parsers';
+import {Command, Option, Usage}                                                                          from 'clipanion';
+import * as t                                                                                            from 'typanion';
 
 // eslint-disable-next-line arca/no-default-export
 export default class YarnCommand extends BaseCommand {
@@ -34,7 +35,11 @@ export default class YarnCommand extends BaseCommand {
 
       If the \`--inline-builds\` option is set, Yarn will verbosely print the output of the build steps of your dependencies (instead of writing them into individual files). This is likely useful mostly for debug purposes only when using Docker-like environments.
 
-      If the \`--skip-builds\` option is set, Yarn will not run the build scripts at all. Note that this is different from setting \`enableScripts\` to false because the later will disable build scripts, and thus affect the content of the artifacts generated on disk, whereas the former will just disable the build step - but not the scripts themselves, which just won't run.
+      If the \`--mode=<mode>\` option is set, Yarn will change which artifacts are generated. The modes currently supported are:
+
+      - \`skip-build\` will not run the build scripts at all. Note that this is different from setting \`enableScripts\` to false because the later will disable build scripts, and thus affect the content of the artifacts generated on disk, whereas the former will just disable the build step - but not the scripts themselves, which just won't run.
+
+      - \`update-lockfile\` will skip the link step altogether, and only fetch packages that are missing from the lockfile (or that have no associated checksums). This mode is typically used by tools like Renovate or Dependabot to keep a lockfile up-to-date without incurring the full install cost.
     `,
     examples: [[
       `Install the project`,
@@ -68,8 +73,9 @@ export default class YarnCommand extends BaseCommand {
     description: `Verbosely print the output of the build steps of dependencies`,
   });
 
-  skipBuilds = Option.Boolean(`--skip-builds`, false, {
-    description: `Skip the build step altogether`,
+  mode = Option.String(`--mode`, {
+    description: `Change what artifacts installs generate`,
+    validator: t.isEnum(InstallMode),
   });
 
   // Legacy flags; will emit errors or warnings when used
@@ -309,7 +315,7 @@ export default class YarnCommand extends BaseCommand {
       stdout: this.context.stdout,
       includeLogs: true,
     }, async (report: StreamReport) => {
-      await project.install({cache, report, immutable, skipBuild: this.skipBuilds});
+      await project.install({cache, report, immutable, mode: this.mode});
     });
 
     return report.exitCode();

packages/plugin-essentials/sources/commands/remove.ts
@@ -1,9 +1,10 @@
 import {BaseCommand, WorkspaceRequiredError}                                from '@yarnpkg/cli';
 import {Configuration, Cache, Descriptor, Project, formatUtils, FormatType} from '@yarnpkg/core';
-import {StreamReport, Workspace}                                            from '@yarnpkg/core';
+import {StreamReport, Workspace, InstallMode}                               from '@yarnpkg/core';
 import {structUtils}                                                        from '@yarnpkg/core';
 import {Command, Option, Usage, UsageError}                                 from 'clipanion';
 import micromatch                                                           from 'micromatch';
+import * as t                                                               from 'typanion';
 
 import * as suggestUtils                                                    from '../suggestUtils';
 import {Hooks}                                                              from '..';
@@ -18,6 +19,12 @@ export default class RemoveCommand extends BaseCommand {
     description: `remove dependencies from the project`,
     details: `
       This command will remove the packages matching the specified patterns from the current workspace.
+
+      If the \`--mode=<mode>\` option is set, Yarn will change which artifacts are generated. The modes currently supported are:
+
+      - \`skip-build\` will not run the build scripts at all. Note that this is different from setting \`enableScripts\` to false because the later will disable build scripts, and thus affect the content of the artifacts generated on disk, whereas the former will just disable the build step - but not the scripts themselves, which just won't run.
+
+      - \`update-lockfile\` will skip the link step altogether, and only fetch packages that are missing from the lockfile (or that have no associated checksums). This mode is typically used by tools like Renovate or Dependabot to keep a lockfile up-to-date without incurring the full install cost.
 
       This command accepts glob patterns as arguments (if valid Idents and supported by [micromatch](https://github.com/micromatch/micromatch)). Make sure to escape the patterns, to prevent your own shell from trying to expand them.
     `,
@@ -41,6 +48,11 @@ export default class RemoveCommand extends BaseCommand {
 
   all = Option.Boolean(`-A,--all`, false, {
     description: `Apply the operation to all workspaces from the current project`,
+  });
+
+  mode = Option.String(`--mode`, {
+    description: `Change what artifacts installs generate`,
+    validator: t.isEnum(InstallMode),
   });
 
   patterns = Option.Rest();
@@ -149,7 +161,7 @@ export default class RemoveCommand extends BaseCommand {
         configuration,
         stdout: this.context.stdout,
       }, async report => {
-        await project.install({cache, report});
+        await project.install({cache, report, mode: this.mode});
       });
 
       return report.exitCode();

commit 6ad0d9004158d34e46c4b4419a9fe3b5ba4d3a71

fix: pass mode to `yarn up --recursive` (#5517)
**What's the problem this PR addresses?**
<!-- Describe the rationale of your PR. -->
<!-- Link all issues that it closes. (Closes/Resolves #xxxx.) -->

While reviewing https://github.com/yarnpkg/berry/pull/5509, I noticed
that the `--mode` flag wasn't passed to `yarn up --recursive`.

The issue started in https://github.com/yarnpkg/berry/pull/2913, when
the flag was originally introduced.

**How did you fix it?**
<!-- A detailed description of your implementation. -->

Made it pass `--mode` to `yarn up --recursive` and added tests.

**Checklist**
<!--- Don't worry if you miss something, chores are automatically
tested. -->
<!--- This checklist exists to help you remember doing the chores when
you submit a PR. -->
<!--- Put an `x` in all the boxes that apply. -->
- [X] I have read the [Contributing
Guide](https://yarnpkg.com/advanced/contributing).

<!-- See
https://yarnpkg.com/advanced/contributing#preparing-your-pr-to-be-released
for more details. -->
<!-- Check with `yarn version check` and fix with `yarn version check
-i` -->
- [X] I have set the packages that need to be released for my changes to
be effective.

<!-- The "Testing chores" workflow validates that your PR follows our
guidelines. -->
<!-- If it doesn't pass, click on it to see details as to what your PR
might be missing. -->
- [X] I will check that all automated PR checks pass before the PR gets
reviewed.

---------

Co-authored-by: Maël Nison <nison.mael@gmail.com>

.yarn/versions/5ee1a872.yml
@@ -1,0 +1,23 @@
+releases:
+  "@yarnpkg/cli": patch
+  "@yarnpkg/plugin-essentials": patch
+
+declined:
+  - "@yarnpkg/plugin-compat"
+  - "@yarnpkg/plugin-constraints"
+  - "@yarnpkg/plugin-dlx"
+  - "@yarnpkg/plugin-init"
+  - "@yarnpkg/plugin-interactive-tools"
+  - "@yarnpkg/plugin-nm"
+  - "@yarnpkg/plugin-npm-cli"
+  - "@yarnpkg/plugin-pack"
+  - "@yarnpkg/plugin-patch"
+  - "@yarnpkg/plugin-pnp"
+  - "@yarnpkg/plugin-pnpm"
+  - "@yarnpkg/plugin-stage"
+  - "@yarnpkg/plugin-typescript"
+  - "@yarnpkg/plugin-version"
+  - "@yarnpkg/plugin-workspace-tools"
+  - "@yarnpkg/builder"
+  - "@yarnpkg/core"
+  - "@yarnpkg/doctor"

packages/plugin-essentials/sources/commands/up.ts
@@ -145,6 +145,7 @@ export default class UpCommand extends BaseCommand {
       stdout: this.context.stdout,
     }, {
       cache,
+      mode: this.mode,
     });
   }
 

packages/acceptance-tests/pkg-tests-specs/sources/commands/up.test.ts
@@ -119,13 +119,49 @@ describe(`Commands`, () => {
           [`no-deps`]: `1.0.0`,
         },
       }, async ({path, run, source}) => {
-        await run(`add`, `-F`, `no-deps@latest`);
+        await run(`up`, `-F`, `no-deps@latest`);
 
         await expect(xfs.readJsonPromise(ppath.join(path, Filename.manifest))).resolves.toMatchObject({
           dependencies: {
             [`no-deps`]: `latest`,
           },
         });
+      }),
+    );
+
+    test(
+      `it should skip build scripts when using --mode=skip-build`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`no-deps-scripted`]: `1.0.0`,
+        },
+      }, async ({path, run, source}) => {
+        const {stdout} = await run(`up`, `no-deps-scripted`, `--mode=skip-build`, {
+          env: {
+            YARN_ENABLE_INLINE_BUILDS: `1`,
+          },
+        });
+
+        expect(stdout).not.toContain(`no-deps-scripted@npm:1.0.0 must be built because it never has been before`);
+        expect(stdout).not.toContain(`STDOUT preinstall out`);
+      }),
+    );
+
+    test(
+      `it should skip build scripts when using --mode=skip-build (recursive)`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`no-deps-scripted`]: `1.0.0`,
+        },
+      }, async ({path, run, source}) => {
+        const {stdout} = await run(`up`, `no-deps-scripted`, `--recursive`, `--mode=skip-build`, {
+          env: {
+            YARN_ENABLE_INLINE_BUILDS: `1`,
+          },
+        });
+
+        expect(stdout).not.toContain(`no-deps-scripted@npm:1.0.0 must be built because it never has been before`);
+        expect(stdout).not.toContain(`STDOUT preinstall out`);
       }),
     );
   });