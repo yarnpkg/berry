a95eaecb42a9b1b248e8384e23f6406bf49dc35a
Kristoffer K <merceyz@users.noreply.github.com>
2023-07-27 03:51:45 +0200

    refactor(pnp)!: remove unnecessary node bug workaround (#5618)
    **What's the problem this PR addresses?**
    
    `@yarnpkg/pnp`'s implementation of `Module._resolveFilename` has a
    workaround for a bug that was present in Node.js <12.5.0, since we no
    longer support those versions the workaround can be removed.
    
    **How did you fix it?**
    
    Removed the workaround.
    
    **Checklist**
    - [x] I have read the [Contributing
    Guide](https://yarnpkg.com/advanced/contributing).
    - [x] I have set the packages that need to be released for my changes to
    be effective.
    - [x] I will check that all automated PR checks pass before the PR gets
    reviewed.

--- .pnp.cjs
+++ .pnp.cjs
@@ -51627,7 +51627,7 @@ function makeDefaultStats() {
 }
 function clearStats(stats) {
   for (const key in stats) {
-    if (Object.prototype.hasOwnProperty.call(stats, key)) {
+    if (Object.hasOwn(stats, key)) {
       const element = stats[key];
       if (typeof element === `number`) {
         stats[key] = 0;
@@ -51643,7 +51643,7 @@ function clearStats(stats) {
 function convertToBigIntStats(stats) {
   const bigintStats = new BigIntStatsEntry();
   for (const key in stats) {
-    if (Object.prototype.hasOwnProperty.call(stats, key)) {
+    if (Object.hasOwn(stats, key)) {
       const element = stats[key];
       if (typeof element === `number`) {
         bigintStats[key] = BigInt(element);
@@ -56821,8 +56821,7 @@ function applyPatch(pnpapi, opts) {
     if (!enableNativeHooks)
       return originalModuleResolveFilename.call(require$$0.Module, request, parent, isMain, options);
     if (options && options.plugnplay === false) {
-      const { plugnplay, ...rest } = options;
-      const forwardedOptions = Object.keys(rest).length > 0 ? rest : void 0;
+      const { plugnplay, ...forwardedOptions } = options;
       try {
         enableNativeHooks = false;
         return originalModuleResolveFilename.call(require$$0.Module, request, parent, isMain, forwardedOptions);
@@ -58487,7 +58486,7 @@ ${candidates.map((candidate) => `Not found: ${getPathForDisplay(candidate)}
       const remappedPath = (!considerBuiltins || !isBuiltinModule(request)) && !isIssuerIgnored() ? resolveUnqualifiedExport(request, unqualifiedPath, conditions, issuer) : unqualifiedPath;
       return resolveUnqualified(remappedPath, { extensions });
     } catch (error) {
-      if (Object.prototype.hasOwnProperty.call(error, `pnpCode`))
+      if (Object.hasOwn(error, `pnpCode`))
         Object.assign(error.data, { request: getPathForDisplay(request), issuer: issuer && getPathForDisplay(issuer) });
       throw error;
     }
