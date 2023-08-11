95407e2029afd106eff0915a7f36985da39ff248
Maël Nison <nison.mael@gmail.com>
2023-08-11 20:14:55 +0200

    Adds debug

--- packages/docusaurus/static/js/custom.js
+++ packages/docusaurus/static/js/custom.js
@@ -4,22 +4,23 @@ const navbarCheck = document.createElement(`div`);
 navbarCheck.style.cssText = `position: absolute; top: 0; left: 0; right: 0; z-index: 9999; margin-top: var(--index-navbar-margin-top); height: var(--ifm-navbar-height); pointer-events: none;`;
 
 const intersectionObserver = new IntersectionObserver(
-  ([e]) => document.documentElement.classList.toggle(`navbar--is-fixed`, e.intersectionRatio < 1),
+  ([e]) => {
+    console.log(`Intersection Observer`, e.intersectionRatio);
+    document.documentElement.classList.toggle(`navbar--is-fixed`, e.intersectionRatio < 1);
+  },
   {threshold: [1]},
 );
 
 document.addEventListener(`DOMContentLoaded`, () => {
+  console.log(`DOMContentLoaded`);
+
   document.body.appendChild(navbarCheck);
   intersectionObserver.observe(navbarCheck);
 
+  console.log(`Set fixed`, bbox.top);
+
   const bbox = navbarCheck.getBoundingClientRect();
   document.documentElement.classList.toggle(`navbar--is-fixed`, bbox.top < 0);
-});
-
-// ---
-
-document.addEventListener(`DOMContentLoaded`, () => {
-  document.html.setAttribute(`x-doc-route`, window.location.pathname);
 });
 
 // ---
