081da10274ee2ceacafe4a6fed668852fa31c11c
Maël Nison <nison.mael@gmail.com>
2023-08-16 14:43:35 +0200

    Update docusaurus.config.js

--- packages/docusaurus/docusaurus.config.js
+++ packages/docusaurus/docusaurus.config.js
@@ -16,7 +16,7 @@ const autoLink = require(`./src/remark/autoLink`);
 const config = {
   title: `Yarn`,
   tagline: `Yarn, the modern JavaScript package manager`,
-  url: `https://yarnpkg.com`,
+  url: process.env.DEPLOY_URL ?? `https://yarnpkg.com`,
   baseUrl: `/`,
   // TODO: Switch back to `throw`
   onBrokenLinks: `warn`,
