eb54c90aef3906ae2d8d14736bdf6b7fc0001ad1
Maël Nison <nison.mael@gmail.com>
2023-08-24 17:41:15 +0200

    Couple of website improvements (#5691)
    - The DT link had an incorrect hostname
    - The DT resolution was crashing the page on `@types` packages
    - The old `/package/<name>` links were broken (added a redirection to
    the new name)
    - Filters the list of version in the version selector to remove the
    nightly builds
    - Changing the value in the version selector changes the displayed
    version
    - Clicking on a different version keeps showing the current file (and
    update its content)
    - Tags are now visible in the sidebar
    
    Fixes #5690

--- packages/docusaurus/docusaurus.config.js
+++ packages/docusaurus/docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
   onBrokenLinks: `warn`,
   onBrokenMarkdownLinks: `warn`,
   favicon: `img/yarn-favicon.svg`,
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
