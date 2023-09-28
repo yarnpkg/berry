886ee04d0bfea5096a4abea947c84ec4d317073d
Maël Nison <nison.mael@gmail.com>
2023-09-28 17:51:03 +0200

    Updates the test

--- packages/yarnpkg-core/tests/tgzUtils.test.ts
+++ packages/yarnpkg-core/tests/tgzUtils.test.ts
@@ -1,5 +1,6 @@
 import {npath, xfs}  from '@yarnpkg/fslib';
 
+import {AsyncPool}   from '../sources/TaskPool';
 import * as tgzUtils from '../sources/tgzUtils';
 
 describe(`tgzUtils`, () => {
@@ -20,6 +21,17 @@ describe(`tgzUtils`, () => {
         ),
       );
       await expect(tgzUtils.convertToZip(data, {compressionLevel: 0})).resolves.toBeTruthy();
+    });
+
+    it(`should be able to convert a tgz without any workers`, async () => {
+      const data = await xfs.readFilePromise(
+        npath.toPortablePath(
+          npath.join(__dirname, `fixtures/parse5-0.0.28.tgz`),
+        ),
+      );
+      await expect(tgzUtils.convertToZip(data, {
+        taskPool: new AsyncPool(tgzUtils.convertToZipWorker, {poolSize: 1}),
+      })).resolves.toBeTruthy();
     });
   });
 });
