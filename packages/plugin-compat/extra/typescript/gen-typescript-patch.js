cf9b5c58f67695ab8c783360ac9dae39e271c62c
Kristoffer K <merceyz@users.noreply.github.com>
2023-10-03 22:37:28 +0200

    fix(compat): update patch for `typescript@5.3.0-beta` (#5759)
    **What's the problem this PR addresses?**
    
    The PnP compatibility patch for TypeScript doesn't apply to
    `typescript@5.3.0-beta`.
    
    Ref https://github.com/microsoft/TypeScript/pull/35206
    
    **How did you fix it?**
    
    Rebased it.
    
    **Checklist**
    - [x] I have read the [Contributing
    Guide](https://yarnpkg.com/advanced/contributing).
    - [x] I have set the packages that need to be released for my changes to
    be effective.
    - [x] I will check that all automated PR checks pass before the PR gets
    reviewed.

--- packages/plugin-compat/extra/typescript/gen-typescript-patch.js
+++ packages/plugin-compat/extra/typescript/gen-typescript-patch.js
@@ -188,7 +188,14 @@ const SLICES = [
     from: `8c288a316928c9c161215fdf91ef015caa610d5b`,
     to: `8c288a316928c9c161215fdf91ef015caa610d5b`,
     onto: `6074b9d12b70757fe68ab2b4da059ea363c4df04`,
-    range: `>=5.2.1-rc`,
+    range: `>=5.2.1-rc <5.3.0-beta`,
+  },
+  // https://github.com/merceyz/TypeScript/tree/merceyz/pnp-5.3
+  {
+    from: `2b564c684dc5338c59c31f4658b737912ad46336`,
+    to: `2b564c684dc5338c59c31f4658b737912ad46336`,
+    onto: `c5de6b57b7f09a6d17eb4a5dab91ecf8f5b25f29`,
+    range: `>=5.3.0-beta`,
   },
 ];
 
