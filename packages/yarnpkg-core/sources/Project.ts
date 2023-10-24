aa023df2569e400889bbc780942278933b6ee707
Maël Nison <nison.mael@gmail.com>
2023-10-24 21:49:01 +0200

    Makes Yarn compatible with Nodebox

--- packages/yarnpkg-core/sources/Project.ts
+++ packages/yarnpkg-core/sources/Project.ts
@@ -2022,6 +2022,9 @@ export class Project {
   }
 
   async persistInstallStateFile() {
+    if (!v8.serialize)
+      return;
+
     const fields = [];
     for (const category of Object.values(INSTALL_STATE_FIELDS))
       fields.push(...category);
