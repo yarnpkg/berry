255734d3ffe4d072bd30dd34c092d49e4654866e
Peter Briggs <146312751+pwbriggs@users.noreply.github.com>
2024-02-25 21:35:54 -0800

    docs: change i.e. to e.g. in search placeholder
    In the website search placeholder text (something like "Search packages (i.e. babel, webpack, react, ...)"), i.e. is used incorrectly. "I.e." means "that is," whereas in this case it should be "e.g.", meaning "for example."

--- packages/plugin-interactive-tools/sources/commands/search.tsx
+++ packages/plugin-interactive-tools/sources/commands/search.tsx
@@ -224,7 +224,7 @@ export default class SearchCommand extends BaseCommand {
               <InkTextInput
                 value={query}
                 onChange={handleQueryOnChange}
-                placeholder={`i.e. babel, webpack, react...`}
+                placeholder={`e.g. babel, webpack, react...`}
                 showCursor={false}
               />
             </Box>
