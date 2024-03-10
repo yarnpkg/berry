04297dfe2cc7c63a109fef3597850cdb42ad5d1f
Maël Nison <nison.mael@gmail.com>
2024-03-10 09:00:23 +0100

    Lint

--- packages/plugin-pack/sources/packUtils.ts
+++ packages/plugin-pack/sources/packUtils.ts
@@ -1,11 +1,11 @@
-import {Manifest, Report, Workspace, scriptUtils}                                    from '@yarnpkg/core';
-import {FakeFS, JailFS, xfs, PortablePath, ppath, Filename, npath, constants, SubFS} from '@yarnpkg/fslib';
-import {Hooks as StageHooks}                                                         from '@yarnpkg/plugin-stage';
-import mm                                                                            from 'micromatch';
-import tar                                                                           from 'tar-stream';
-import {createGzip}                                                                  from 'zlib';
+import {Manifest, Report, Workspace, scriptUtils}                            from '@yarnpkg/core';
+import {FakeFS, SubFS, xfs, PortablePath, ppath, Filename, npath, constants} from '@yarnpkg/fslib';
+import {Hooks as StageHooks}                                                 from '@yarnpkg/plugin-stage';
+import mm                                                                    from 'micromatch';
+import tar                                                                   from 'tar-stream';
+import {createGzip}                                                          from 'zlib';
 
-import {Hooks}                                                                       from './';
+import {Hooks}                                                               from './';
 
 const NEVER_IGNORE = [
   `/package.json`,
