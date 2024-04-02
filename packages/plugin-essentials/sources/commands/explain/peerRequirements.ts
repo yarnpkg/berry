75988c7abe311f02c4dcfb60fe80f09f39d8735b
Clement Yan <yanclement94@gmail.com>
2024-04-03 00:02:04 +0800

    feat: Reimplement explain peer-requirements <hash>

--- packages/plugin-essentials/sources/commands/explain/peerRequirements.ts
+++ packages/plugin-essentials/sources/commands/explain/peerRequirements.ts
@@ -1,8 +1,9 @@
-import {BaseCommand}                                                                                               from '@yarnpkg/cli';
-import {Configuration, MessageName, Project, StreamReport, structUtils, semverUtils, formatUtils, PeerWarningType} from '@yarnpkg/core';
-import {Command, Option}                                                                                           from 'clipanion';
-import {Writable}                                                                                                  from 'stream';
-import * as t                                                                                                      from 'typanion';
+import {BaseCommand}                                                                                             from '@yarnpkg/cli';
+import type {PeerRequestNode}                                                                                    from '@yarnpkg/core/sources/Project';
+import {Configuration, MessageName, Project, StreamReport, structUtils, formatUtils, treeUtils, PeerWarningType} from '@yarnpkg/core';
+import {Command, Option}                                                                                         from 'clipanion';
+import {Writable}                                                                                                from 'stream';
+import * as t                                                                                                    from 'typanion';
 
 // eslint-disable-next-line arca/no-default-export
 export default class ExplainPeerRequirementsCommand extends BaseCommand {
@@ -46,19 +47,34 @@ export default class ExplainPeerRequirementsCommand extends BaseCommand {
 
     await project.applyLightResolution();
 
-    return await explainPeerRequirements(this.hash, project, {
+    return await explainPeerRequirement(this.hash, project, {
       stdout: this.context.stdout,
     });
   }
 }
 
-export async function explainPeerRequirements(peerRequirementsHash: string, project: Project, opts: {stdout: Writable}) {
+export async function explainPeerRequirement(peerRequirementsHash: string, project: Project, opts: {stdout: Writable}) {
+  const root = project.peerRequirementNodes.get(peerRequirementsHash);
+  if (typeof root === `undefined`)
+    throw new Error(`No peerDependency requirements found for hash: "${peerRequirementsHash}"`);
+
+  const makeTreeNode = (request: PeerRequestNode): treeUtils.TreeNode => {
+    return {
+      value: formatUtils.tuple(formatUtils.Type.DEPENDENT, {locator: request.requester, descriptor: request.descriptor}),
+      children: Object.fromEntries(
+        Array.from(request.children.values(), child => {
+          return [
+            structUtils.stringifyLocator(child.requester),
+            child ? makeTreeNode(child) : false,
+          ];
+        }),
+      ),
+    };
+  };
+
   const warning = project.peerWarnings.find(warning => {
     return warning.hash === peerRequirementsHash;
   });
-
-  if (typeof warning === `undefined`)
-    throw new Error(`No peerDependency requirements found for hash: "${peerRequirementsHash}"`);
 
   const report = await StreamReport.start({
     configuration: project.configuration,
@@ -67,87 +83,69 @@ export async function explainPeerRequirements(peerRequirementsHash: string, proj
     includePrefix: false,
   }, async report => {
     const Marks = formatUtils.mark(project.configuration);
+    const mark = warning ? Marks.Cross : Marks.Check;
 
-    switch (warning.type) {
-      case PeerWarningType.NotCompatibleAggregate: {
-        report.reportInfo(MessageName.UNNAMED, `We have a problem with ${formatUtils.pretty(project.configuration, warning.requested, formatUtils.Type.IDENT)}, which is provided with version ${structUtils.prettyReference(project.configuration, warning.version)}.`);
-        report.reportInfo(MessageName.UNNAMED, `It is needed by the following direct dependencies of workspaces in your project:`);
+    report.reportInfo(MessageName.UNNAMED, `Package ${
+      formatUtils.pretty(project.configuration, root.subject, formatUtils.Type.LOCATOR)
+    } is requested to provide ${
+      formatUtils.pretty(project.configuration, root.ident, formatUtils.Type.IDENT)
+    } by its descendants`);
 
-        report.reportSeparator();
+    report.reportSeparator();
 
-        for (const dependent of warning.requesters.values()) {
-          const dependentPkg = project.storedPackages.get(dependent.locatorHash);
-          if (!dependentPkg)
-            throw new Error(`Assertion failed: Expected the package to be registered`);
+    report.reportInfo(MessageName.UNNAMED, formatUtils.pretty(project.configuration, root.subject, formatUtils.Type.LOCATOR));
+    treeUtils.emitTree({
+      children: Object.fromEntries(
+        Array.from(root.requests.values(), request => {
+          return [
+            structUtils.stringifyLocator(request.requester),
+            makeTreeNode(request),
+          ];
+        }),
+      ),
+    }, {
+      configuration: project.configuration,
+      stdout: opts.stdout,
+      json: false,
+    });
 
-          const descriptor = dependentPkg?.peerDependencies.get(warning.requested.identHash);
-          if (!descriptor)
-            throw new Error(`Assertion failed: Expected the package to list the peer dependency`);
+    report.reportSeparator();
 
-          const mark = semverUtils.satisfiesWithPrereleases(warning.version, descriptor.range)
-            ? Marks.Check
-            : Marks.Cross;
+    if (root.provided.range === `missing:`) {
+      const problem = warning ? `` : ` , but all peer requests are optional`;
 
-          report.reportInfo(null, `  ${mark} ${structUtils.prettyLocator(project.configuration, dependent)} (via ${structUtils.prettyRange(project.configuration, descriptor.range)})`);
+      report.reportInfo(MessageName.UNNAMED, `${mark} Package ${
+        formatUtils.pretty(project.configuration, root.subject, formatUtils.Type.LOCATOR)
+      } does not provide ${
+        formatUtils.pretty(project.configuration, root.ident, formatUtils.Type.IDENT)
+      }${problem}.`);
+    } else {
+      const providedLocatorHash = project.storedResolutions.get(root.provided.descriptorHash);
+      if (!providedLocatorHash)
+        throw new Error(`Assertion failed: Expected the descriptor to be registered`);
+
+      const providedPackage = project.storedPackages.get(providedLocatorHash);
+      if (!providedPackage)
+        throw new Error(`Assertion failed: Expected the package to be registered`);
+
+      report.reportInfo(MessageName.UNNAMED, `${mark} Package ${
+        formatUtils.pretty(project.configuration, root.subject, formatUtils.Type.LOCATOR)
+      } provides ${
+        formatUtils.pretty(project.configuration, root.ident, formatUtils.Type.IDENT)
+      } with version ${
+        structUtils.prettyReference(project.configuration, providedPackage.version ?? `0.0.0`)
+      }, ${warning ? `which does not satisfy all requests.` : `which satisfies all requests`}`);
+
+      if (warning?.type === PeerWarningType.NodeNotCompatible) {
+        if (warning.range) {
+          report.reportInfo(MessageName.UNNAMED, `  The combined requested range is ${formatUtils.pretty(project.configuration, warning.range, formatUtils.Type.RANGE)}`);
+        } else {
+          report.reportInfo(MessageName.UNNAMED, `  Unfortunately, the requested ranges have no overlap`);
         }
-
-        const transitiveLinks = [...warning.links.values()].filter(link => {
-          return !warning.requesters.has(link.locatorHash);
-        });
-
-        if (transitiveLinks.length > 0) {
-          report.reportSeparator();
-          report.reportInfo(MessageName.UNNAMED, `However, those packages themselves have more dependencies listing ${structUtils.prettyIdent(project.configuration, warning.requested)} as peer dependency:`);
-          report.reportSeparator();
-
-          for (const link of transitiveLinks) {
-            const linkPkg = project.storedPackages.get(link.locatorHash);
-            if (!linkPkg)
-              throw new Error(`Assertion failed: Expected the package to be registered`);
-
-            const descriptor = linkPkg?.peerDependencies.get(warning.requested.identHash);
-            if (!descriptor)
-              throw new Error(`Assertion failed: Expected the package to list the peer dependency`);
-
-            const mark = semverUtils.satisfiesWithPrereleases(warning.version, descriptor.range)
-              ? Marks.Check
-              : Marks.Cross;
-
-            report.reportInfo(null, `  ${mark} ${structUtils.prettyLocator(project.configuration, link)} (via ${structUtils.prettyRange(project.configuration, descriptor.range)})`);
-          }
-        }
-
-        const allRanges = Array.from(warning.links.values(), locator => {
-          const pkg = project.storedPackages.get(locator.locatorHash);
-          if (typeof pkg === `undefined`)
-            throw new Error(`Assertion failed: Expected the package to be registered`);
-
-          const peerDependency = pkg.peerDependencies.get(warning.requested.identHash);
-          if (typeof peerDependency === `undefined`)
-            throw new Error(`Assertion failed: Expected the ident to be registered`);
-
-          return peerDependency.range;
-        });
-
-        if (allRanges.length > 1) {
-          const resolvedRange = semverUtils.simplifyRanges(allRanges);
-
-          report.reportSeparator();
-
-          if (resolvedRange === null) {
-            report.reportInfo(MessageName.UNNAMED, `Unfortunately, put together, we found no single range that can satisfy all those peer requirements.`);
-            report.reportInfo(MessageName.UNNAMED, `Your best option may be to try to upgrade some dependencies with ${formatUtils.pretty(project.configuration, `yarn up`, formatUtils.Type.CODE)}, or silence the warning via ${formatUtils.pretty(project.configuration, `logFilters`, formatUtils.Type.CODE)}.`);
-          } else {
-            report.reportInfo(MessageName.UNNAMED, `Put together, the final range we computed is ${formatUtils.pretty(project.configuration, resolvedRange, formatUtils.Type.RANGE)}`);
-          }
-        }
-      } break;
-
-      default: {
-        report.reportInfo(MessageName.UNNAMED, `The ${formatUtils.pretty(project.configuration, `yarn explain peer-requirements`, formatUtils.Type.CODE)} command doesn't support this warning type yet.`);
-      } break;
+      }
     }
   });
+
 
   return report.exitCode();
 }
