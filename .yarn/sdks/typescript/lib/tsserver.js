a0186c8011af94c36783e8c43dcde5c4fa302def
Kristoffer K <merceyz@users.noreply.github.com>
2024-05-02 11:01:28 +0200

    fix(sdks): only patch typescript entry point for >= 5.5 (#6263)
    **What's the problem this PR addresses?**
    
    The SDK changes in https://github.com/yarnpkg/berry/pull/6248 broke
    support for older TypeScript versions.
    I tested it on `5.4.1-rc` (master) and it was fine but it crashes on
    5.2.0-beta
    (https://github.com/yarnpkg/berry/blob/4308dca8091438e8f88682e59ef5ba5bc72241ca/package.json#L26)
    
    **How did you fix it?**
    
    Check the TypeScript version and only apply the patch if needed.
    
    **Checklist**
    - [x] I have read the [Contributing
    Guide](https://yarnpkg.com/advanced/contributing).
    - [x] I have set the packages that need to be released for my changes to
    be effective.
    - [x] I will check that all automated PR checks pass before the PR gets
    reviewed.

--- .yarn/sdks/typescript/lib/tsserver.js
+++ .yarn/sdks/typescript/lib/tsserver.js
@@ -221,7 +221,12 @@ const moduleWrapper = tsserver => {
   return tsserver;
 };
 
-moduleWrapper(absRequire(`typescript`));
+const [major, minor] = absRequire(`typescript/package.json`).version.split(`.`, 2).map(value => parseInt(value, 10));
+// In TypeScript@>=5.5 the tsserver uses the public TypeScript API so that needs to be patched as well.
+// Ref https://github.com/microsoft/TypeScript/pull/55326
+if (major > 5 || (major === 5 && minor >= 5)) {
+  moduleWrapper(absRequire(`typescript`));
+}
 
 // Defer to the real typescript/lib/tsserver.js your application uses
 module.exports = moduleWrapper(absRequire(`typescript/lib/tsserver.js`));
