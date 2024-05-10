f96145e97ef410220b9032dbb8b8c239c2cf0e06
Aetotvl6789 <68666302+Aetotvl6789@users.noreply.github.com>
2024-05-10 20:16:11 +0700

    Delete packages/docusaurus directory

--- packages/docusaurus/src/lib/descriptionRenderer.tsx
+++
@@ -1,8 +0,0 @@
-import MarkdownIt from 'markdown-it';
-import React      from 'react';
-
-const md = new MarkdownIt();
-
-export const descriptionRenderer = {
-  render: (text: string) => <div dangerouslySetInnerHTML={{__html: md.render(text)}}/>,
-};
