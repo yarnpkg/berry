d463ea48b29649bf39b4c9b87641104fa9c2e2cf
Maël Nison <nison.mael@gmail.com>
2024-07-29 10:28:48 +0200

    Adds support for user-defined wrappers in the SDK (#6370)
    ## What's the problem this PR addresses?
    
    At work we're patching the SDK files to also send monitoring information
    to our servers (so we can know how much time TS takes to run within
    VSCode). This makes updating the SDK a little difficult, as we need to
    remember to re-add the patched code after each update.
    
    ## How did you fix it?
    
    The SDK will now automatically require the optional `user.sdk.cjs` file
    if present, which must be located next to the file to patch.
    
    ## Checklist
    
    <!--- Don't worry if you miss something, chores are automatically
    tested. -->
    <!--- This checklist exists to help you remember doing the chores when
    you submit a PR. -->
    <!--- Put an `x` in all the boxes that apply. -->
    - [ ] I have read the [Contributing
    Guide](https://yarnpkg.com/advanced/contributing).
    
    <!-- See
    https://yarnpkg.com/advanced/contributing#preparing-your-pr-to-be-released
    for more details. -->
    <!-- Check with `yarn version check` and fix with `yarn version check
    -i` -->
    - [ ] I have set the packages that need to be released for my changes to
    be effective.
    
    <!-- The "Testing chores" workflow validates that your PR follows our
    guidelines. -->
    <!-- If it doesn't pass, click on it to see details as to what your PR
    might be missing. -->
    - [ ] I will check that all automated PR checks pass before the PR gets
    reviewed.

--- .yarn/sdks/typescript/lib/tsserver.js
+++ .yarn/sdks/typescript/lib/tsserver.js
@@ -8,6 +8,7 @@ const {pathToFileURL} = require(`url`);
 const relPnpApiPath = "../../../../.pnp.cjs";
 
 const absPnpApiPath = resolve(__dirname, relPnpApiPath);
+const absUserWrapperPath = resolve(__dirname, `./sdk.user.cjs`);
 const absRequire = createRequire(absPnpApiPath);
 
 const absPnpLoaderPath = resolve(absPnpApiPath, `../.pnp.loader.mjs`);
@@ -23,7 +24,15 @@ if (existsSync(absPnpApiPath)) {
   }
 }
 
-const moduleWrapper = tsserver => {
+const wrapWithUserWrapper = existsSync(absUserWrapperPath)
+  ? exports => absRequire(absUserWrapperPath)(exports)
+  : exports => exports;
+
+const moduleWrapper = exports => {
+  return wrapWithUserWrapper(moduleWrapperFn(exports));
+};
+
+const moduleWrapperFn = tsserver => {
   if (!process.versions.pnp) {
     return tsserver;
   }
