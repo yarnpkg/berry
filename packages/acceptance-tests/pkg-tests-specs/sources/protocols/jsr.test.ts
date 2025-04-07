ed6bc5dded08348e3863921bf02b3ee40d38860c
Maël Nison <nison.mael@gmail.com>
2025-04-07 15:28:12 +0200

    Adds packing translation

--- packages/acceptance-tests/pkg-tests-specs/sources/protocols/jsr.test.ts
+++ packages/acceptance-tests/pkg-tests-specs/sources/protocols/jsr.test.ts
@@ -1,5 +1,6 @@
-import {ppath, xfs} from '@yarnpkg/fslib';
-import {tests}      from 'pkg-tests-core';
+import {ppath, xfs}    from '@yarnpkg/fslib';
+import {fs as fsUtils} from 'pkg-tests-core';
+import {tests}         from 'pkg-tests-core';
 
 describe(`Protocols`, () => {
   describe(`jsr:`, () => {
@@ -49,6 +50,41 @@ describe(`Protocols`, () => {
           await expect(source(`require('foo')`)).resolves.toMatchObject({
             name: `@jsr/no-deps-jsr`,
             version: `1.0.0`,
+          });
+        },
+      ),
+    );
+
+    test(
+      `it should replace the jsr registry with a npm registry during packing`,
+      makeTemporaryEnv(
+        {
+          dependencies: {[`no-deps-jsr`]: `jsr:1.0.0`},
+        },
+        async ({path, run, source}) => {
+          await xfs.writeFilePromise(ppath.join(path, `.yarnrc.yml`), JSON.stringify({
+            [`npmScopes`]: {
+              [`jsr`]: {
+                [`npmRegistryServer`]: `${await tests.startPackageServer()}/registry/jsr`,
+              },
+            },
+          }));
+
+          await run(`install`);
+          await run(`pack`);
+
+          const tarballPath = ppath.join(path, `package.tgz`);
+          const unpackedPath = ppath.join(path, `unpacked`);
+
+          await xfs.mkdirPromise(unpackedPath);
+          await fsUtils.unpackToDirectory(unpackedPath, tarballPath);
+
+          const manifest = await xfs.readJsonPromise(ppath.join(unpackedPath, `package`, `package.json`));
+
+          expect(manifest).toMatchObject({
+            dependencies: {
+              [`no-deps-jsr`]: `npm:@jsr/no-deps-jsr@1.0.0`,
+            },
           });
         },
       ),
--- packages/plugin-jsr/sources/index.ts
+++ packages/plugin-jsr/sources/index.ts
@@ -1,23 +1,52 @@
-import {Descriptor, Locator, Plugin, Project, ResolveOptions, Resolver} from '@yarnpkg/core';
-import {structUtils, semverUtils}                                       from '@yarnpkg/core';
+import {Descriptor, Locator, Plugin, Project, ResolveOptions, Resolver, Workspace} from '@yarnpkg/core';
+import {structUtils, semverUtils}                                                  from '@yarnpkg/core';
 
-function reduceDependency(dependency: Descriptor, project: Project, locator: Locator, initialDependency: Descriptor, {resolver, resolveOptions}: {resolver: Resolver, resolveOptions: ResolveOptions}) {
-  if (dependency.range.startsWith(`jsr:`)) {
-    if (semverUtils.validRange(dependency.range.slice(4)))
-      return structUtils.makeDescriptor(dependency, `npm:${structUtils.wrapIdentIntoScope(dependency, `jsr`)}@${dependency.range.slice(4)}`);
+function normalizeJsrDependency(dependency: Descriptor) {
+  if (semverUtils.validRange(dependency.range.slice(4)))
+    return structUtils.makeDescriptor(dependency, `npm:${structUtils.wrapIdentIntoScope(dependency, `jsr`)}@${dependency.range.slice(4)}`);
 
-    const parsedRange = structUtils.tryParseDescriptor(dependency.range.slice(4), true);
-    if (parsedRange !== null) {
-      return structUtils.makeDescriptor(dependency, `npm:${structUtils.wrapIdentIntoScope(parsedRange, `jsr`)}@${parsedRange.range}`);
-    }
-  }
+  const parsedRange = structUtils.tryParseDescriptor(dependency.range.slice(4), true);
+  if (parsedRange !== null)
+    return structUtils.makeDescriptor(dependency, `npm:${structUtils.wrapIdentIntoScope(parsedRange, `jsr`)}@${parsedRange.range}`);
+
 
   return dependency;
+}
+
+function reduceDependency(dependency: Descriptor, project: Project, locator: Locator, initialDependency: Descriptor, {resolver, resolveOptions}: {resolver: Resolver, resolveOptions: ResolveOptions}) {
+  return dependency.range.startsWith(`jsr:`)
+    ? normalizeJsrDependency(dependency)
+    : dependency;
+}
+
+const DEPENDENCY_TYPES = [`dependencies`, `devDependencies`, `peerDependencies`];
+
+function beforeWorkspacePacking(workspace: Workspace, rawManifest: any) {
+  for (const dependencyType of DEPENDENCY_TYPES) {
+    for (const descriptor of workspace.manifest.getForScope(dependencyType).values()) {
+      if (!descriptor.range.startsWith(`jsr:`))
+        continue;
+
+      const normalizedDescriptor = normalizeJsrDependency(descriptor);
+
+      // Ensure optional dependencies are handled as well
+      const identDescriptor = dependencyType === `dependencies`
+        ? structUtils.makeDescriptor(descriptor, `unknown`)
+        : null;
+
+      const finalDependencyType = identDescriptor !== null && workspace.manifest.ensureDependencyMeta(identDescriptor).optional
+        ? `optionalDependencies`
+        : dependencyType;
+
+      rawManifest[finalDependencyType][structUtils.stringifyIdent(descriptor)] = normalizedDescriptor.range;
+    }
+  }
 }
 
 const plugin: Plugin = {
   hooks: {
     reduceDependency,
+    beforeWorkspacePacking,
   },
 };
 
