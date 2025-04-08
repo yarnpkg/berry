09ce636cef8bd6b3aa0a949ce219435f1047838b
Maël Nison <nison.mael@gmail.com>
2025-04-08 09:12:54 +0200

    Adds support for the `jsr:` protocol (#6752)
    ## What's the problem this PR addresses?
    
    I noticed https://github.com/pnpm/pnpm/pull/9358 and figured it'd make
    sense to support this in Yarn as well.
    
    ## How did you fix it?
    
    Yarn will natively understand the `jsr` protocol and automatically
    reroute the dependency to its `@jsr`-prefixed variant.
    
    Still need to add tests.
    
    ## Checklist
    
    <!--- Don't worry if you miss something, chores are automatically
    tested. -->
    <!--- This checklist exists to help you remember doing the chores when
    you submit a PR. -->
    <!--- Put an `x` in all the boxes that apply. -->
    - [x] I have read the [Contributing
    Guide](https://yarnpkg.com/advanced/contributing).
    
    <!-- See
    https://yarnpkg.com/advanced/contributing#preparing-your-pr-to-be-released
    for more details. -->
    <!-- Check with `yarn version check` and fix with `yarn version check
    -i` -->
    - [x] I have set the packages that need to be released for my changes to
    be effective.
    
    <!-- The "Testing chores" workflow validates that your PR follows our
    guidelines. -->
    <!-- If it doesn't pass, click on it to see details as to what your PR
    might be missing. -->
    - [x] I will check that all automated PR checks pass before the PR gets
    reviewed.
    
    ---------
    
    Co-authored-by: Luca Casonato <hello@lcas.dev>

---
+++ packages/acceptance-tests/pkg-tests-specs/sources/protocols/jsr.test.ts
@@ -0,0 +1,93 @@
+import {ppath, xfs}    from '@yarnpkg/fslib';
+import {fs as fsUtils} from 'pkg-tests-core';
+import {tests}         from 'pkg-tests-core';
+
+describe(`Protocols`, () => {
+  describe(`jsr:`, () => {
+    test(
+      `it should allow installing a package from a jsr registry`,
+      makeTemporaryEnv(
+        {
+          dependencies: {[`no-deps-jsr`]: `jsr:1.0.0`},
+        },
+        async ({path, run, source}) => {
+          await xfs.writeFilePromise(ppath.join(path, `.yarnrc.yml`), JSON.stringify({
+            [`npmScopes`]: {
+              [`jsr`]: {
+                [`npmRegistryServer`]: `${await tests.startPackageServer()}/registry/jsr`,
+              },
+            },
+          }));
+
+          await run(`install`);
+
+          await expect(source(`require('no-deps-jsr')`)).resolves.toMatchObject({
+            // The package name is prefixed with @jsr/ because that's what the registry returns
+            name: `@jsr/no-deps-jsr`,
+            version: `1.0.0`,
+          });
+        },
+      ),
+    );
+
+    test(
+      `it should allow renaming packages`,
+      makeTemporaryEnv(
+        {
+          dependencies: {[`foo`]: `jsr:no-deps-jsr@1.0.0`},
+        },
+        async ({path, run, source}) => {
+          await xfs.writeFilePromise(ppath.join(path, `.yarnrc.yml`), JSON.stringify({
+            [`npmScopes`]: {
+              [`jsr`]: {
+                [`npmRegistryServer`]: `${await tests.startPackageServer()}/registry/jsr`,
+              },
+            },
+          }));
+
+          await run(`install`);
+
+          await expect(source(`require('foo')`)).resolves.toMatchObject({
+            name: `@jsr/no-deps-jsr`,
+            version: `1.0.0`,
+          });
+        },
+      ),
+    );
+
+    test(
+      `it should replace the jsr registry with a npm registry during packing`,
+      makeTemporaryEnv(
+        {
+          dependencies: {[`no-deps-jsr`]: `jsr:1.0.0`},
+        },
+        async ({path, run, source}) => {
+          await xfs.writeFilePromise(ppath.join(path, `.yarnrc.yml`), JSON.stringify({
+            [`npmScopes`]: {
+              [`jsr`]: {
+                [`npmRegistryServer`]: `${await tests.startPackageServer()}/registry/jsr`,
+              },
+            },
+          }));
+
+          await run(`install`);
+          await run(`pack`);
+
+          const tarballPath = ppath.join(path, `package.tgz`);
+          const unpackedPath = ppath.join(path, `unpacked`);
+
+          await xfs.mkdirPromise(unpackedPath);
+          await fsUtils.unpackToDirectory(unpackedPath, tarballPath);
+
+          const manifest = await xfs.readJsonPromise(ppath.join(unpackedPath, `package`, `package.json`));
+
+          expect(manifest).toMatchObject({
+            dependencies: {
+              [`no-deps-jsr`]: `npm:@jsr/no-deps-jsr@1.0.0`,
+            },
+          });
+        },
+      ),
+    );
+  });
+});
