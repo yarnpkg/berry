c7e0ad8c15ce2e71d590fe296e2067d18486656b
Paulius Valiūnas <66480813+paulius-valiunas@users.noreply.github.com>
2025-05-02 11:56:10 +0300

    add `@fastify/type-provider-typebox^5.0.0`

--- packages/yarnpkg-extensions/sources/index.ts
+++ packages/yarnpkg-extensions/sources/index.ts
@@ -1000,4 +1000,14 @@ export const packageExtensions: Array<[string, PackageExtensionData]> = [
       csstype: `^3.0.10`,
     },
   }],
+  [`@fastify/type-provider-typebox^5.0.0`, {
+    peerDependencies: {
+      fastify: `^5.0.0`,
+    },
+  }],
+  [`@fastify/type-provider-typebox^4.0.0`, {
+    peerDependencies: {
+      fastify: `^4.0.0`,
+    },
+  }],
 ];
