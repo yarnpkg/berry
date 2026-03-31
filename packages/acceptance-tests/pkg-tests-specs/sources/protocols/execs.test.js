24f50b12bf5a200b116c948724476953c0e46549
Maël Nison <mael.nison@mistral.ai>
2026-03-31 11:35:40 +0200

    Makes the exec protocol respect enableScripts

--- packages/acceptance-tests/pkg-tests-specs/sources/protocols/execs.test.js
+++ packages/acceptance-tests/pkg-tests-specs/sources/protocols/execs.test.js
@@ -22,6 +22,78 @@ describe(`Protocols`, () => {
     );
 
     test(
+      `it should respect \`enableScripts\``,
+      makeTemporaryEnv({
+        dependencies: {
+          [`dynamic-pkg`]: `exec:./genpkg.js`,
+        },
+      }, async ({path, run}) => {
+        await xfs.writeFilePromise(`${path}/genpkg.js`, `
+          const {buildDir} = execEnv;
+          fs.writeFileSync(path.join(buildDir, 'index.js'), 'module.exports = 42;');
+          fs.writeFileSync(path.join(buildDir, 'package.json'), '{}');
+        `);
+
+        await xfs.writeFilePromise(`${path}/.yarnrc.yml`, `enableScripts: false\n`);
+
+        await expect(run(`install`)).rejects.toThrow(/all scripts have been disabled/);
+      }),
+    );
+
+    test(
+      `it should allow \`exec:\` when explicitly enabled via \`dependenciesMeta[].built\``,
+      makeTemporaryEnv({
+        dependencies: {
+          [`dynamic-pkg`]: `exec:./genpkg.js`,
+        },
+        dependenciesMeta: {
+          [`dynamic-pkg`]: {
+            built: true,
+          },
+        },
+      }, async ({path, run, source}) => {
+        await xfs.writeFilePromise(`${path}/genpkg.js`, `
+          const {buildDir} = execEnv;
+          fs.writeFileSync(path.join(buildDir, 'index.js'), 'module.exports = 42;');
+          fs.writeFileSync(path.join(buildDir, 'package.json'), '{}');
+        `);
+
+        await xfs.writeFilePromise(`${path}/.yarnrc.yml`, `enableScripts: false\n`);
+
+        await run(`install`);
+
+        await expect(source(`require('dynamic-pkg')`)).resolves.toEqual(42);
+      }),
+    );
+
+    test(
+      `it should prevent non-workspaces from depending on \`exec:\` packages`,
+      makeTemporaryEnv({
+        dependencies: {
+          [`wrapper`]: `file:./wrapper`,
+        },
+      }, async ({path, run}) => {
+        await xfs.mkdirPromise(`${path}/wrapper`);
+
+        await xfs.writeFilePromise(`${path}/wrapper/package.json`, JSON.stringify({
+          name: `wrapper`,
+          version: `1.0.0`,
+          dependencies: {
+            [`dynamic-pkg`]: `exec:./genpkg.js`,
+          },
+        }));
+
+        await xfs.writeFilePromise(`${path}/wrapper/genpkg.js`, `
+          const {buildDir} = execEnv;
+          fs.writeFileSync(path.join(buildDir, 'index.js'), 'module.exports = 42;');
+          fs.writeFileSync(path.join(buildDir, 'package.json'), '{}');
+        `);
+
+        await expect(run(`install`)).rejects.toThrow(/only workspaces can depend on exec: packages/);
+      }),
+    );
+
+    test(
       `it should correctly inject the built-in modules as global variables`,
       makeTemporaryEnv({
         dependencies: {
--- packages/yarnpkg-core/sources/Project.ts
+++ packages/yarnpkg-core/sources/Project.ts
@@ -751,6 +751,7 @@ export class Project {
     const packageExtensions = await this.configuration.getPackageExtensions();
 
     const pkg = this.configuration.normalizePackage(originalPkg, {packageExtensions});
+    const isWorkspacePackage = this.tryWorkspaceByLocator(pkg) !== null;
 
     for (const [identHash, descriptor] of pkg.dependencies) {
       const dependency = await this.configuration.reduceHook(hooks => {
@@ -762,6 +763,9 @@ export class Project {
 
       if (!structUtils.areIdentsEqual(descriptor, dependency))
         throw new Error(`Assertion failed: The descriptor ident cannot be changed through aliases`);
+
+      if (!isWorkspacePackage && dependency.range.startsWith(`exec:`))
+        throw new ReportError(MessageName.INVALID_MANIFEST, `${structUtils.prettyLocator(this.configuration, pkg)} lists ${structUtils.prettyDescriptor(this.configuration, dependency)} as dependency, but only workspaces can depend on exec: packages.`);
 
       const bound = resolver.bindDescriptor(dependency, pkg, resolveOptions);
       pkg.dependencies.set(identHash, bound);
