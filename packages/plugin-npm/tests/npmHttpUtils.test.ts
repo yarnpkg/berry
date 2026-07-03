e5e75672b52bb299460b43892ac6ba281065c718
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-07-03 11:18:01 +0200

    Skip OTP prompts in non-TTY mode

--- packages/plugin-npm/tests/npmHttpUtils.test.ts
+++ packages/plugin-npm/tests/npmHttpUtils.test.ts
@@ -26,3 +26,59 @@ describe(`npmHttpUtils.get`, () => {
     }
   }
 });
+
+describe(`npmHttpUtils.put`, () => {
+  it(`shouldn't ask for an OTP when stdin isn't a TTY`, async () => {
+    const configuration = await makeConfiguration();
+
+    const originalIsTestEnv = process.env.YARN_IS_TEST_ENV;
+    const originalIsTTY = process.stdin.isTTY;
+
+    process.env.YARN_IS_TEST_ENV = ``;
+    Object.defineProperty(process.stdin, `isTTY`, {
+      configurable: true,
+      value: false,
+    });
+
+    try {
+      let requestCount = 0;
+
+      await expect(npmHttpUtils.put(`/foo`, {}, {
+        configuration,
+        registry: `https://example.org`,
+        authType: npmHttpUtils.AuthType.NO_AUTH,
+        async wrapNetworkRequest() {
+          return async () => {
+            requestCount += 1;
+
+            const error: Error & {response?: any} = new Error(`OTP required`);
+            error.name = `HTTPError`;
+            error.response = {
+              body: {
+                error: `OTP required`,
+              },
+              headers: {
+                [`www-authenticate`]: `OTP`,
+              },
+              statusCode: 401,
+            };
+
+            throw error;
+          };
+        },
+      })).rejects.toThrow(/The registry requires additional authentication, but Yarn is not running in an interactive terminal/);
+
+      expect(requestCount).toBe(1);
+    } finally {
+      if (typeof originalIsTestEnv === `undefined`)
+        delete process.env.YARN_IS_TEST_ENV;
+      else
+        process.env.YARN_IS_TEST_ENV = originalIsTestEnv;
+
+      Object.defineProperty(process.stdin, `isTTY`, {
+        configurable: true,
+        value: originalIsTTY,
+      });
+    }
+  });
+});
